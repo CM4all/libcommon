5bcb9e1ecdb82809d4940c6662ddbdf052d23043
Max Kellermann <max.kellermann@gmail.com>
2017-03-17 11:09:30 +0100

    io/WriteFile: new utility library

---
+++ src/io/WriteFile.cxx
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2015-2017 Max Kellermann <max.kellermann@gmail.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "WriteFile.hxx"
+#include "UniqueFileDescriptor.hxx"
+#include "util/ConstBuffer.hxx"
+#include "util/StringView.hxx"
+
+#include <errno.h>
+#include <fcntl.h>
+
+static WriteFileResult
+TryWriteExistingFile(const char *path, ConstBuffer<void> value)
+{
+	UniqueFileDescriptor fd;
+	if (!fd.Open(path, O_WRONLY))
+		return WriteFileResult::ERROR;
+
+	ssize_t nbytes = fd.Write(value.data, value.size);
+	if (nbytes < 0)
+		return WriteFileResult::ERROR;
+	else if (size_t(nbytes) == value.size)
+		return WriteFileResult::SUCCESS;
+	else
+		return WriteFileResult::SHORT;
+}
+
+WriteFileResult
+TryWriteExistingFile(const char *path, const char *value)
+{
+	return TryWriteExistingFile(path, StringView(value).ToVoid());
+}
---
+++ src/io/WriteFile.hxx
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2015-2017 Max Kellermann <max.kellermann@gmail.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef WRITE_FILE_HXX
+#define WRITE_FILE_HXX
+
+enum class WriteFileResult {
+	/**
+	 * The operation was successful.
+	 */
+	SUCCESS,
+
+	/**
+	 * There was an I/O error, and errno contains details.
+	 */
+	ERROR,
+
+	/**
+	 * The write operation was too short - not all bytes were
+	 * written.
+	 */
+	SHORT,
+};
+
+/**
+ * Attempt to write a string to the given file.  It must already
+ * exist, and it is not truncated or appended.  This function is
+ * useful to write "special" files like the ones in /proc.
+ */
+WriteFileResult
+TryWriteExistingFile(const char *path, const char *value);
+
+#endif
