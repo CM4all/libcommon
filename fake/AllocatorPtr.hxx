fa8ed3f7d3e04c6f636e438523fed859f31285cf
Max Kellermann <mk@cm4all.com>
2017-03-27 15:08:11 +0200

    fake/AllocatorPtr: add method Concat()

--- fake/AllocatorPtr.hxx
+++ fake/AllocatorPtr.hxx
@@ -6,6 +6,7 @@
 #define ALLOCATOR_PTR_HXX
 
 #include "util/ConstBuffer.hxx"
+#include "util/StringView.hxx"
 
 #include <forward_list>
 #include <functional>
@@ -13,8 +14,6 @@
 
 #include <stdlib.h>
 #include <string.h>
-
-struct StringView;
 
 class Allocator {
     std::forward_list<std::function<void()>> cleanup;
@@ -43,6 +42,14 @@ public:
         return p;
     }
 
+    template<typename... Args>
+    char *Concat(Args&&... args) {
+        const size_t length = ConcatLength(args...);
+        char *result = NewArray<char>(length + 1);
+        *ConcatCopy(result, args...) = 0;
+        return result;
+    }
+
     template<typename T, typename... Args>
     T *New(Args&&... args) {
         auto p = new T(std::forward<Args>(args)...);
@@ -54,6 +61,36 @@ public:
     T *NewArray(size_t n) {
         auto p = new T[n];
         cleanup.emplace_front([p](){ delete[] p; });
+        return p;
+    }
+
+private:
+    template<typename... Args>
+    static size_t ConcatLength(const char *s, Args... args) {
+        return strlen(s) + ConcatLength(args...);
+    }
+
+    template<typename... Args>
+    static constexpr size_t ConcatLength(StringView s, Args... args) {
+        return s.size + ConcatLength(args...);
+    }
+
+    static constexpr size_t ConcatLength() {
+        return 0;
+    }
+
+    template<typename... Args>
+    static char *ConcatCopy(char *p, const char *s, Args... args) {
+        return ConcatCopy(stpcpy(p, s), args...);
+    }
+
+    template<typename... Args>
+    static char *ConcatCopy(char *p, StringView s, Args... args) {
+        return ConcatCopy((char *)mempcpy(p, s.data, s.size), args...);
+    }
+
+    template<typename... Args>
+    static char *ConcatCopy(char *p) {
         return p;
     }
 };
@@ -70,6 +107,11 @@ public:
 
     const char *CheckDup(const char *src) {
         return src != nullptr ? allocator.Dup(src) : nullptr;
+    }
+
+    template<typename... Args>
+    char *Concat(Args&&... args) {
+        return allocator.Concat(std::forward<Args>(args)...);
     }
 
     template<typename T, typename... Args>
