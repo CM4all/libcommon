b0987fae05729c8ff04e55bf2c18178c13b16f77
Max Kellermann <mk@cm4all.com>
2017-03-27 12:48:33 +0200

    adata: import from beng-proxy 870fccd4efb1352c56a974

---
+++ fake/translation/Features.hxx
@@ -0,0 +1,23 @@
+/*
+ * This header enables or disables certain features of the translation
+ * client.  More specifically, it can be used to eliminate
+ * #TranslateRequest and #TranslateResponse attributes.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ */
+
+#ifndef BENG_PROXY_TRANSLATION_FEATURES_HXX
+#define BENG_PROXY_TRANSLATION_FEATURES_HXX
+
+#define TRANSLATION_ENABLE_CACHE 0
+#define TRANSLATION_ENABLE_WANT 0
+#define TRANSLATION_ENABLE_EXPAND 0
+#define TRANSLATION_ENABLE_SESSION 0
+#define TRANSLATION_ENABLE_HTTP 0
+#define TRANSLATION_ENABLE_WIDGET 0
+#define TRANSLATION_ENABLE_RADDRESS 0
+#define TRANSLATION_ENABLE_TRANSFORMATION 0
+#define TRANSLATION_ENABLE_EXECUTE 0
+#define TRANSLATION_ENABLE_JAILCGI 0
+
+#endif
