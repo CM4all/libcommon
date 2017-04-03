a20b3764d120e251811bf849b59dfdf0687d3629
Max Kellermann <mk@cm4all.com>
2017-04-03 22:05:32 +0200

    spawn/Hook: add API documentation

--- src/spawn/Hook.hxx
+++ src/spawn/Hook.hxx
@@ -7,8 +7,22 @@
 
 struct PreparedChildProcess;
 
+/**
+ * Hooks to be called by the spawn server.
+ */
 class SpawnHook {
 public:
+    /**
+     * Verify the #PreparedChildProcess object, especially the #UidGid
+     * settings, whether these credentials are allowed.
+     *
+     * Throws std::runtime_error if the #PreparedChildProcess object
+     * is rejected.
+     *
+     * @return true if the #PreparedChildProcess object is accepted,
+     * false to apply the default checks instead (which may then
+     * result in acceptance or rejection)
+     */
     virtual bool Verify(const PreparedChildProcess &) {
         return false;
     }
