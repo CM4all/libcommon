0f9f9342e6d219cf3d84726af7485a26ec3c6f5c
Max Kellermann <mk@cm4all.com>
2017-04-06 21:09:20 +0200

    time/gmtime: move from beng-proxy 0fdbdb04914d222e71a997edd9b67283776f6a88

---
+++ src/time/gmtime.h
@@ -0,0 +1,28 @@
+/*
+ * Babak's fast gmtime() implementation.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ */
+
+#ifndef __BENG_GMTIME_H
+#define __BENG_GMTIME_H
+
+#include <stdint.h>
+#include <time.h>
+
+typedef struct tm xbrokentime;
+
+#define LIBCORE__STDCALL(x) x
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+LIBCORE__STDCALL(xbrokentime *)
+sysx_time_gmtime(time_t tm32, xbrokentime *tmrec);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
