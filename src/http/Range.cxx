4475f54690fc2627c5ed2cf6670ff20bef3f2d29
Max Kellermann <mk@cm4all.com>
2017-04-10 13:17:25 +0200

    http/Range: move from beng-proxy fe8e3e78bb7643eb9c3cd578b24f3ccaf69000cb

---
+++ src/http/Range.cxx
@@ -0,0 +1,62 @@
+/*
+ * author: Max Kellermann <mk@cm4all.com>
+ */
+
+#include "Range.hxx"
+
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+
+void
+HttpRangeRequest::ParseRangeHeader(const char *p)
+{
+    assert(p != nullptr);
+    assert(type == Type::NONE);
+    assert(skip == 0);
+
+    if (strncmp(p, "bytes=", 6) != 0) {
+        type = Type::INVALID;
+        return;
+    }
+
+    p += 6;
+
+    char *endptr;
+
+    if (*p == '-') {
+        /* suffix-byte-range-spec */
+        ++p;
+
+        uint64_t v = strtoull(p, &endptr, 10);
+        if (v >= size)
+            return;
+
+        skip = size - v;
+    } else {
+        skip = strtoull(p, &endptr, 10);
+        if (skip >= size) {
+            type = Type::INVALID;
+            return;
+        }
+
+        if (*endptr == '-') {
+            p = endptr + 1;
+            if (*p == 0) {
+                /* "wget -c" */
+                type = Type::VALID;
+                return;
+            }
+
+            uint64_t v = strtoull(p, &endptr, 10);
+            if (*endptr != 0 || v < skip || v >= size) {
+                type = Type::INVALID;
+                return;
+            }
+
+            size = v + 1;
+        }
+    }
+
+    type = Type::VALID;
+}
---
+++ src/http/Range.hxx
@@ -0,0 +1,29 @@
+/*
+ * author: Max Kellermann <mk@cm4all.com>
+ */
+
+#ifndef HTTP_RANGE_HXX
+#define HTTP_RANGE_HXX
+
+#include <inline/compiler.h>
+
+#include <chrono>
+
+struct HttpRangeRequest {
+    enum class Type {
+        NONE,
+        VALID,
+        INVALID,
+    } type = Type::NONE;
+
+    uint64_t skip = 0, size;
+
+    explicit HttpRangeRequest(uint64_t _size):size(_size) {}
+
+    /**
+     * Parse a "Range" request header.
+     */
+    void ParseRangeHeader(const char *p);
+};
+
+#endif
