62c43db929a9d8b61a2770a6866557b814abc1a2
Max Kellermann <mk@cm4all.com>
2017-04-21 11:19:18 +0200

    io/FdType: move from beng-proxy cc141885928184ecfeb93f927f6125eaa9afe434

---
+++ src/io/FdType.hxx
@@ -0,0 +1,35 @@
+/*
+ * author: Max Kellermann <mk@cm4all.com>
+ */
+
+#ifndef FD_TYPE_HXX
+#define FD_TYPE_HXX
+
+enum FdType {
+    /**
+     * No file descriptor available.  Special value that is only
+     * supported by few libraries.
+     */
+    FD_NONE = 00,
+
+    FD_FILE = 01,
+    FD_PIPE = 02,
+    FD_SOCKET = 04,
+    FD_TCP = 010,
+
+    /** a character device, such as /dev/zero or /dev/null */
+    FD_CHARDEV = 020,
+};
+
+typedef unsigned FdTypeMask;
+
+static constexpr FdTypeMask FD_ANY_SOCKET = FD_SOCKET | FD_TCP;
+static constexpr FdTypeMask FD_ANY = FD_FILE | FD_PIPE | FD_ANY_SOCKET;
+
+static constexpr inline bool
+IsAnySocket(FdType type)
+{
+    return (FdTypeMask(type) & FD_ANY_SOCKET) != 0;
+}
+
+#endif
