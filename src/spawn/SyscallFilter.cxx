172fd19aabf63706bef49bbadca5f4bb74485d3b
Max Kellermann <mk@cm4all.com>
2017-05-15 18:19:28 +0200

    spawn/SyscallFilter: move forbidden system calls to array

--- src/spawn/SyscallFilter.cxx
+++ src/spawn/SyscallFilter.cxx
@@ -5,20 +5,29 @@
 #include "SyscallFilter.hxx"
 #include "SeccompFilter.hxx"
 
+/**
+ * The system calls which are forbidden unconditionally.
+ */
+static constexpr int forbidden_syscalls[] = {
+    SCMP_SYS(adjtimex),
+    SCMP_SYS(delete_module),
+    SCMP_SYS(init_module),
+
+    /* ptrace() is dangerous because it allows breaking out of
+       namespaces */
+    SCMP_SYS(ptrace),
+
+    SCMP_SYS(reboot),
+    SCMP_SYS(settimeofday),
+    SCMP_SYS(swapoff),
+    SCMP_SYS(swapon),
+};
+
 void
 BuildSyscallFilter(Seccomp::Filter &sf)
 {
     /* forbid a bunch of dangerous system calls */
 
-    sf.AddRule(SCMP_ACT_KILL, SCMP_SYS(init_module));
-    sf.AddRule(SCMP_ACT_KILL, SCMP_SYS(delete_module));
-    sf.AddRule(SCMP_ACT_KILL, SCMP_SYS(reboot));
-    sf.AddRule(SCMP_ACT_KILL, SCMP_SYS(settimeofday));
-    sf.AddRule(SCMP_ACT_KILL, SCMP_SYS(adjtimex));
-    sf.AddRule(SCMP_ACT_KILL, SCMP_SYS(swapon));
-    sf.AddRule(SCMP_ACT_KILL, SCMP_SYS(swapoff));
-
-    /* ptrace() is dangerous because it allows breaking out of
-       namespaces */
-    sf.AddRule(SCMP_ACT_KILL, SCMP_SYS(ptrace));
+    for (auto i : forbidden_syscalls)
+        sf.AddRule(SCMP_ACT_KILL, i);
 }
