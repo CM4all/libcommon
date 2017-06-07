0162aa54d7e898833031a9584c4c18784e28deaf
Max Kellermann <mk@cm4all.com>
2017-06-07 20:06:41 +0200

    net/ConnectSocket: move from qrelay

---
+++ src/net/ConnectSocket.cxx
@@ -0,0 +1,86 @@
+/*
+ * author: Max Kellermann <mk@cm4all.com>
+ */
+
+#include "ConnectSocket.hxx"
+#include "net/UniqueSocketDescriptor.hxx"
+#include "net/SocketAddress.hxx"
+#include "system/Error.hxx"
+
+#include <stdexcept>
+
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+static constexpr timeval connect_timeout{10, 0};
+
+void
+ConnectSocketHandler::OnSocketConnectTimeout()
+{
+    /* default implementation falls back to OnSocketConnectError() */
+    OnSocketConnectError(std::make_exception_ptr(std::runtime_error("Connect timeout")));
+}
+
+ConnectSocket::ConnectSocket(EventLoop &_event_loop,
+                             ConnectSocketHandler &_handler)
+    :handler(_handler),
+     event(_event_loop, BIND_THIS_METHOD(OnEvent))
+{
+}
+
+ConnectSocket::~ConnectSocket()
+{
+    if (fd.IsDefined()) {
+        event.Delete();
+        fd.Close();
+    }
+}
+
+static UniqueSocketDescriptor
+Connect(const SocketAddress address)
+{
+    UniqueSocketDescriptor fd;
+    if (!fd.CreateNonBlock(address.GetFamily(), SOCK_STREAM, 0))
+        throw MakeErrno("Failed to create socket");
+
+    if (!fd.Connect(address) && errno != EINPROGRESS)
+        throw MakeErrno("Failed to connect");
+
+    return fd;
+}
+
+bool
+ConnectSocket::Connect(const SocketAddress address)
+{
+    assert(!fd.IsDefined());
+
+    try {
+        fd = ::Connect(address);
+    } catch (...) {
+        handler.OnSocketConnectError(std::current_exception());
+        return false;
+    }
+
+    event.Set(fd.Get(), SocketEvent::WRITE);
+    event.Add(connect_timeout);
+    return true;
+}
+
+void
+ConnectSocket::OnEvent(unsigned events)
+{
+    if (events & SocketEvent::TIMEOUT) {
+        handler.OnSocketConnectTimeout();
+        return;
+    }
+
+    int s_err = fd.GetError();
+    if (s_err != 0) {
+        handler.OnSocketConnectError(std::make_exception_ptr(MakeErrno(s_err, "Failed to connect")));
+        return;
+    }
+
+    handler.OnSocketConnectSuccess(std::exchange(fd,
+                                                 UniqueSocketDescriptor()));
+}
---
+++ src/net/ConnectSocket.hxx
@@ -0,0 +1,44 @@
+/*
+ * author: Max Kellermann <mk@cm4all.com>
+ */
+
+#ifndef CONNECT_SOCKET_HXX
+#define CONNECT_SOCKET_HXX
+
+#include "net/UniqueSocketDescriptor.hxx"
+#include "event/SocketEvent.hxx"
+
+#include <exception>
+
+class Error;
+class SocketAddress;
+
+class ConnectSocketHandler {
+public:
+    virtual void OnSocketConnectSuccess(UniqueSocketDescriptor &&fd) = 0;
+    virtual void OnSocketConnectTimeout();
+    virtual void OnSocketConnectError(std::exception_ptr ep) = 0;
+};
+
+/**
+ * A class that connects to a SocketAddress.
+ */
+class ConnectSocket {
+    ConnectSocketHandler &handler;
+
+    UniqueSocketDescriptor fd;
+
+    SocketEvent event;
+
+public:
+    explicit ConnectSocket(EventLoop &_event_loop,
+                           ConnectSocketHandler &_handler);
+    ~ConnectSocket();
+
+    bool Connect(SocketAddress address);
+
+private:
+    void OnEvent(unsigned events);
+};
+
+#endif
