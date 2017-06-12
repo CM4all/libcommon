65371e777c3560e1685bc3bacccb49a8dacfd7a4
Max Kellermann <mk@cm4all.com>
2017-06-12 13:04:19 +0200

    pg/Reflection move code from beng-proxy 7650a435c4d1c34f7d826b9dd72bf2e9e1962c99

---
+++ src/pg/Reflection.cxx
@@ -0,0 +1,36 @@
+/*
+ * author: Max Kellermann <mk@cm4all.com>
+ */
+
+#include "Reflection.hxx"
+#include "Connection.hxx"
+#include "CheckError.hxx"
+
+namespace Pg {
+
+bool
+ColumnExists(Pg::Connection &c, const char *schema,
+             const char *table_name, const char *column_name)
+{
+    assert(schema != nullptr);
+    assert(*schema != 0);
+
+    return CheckError(c.ExecuteParams("SELECT data_type FROM INFORMATION_SCHEMA.COLUMNS "
+                                      "WHERE table_schema=$1 AND table_name=$2 AND column_name=$3",
+                                      schema, table_name, column_name)).GetRowCount() > 0;
+
+}
+
+bool
+IndexExists(Pg::Connection &c, const char *schema,
+            const char *table_name, const char *index_name)
+{
+    assert(schema != nullptr);
+    assert(*schema != 0);
+
+    return CheckError(c.ExecuteParams("SELECT indexdef FROM pg_indexes "
+                                      "WHERE schemaname=$1 AND tablename=$2 AND indexname=$3",
+                                      schema, table_name, index_name)).GetRowCount() > 0;
+}
+
+}
---
+++ src/pg/Reflection.hxx
@@ -0,0 +1,34 @@
+/*
+ * author: Max Kellermann <mk@cm4all.com>
+ */
+
+#ifndef PG_REFLECTION_HXX
+#define PG_REFLECTION_HXX
+
+namespace Pg {
+
+class Connection;
+
+/**
+ * Does a column with the specified name exist in the table?
+ *
+ * @param schema the schema name (must not be nullptr or empty, as
+ * there is no fallback to "public")
+ */
+bool
+ColumnExists(Connection &c, const char *schema,
+             const char *table_name, const char *column_name);
+
+/**
+ * Does an index with the specified name exist in the table?
+ *
+ * @param schema the schema name (must not be nullptr or empty, as
+ * there is no fallback to "public")
+ */
+bool
+IndexExists(Connection &c, const char *schema,
+            const char *table_name, const char *index_name);
+
+}
+
+#endif
