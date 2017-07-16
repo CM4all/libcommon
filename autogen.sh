7a460b5693c071fe4eebb005534bdfeab2fbcf5b
Max Kellermann <mk@cm4all.com>
2017-07-16 21:05:02 +0200

    meson.build: move -Werror to autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
 rm -rf build
-exec meson . build -Dprefix=/usr/local/stow/libcm4all-common -Db_sanitize=address "$@"
+exec meson . build -Dprefix=/usr/local/stow/libcm4all-common --werror -Db_sanitize=address "$@"
