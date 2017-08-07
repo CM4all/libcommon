2a5c1bfcab154691d4c32099d03355aed01f66fc
Max Kellermann <mk@cm4all.com>
2017-08-07 09:25:32 +0200

    curl/Request: add method GetInfo()

--- src/curl/Request.cxx
+++ src/curl/Request.cxx
@@ -123,7 +123,7 @@ CurlRequest::FinishHeaders()
 	state = State::BODY;
 
 	long status = 0;
-	curl_easy_getinfo(easy.Get(), CURLINFO_RESPONSE_CODE, &status);
+	easy.GetInfo(CURLINFO_RESPONSE_CODE, &status);
 
 	handler.OnHeaders(status, std::move(headers));
 }
