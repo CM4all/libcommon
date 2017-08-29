113de45a13e06492818fa83174215f21ff748821
Max Kellermann <mk@cm4all.com>
2017-08-29 11:07:56 +0200

    io/Logger: indent with tabs

--- src/io/Logger.cxx
+++ src/io/Logger.cxx
@@ -41,59 +41,58 @@
 unsigned LoggerDetail::max_level = 1;
 
 LoggerDetail::ParamWrapper<std::exception_ptr>::ParamWrapper(std::exception_ptr ep)
-    :ParamWrapper<std::string>(GetFullMessage(ep)) {}
+	:ParamWrapper<std::string>(GetFullMessage(ep)) {}
 
 static constexpr struct iovec
 ToIovec(StringView s)
 {
-    return {const_cast<char *>(s.data), s.size};
+	return {const_cast<char *>(s.data), s.size};
 }
 
 gcc_pure
 static struct iovec
 ToIovec(const char *s)
 {
-    return ToIovec(StringView(s));
+	return ToIovec(StringView(s));
 }
 
 void
 LoggerDetail::WriteV(StringView domain,
-                     ConstBuffer<StringView> buffers) noexcept
+		     ConstBuffer<StringView> buffers) noexcept
 {
-    StaticArray<struct iovec, 64> v;
+	StaticArray<struct iovec, 64> v;
 
-    if (!domain.IsEmpty()) {
-        v.push_back(ToIovec("["));
-        v.push_back(ToIovec(domain));
-        v.push_back(ToIovec("] "));
-    }
+	if (!domain.IsEmpty()) {
+		v.push_back(ToIovec("["));
+		v.push_back(ToIovec(domain));
+		v.push_back(ToIovec("] "));
+	}
 
-    for (const auto i : buffers) {
-        if (v.size() >= v.capacity() - 1)
-            break;
+	for (const auto i : buffers) {
+		if (v.size() >= v.capacity() - 1)
+			break;
 
-        v.push_back(ToIovec(i));
-    }
+		v.push_back(ToIovec(i));
+	}
 
-    v.push_back(ToIovec("\n"));
+	v.push_back(ToIovec("\n"));
 
-    writev(STDERR_FILENO, v.raw(), v.size());
+	writev(STDERR_FILENO, v.raw(), v.size());
 }
 
 void
 LoggerDetail::Format(unsigned level, StringView domain,
-                     const char *fmt, ...) noexcept
+		     const char *fmt, ...) noexcept
 {
-    if (!CheckLevel(level))
-        return;
+	if (!CheckLevel(level))
+		return;
 
-    char buffer[2048];
+	char buffer[2048];
 
-    va_list ap;
-    va_start(ap, fmt);
-    StringView s(buffer, vsnprintf(buffer, sizeof(buffer), fmt, ap));
-    va_end(ap);
+	va_list ap;
+	va_start(ap, fmt);
+	StringView s(buffer, vsnprintf(buffer, sizeof(buffer), fmt, ap));
+	va_end(ap);
 
-    WriteV(domain, {&s, 1});
+	WriteV(domain, {&s, 1});
 }
-
--- src/io/Logger.hxx
+++ src/io/Logger.hxx
@@ -50,68 +50,68 @@ struct ParamWrapper;
 
 template<>
 struct ParamWrapper<StringView> {
-    StringView value;
+	StringView value;
 
-    constexpr explicit ParamWrapper(StringView _value):value(_value) {}
+	constexpr explicit ParamWrapper(StringView _value):value(_value) {}
 
-    constexpr StringView GetValue() const {
-        return value;
-    }
+	constexpr StringView GetValue() const {
+		return value;
+	}
 };
 
 template<>
 struct ParamWrapper<const char *> : ParamWrapper<StringView> {
-    explicit ParamWrapper(const char *_value)
-      :ParamWrapper<StringView>(_value) {}
+	explicit ParamWrapper(const char *_value)
+		:ParamWrapper<StringView>(_value) {}
 };
 
 template<>
 struct ParamWrapper<char *> : ParamWrapper<StringView> {
-    explicit ParamWrapper(char *_value)
-      :ParamWrapper<StringView>(_value) {}
+	explicit ParamWrapper(char *_value)
+		:ParamWrapper<StringView>(_value) {}
 };
 
 template<>
 struct ParamWrapper<std::string> {
-    std::string value;
+	std::string value;
 
-    template<typename S>
-    explicit ParamWrapper(S &&_value)
-        :value(std::forward<S>(_value)) {}
+	template<typename S>
+	explicit ParamWrapper(S &&_value)
+		:value(std::forward<S>(_value)) {}
 
-    gcc_pure
-    StringView GetValue() const {
-        return {value.data(), value.length()};
-    }
+	gcc_pure
+	StringView GetValue() const {
+		return {value.data(), value.length()};
+	}
 };
 
 template<>
 struct ParamWrapper<std::exception_ptr> : ParamWrapper<std::string> {
-    explicit ParamWrapper(std::exception_ptr ep);
+	explicit ParamWrapper(std::exception_ptr ep);
 };
 
 template<>
 struct ParamWrapper<int> {
-    char data[16];
-    size_t size;
+	char data[16];
+	size_t size;
 
-    ParamWrapper(int _value):size(sprintf(data, "%i", _value)) {}
+	ParamWrapper(int _value):size(sprintf(data, "%i", _value)) {}
 
-    StringView GetValue() const {
-        return {data, size};
-    }
+	StringView GetValue() const {
+		return {data, size};
+	}
 };
 
 template<>
 struct ParamWrapper<unsigned> {
-    char data[16];
-    size_t size;
+	char data[16];
+	size_t size;
 
-    ParamWrapper(int _value):size(sprintf(data, "%u", _value)) {}
+	ParamWrapper(int _value):size(sprintf(data, "%u", _value)) {}
 
-    StringView GetValue() const {
-        return {data, size};
-    }
+	StringView GetValue() const {
+		return {data, size};
+	}
 };
 
 template<typename... Params>
@@ -120,64 +120,64 @@ class ParamCollector;
 template<>
 class ParamCollector<> {
 public:
-    static constexpr size_t Count() {
-        return 0;
-    }
+	static constexpr size_t Count() {
+		return 0;
+	}
 
-    template<typename O>
-    size_t Fill(O) const noexcept {
-        return 0;
-    }
+	template<typename O>
+	size_t Fill(O) const noexcept {
+		return 0;
+	}
 };
 
 template<typename T>
 class ParamCollector<T> {
-    ParamWrapper<T> wrapper;
+	ParamWrapper<T> wrapper;
 
 public:
-    explicit ParamCollector(const T &t):wrapper(t) {}
+	explicit ParamCollector(const T &t):wrapper(t) {}
 
-    static constexpr size_t Count() {
-        return 1;
-    }
+	static constexpr size_t Count() {
+		return 1;
+	}
 
-    template<typename O>
-    O Fill(O output) const noexcept {
-        *output++ = wrapper.GetValue();
-        return output;
-    }
+	template<typename O>
+	O Fill(O output) const noexcept {
+		*output++ = wrapper.GetValue();
+		return output;
+	}
 };
 
 template<typename T, typename... Rest>
 class ParamCollector<T, Rest...> {
-    ParamCollector<T> first;
-    ParamCollector<Rest...> rest;
+	ParamCollector<T> first;
+	ParamCollector<Rest...> rest;
 
 public:
-    explicit ParamCollector(const T &t, Rest... _rest)
-        :first(t), rest(_rest...) {}
+	explicit ParamCollector(const T &t, Rest... _rest)
+		:first(t), rest(_rest...) {}
 
-    static constexpr size_t Count() {
-        return decltype(first)::Count() + decltype(rest)::Count();
-    }
+	static constexpr size_t Count() {
+		return decltype(first)::Count() + decltype(rest)::Count();
+	}
 
-    template<typename O>
-    O Fill(O output) const noexcept {
-        return rest.Fill(first.Fill(output));
-    }
+	template<typename O>
+	O Fill(O output) const noexcept {
+		return rest.Fill(first.Fill(output));
+	}
 };
 
 template<typename... Params>
 class ParamArray {
-    ParamCollector<Params...> collector;
+	ParamCollector<Params...> collector;
 
 public:
-    static constexpr size_t count = decltype(collector)::Count();
-    std::array<StringView, decltype(collector)::Count()> values;
+	static constexpr size_t count = decltype(collector)::Count();
+	std::array<StringView, decltype(collector)::Count()> values;
 
-    explicit ParamArray(Params... params):collector(params...) {
-        collector.Fill(&values.front());
-    }
+	explicit ParamArray(Params... params):collector(params...) {
+		collector.Fill(&values.front());
+	}
 };
 
 extern unsigned max_level;
@@ -185,7 +185,7 @@ extern unsigned max_level;
 inline bool
 CheckLevel(unsigned level)
 {
-    return level <= max_level;
+	return level <= max_level;
 }
 
 void
@@ -195,12 +195,12 @@ template<typename... Params>
 void
 LogConcat(unsigned level, StringView domain, Params... _params) noexcept
 {
-    if (!CheckLevel(level))
-        return;
+	if (!CheckLevel(level))
+		return;
 
-    const ParamArray<Params...> params(_params...);
-    WriteV(domain,
-           {&params.values.front(), params.values.size()});
+	const ParamArray<Params...> params(_params...);
+	WriteV(domain,
+	       {&params.values.front(), params.values.size()});
 }
 
 gcc_printf(3, 4)
@@ -212,90 +212,90 @@ Format(unsigned level, StringView domain, const char *fmt, ...) noexcept;
 inline void
 SetLogLevel(unsigned level)
 {
-    LoggerDetail::max_level = level;
+	LoggerDetail::max_level = level;
 }
 
 inline bool
 CheckLogLevel(unsigned level)
 {
-    return LoggerDetail::CheckLevel(level);
+	return LoggerDetail::CheckLevel(level);
 }
 
 template<typename D, typename... Params>
 void
 LogConcat(unsigned level, D &&domain, Params... params) noexcept
 {
-    LoggerDetail::LogConcat(level, std::forward<D>(domain),
-                            std::forward<Params>(params)...);
+	LoggerDetail::LogConcat(level, std::forward<D>(domain),
+				std::forward<Params>(params)...);
 }
 
 template<typename D, typename... Params>
 void
 LogFormat(unsigned level, D &&domain,
-          const char *fmt, Params... params) noexcept
+	  const char *fmt, Params... params) noexcept
 {
-    LoggerDetail::Format(level, std::forward<D>(domain),
-                         fmt, std::forward<Params>(params)...);
+	LoggerDetail::Format(level, std::forward<D>(domain),
+			     fmt, std::forward<Params>(params)...);
 }
 
 template<typename Domain>
 class BasicLogger : public Domain {
 public:
-    BasicLogger() = default;
+	BasicLogger() = default;
 
-    template<typename D>
-    explicit BasicLogger(D &&_domain)
-        :Domain(std::forward<D>(_domain)) {}
+	template<typename D>
+	explicit BasicLogger(D &&_domain)
+		:Domain(std::forward<D>(_domain)) {}
 
-    static bool CheckLevel(unsigned level) {
-        return LoggerDetail::CheckLevel(level);
-    }
+	static bool CheckLevel(unsigned level) {
+		return LoggerDetail::CheckLevel(level);
+	}
 
-    template<typename... Params>
-    void operator()(unsigned level, Params... params) const noexcept {
-        LoggerDetail::LogConcat(level, GetDomain(),
-                                std::forward<Params>(params)...);
-    }
+	template<typename... Params>
+	void operator()(unsigned level, Params... params) const noexcept {
+		LoggerDetail::LogConcat(level, GetDomain(),
+					std::forward<Params>(params)...);
+	}
 
-    template<typename... Params>
-    void Format(unsigned level,
-                const char *fmt, Params... params) const noexcept {
-        LoggerDetail::Format(level, GetDomain(),
-                             fmt, std::forward<Params>(params)...);
-    }
+	template<typename... Params>
+	void Format(unsigned level,
+		    const char *fmt, Params... params) const noexcept {
+		LoggerDetail::Format(level, GetDomain(),
+				     fmt, std::forward<Params>(params)...);
+	}
 
 private:
-    StringView GetDomain() const {
-        return Domain::GetDomain();
-    }
+	StringView GetDomain() const {
+		return Domain::GetDomain();
+	}
 
-    void WriteV(ConstBuffer<StringView> buffers) const noexcept {
-        LoggerDetail::WriteV(GetDomain(), buffers);
-    }
+	void WriteV(ConstBuffer<StringView> buffers) const noexcept {
+		LoggerDetail::WriteV(GetDomain(), buffers);
+	}
 };
 
 class StringLoggerDomain {
-    std::string name;
+	std::string name;
 
 public:
-    StringLoggerDomain() = default;
+	StringLoggerDomain() = default;
 
-    template<typename T>
-    explicit StringLoggerDomain(T &&_name)
-        :name(std::forward<T>(_name)) {}
+	template<typename T>
+	explicit StringLoggerDomain(T &&_name)
+		:name(std::forward<T>(_name)) {}
 
-    StringView GetDomain() const {
-        return {name.data(), name.length()};
-    }
+	StringView GetDomain() const {
+		return {name.data(), name.length()};
+	}
 };
 
 class Logger : public BasicLogger<StringLoggerDomain> {
 public:
-    Logger() = default;
+	Logger() = default;
 
-    template<typename D>
-    explicit Logger(D &&_domain)
-        :BasicLogger(std::forward<D>(_domain)) {}
+	template<typename D>
+	explicit Logger(D &&_domain)
+		:BasicLogger(std::forward<D>(_domain)) {}
 };
 
 /**
@@ -303,15 +303,15 @@ public:
  * string as its domain.
  */
 class LiteralLoggerDomain {
-    StringView domain;
+	StringView domain;
 
 public:
-    constexpr explicit LiteralLoggerDomain(StringView _domain=nullptr)
-        :domain(_domain) {}
+	constexpr explicit LiteralLoggerDomain(StringView _domain=nullptr)
+		:domain(_domain) {}
 
-    constexpr StringView GetDomain() const {
-        return domain;
-    }
+	constexpr StringView GetDomain() const {
+		return domain;
+	}
 };
 
 /**
@@ -320,38 +320,38 @@ public:
  */
 class LLogger : public BasicLogger<LiteralLoggerDomain> {
 public:
-    LLogger() = default;
+	LLogger() = default;
 
-    template<typename D>
-    explicit LLogger(D &&_domain)
-        :BasicLogger(std::forward<D>(_domain)) {}
+	template<typename D>
+	explicit LLogger(D &&_domain)
+		:BasicLogger(std::forward<D>(_domain)) {}
 };
 
 class LoggerDomainFactory {
 public:
-    virtual std::string MakeLoggerDomain() const noexcept = 0;
+	virtual std::string MakeLoggerDomain() const noexcept = 0;
 };
 
 class LazyLoggerDomain {
-    LoggerDomainFactory &factory;
+	LoggerDomainFactory &factory;
 
-    mutable std::string cache;
+	mutable std::string cache;
 
 public:
-    explicit LazyLoggerDomain(LoggerDomainFactory &_factory)
-        :factory(_factory) {}
+	explicit LazyLoggerDomain(LoggerDomainFactory &_factory)
+		:factory(_factory) {}
 
-    StringView GetDomain() const {
-        if (cache.empty())
-            cache = factory.MakeLoggerDomain();
-        return {cache.data(), cache.length()};
-    }
+	StringView GetDomain() const {
+		if (cache.empty())
+			cache = factory.MakeLoggerDomain();
+		return {cache.data(), cache.length()};
+	}
 };
 
 class LazyDomainLogger : public BasicLogger<LazyLoggerDomain> {
 public:
-    explicit LazyDomainLogger(LoggerDomainFactory &_factory)
-        :BasicLogger(_factory) {}
+	explicit LazyDomainLogger(LoggerDomainFactory &_factory)
+		:BasicLogger(_factory) {}
 };
 
 #endif
