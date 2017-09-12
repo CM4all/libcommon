02c2d26d931a9393e74b8448ef98df1accff783a
Max Kellermann <mk@cm4all.com>
2017-09-12 15:03:38 +0200

    util/PrintException: indent with tabs

--- src/util/PrintException.cxx
+++ src/util/PrintException.cxx
@@ -35,24 +35,24 @@
 void
 PrintException(const std::exception &e)
 {
-    fprintf(stderr, "%s\n", e.what());
-    try {
-        std::rethrow_if_nested(e);
-    } catch (const std::exception &nested) {
-        PrintException(nested);
-    } catch (...) {
-        fprintf(stderr, "Unrecognized nested exception\n");
-    }
+	fprintf(stderr, "%s\n", e.what());
+	try {
+		std::rethrow_if_nested(e);
+	} catch (const std::exception &nested) {
+		PrintException(nested);
+	} catch (...) {
+		fprintf(stderr, "Unrecognized nested exception\n");
+	}
 }
 
 void
 PrintException(const std::exception_ptr &ep)
 {
-    try {
-        std::rethrow_exception(ep);
-    } catch (const std::exception &e) {
-        PrintException(e);
-    } catch (...) {
-        fprintf(stderr, "Unrecognized exception\n");
-    }
+	try {
+		std::rethrow_exception(ep);
+	} catch (const std::exception &e) {
+		PrintException(e);
+	} catch (...) {
+		fprintf(stderr, "Unrecognized exception\n");
+	}
 }
