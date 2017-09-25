18dc0ccc4fb8479c705852174b53d13e0d1e0d8c
Max Kellermann <mk@cm4all.com>
2017-09-25 19:56:26 +0200

    ssl/Edit: indent with tabs

--- src/ssl/Edit.cxx
+++ src/ssl/Edit.cxx
@@ -40,26 +40,26 @@
 static UniqueX509_EXTENSION
 MakeExt(int nid, const char *value)
 {
-    UniqueX509_EXTENSION ext(X509V3_EXT_conf_nid(nullptr, nullptr, nid,
-                                                 const_cast<char *>(value)));
-    if (ext == nullptr)
-        throw SslError("X509V3_EXT_conf_nid() failed");
+	UniqueX509_EXTENSION ext(X509V3_EXT_conf_nid(nullptr, nullptr, nid,
+						     const_cast<char *>(value)));
+	if (ext == nullptr)
+		throw SslError("X509V3_EXT_conf_nid() failed");
 
-    return ext;
+	return ext;
 }
 
 void
 AddExt(X509 &cert, int nid, const char *value)
 {
-    X509_add_ext(&cert, MakeExt(nid, value).get(), -1);
+	X509_add_ext(&cert, MakeExt(nid, value).get(), -1);
 }
 
 void
 AddAltNames(X509_REQ &req, OpenSSL::GeneralNames gn)
 {
-    UniqueX509_EXTENSIONS sk(sk_X509_EXTENSION_new_null());
-    sk_X509_EXTENSION_push(sk.get(),
-                           X509V3_EXT_i2d(NID_subject_alt_name, 0, gn.get()));
+	UniqueX509_EXTENSIONS sk(sk_X509_EXTENSION_new_null());
+	sk_X509_EXTENSION_push(sk.get(),
+			       X509V3_EXT_i2d(NID_subject_alt_name, 0, gn.get()));
 
-    X509_REQ_add_extensions(&req, sk.get());
+	X509_REQ_add_extensions(&req, sk.get());
 }
