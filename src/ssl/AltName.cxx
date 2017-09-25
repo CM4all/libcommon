31c9bf37997073e6adcd144d04ae28dbae61d238
Max Kellermann <mk@cm4all.com>
2017-09-25 23:56:40 +0200

    ssl/AltName: indent with tabs

--- src/ssl/AltName.cxx
+++ src/ssl/AltName.cxx
@@ -36,35 +36,35 @@
 static void
 FillNameList(std::forward_list<std::string> &list, OpenSSL::GeneralNames src)
 {
-    for (const auto &name : src) {
-        if (name.GetType() == GEN_DNS) {
-            const auto dns_name = name.GetDnsName();
-            if (dns_name.IsNull())
-                continue;
+	for (const auto &name : src) {
+		if (name.GetType() == GEN_DNS) {
+			const auto dns_name = name.GetDnsName();
+			if (dns_name.IsNull())
+				continue;
 
-            list.emplace_front(dns_name.data, dns_name.size);
-        }
-    }
+			list.emplace_front(dns_name.data, dns_name.size);
+		}
+	}
 }
 
 gcc_pure
 std::forward_list<std::string>
 GetSubjectAltNames(X509 &cert)
 {
-    std::forward_list<std::string> list;
+	std::forward_list<std::string> list;
 
-    for (int i = -1;
-         (i = X509_get_ext_by_NID(&cert, NID_subject_alt_name, i)) >= 0;) {
-        auto ext = X509_get_ext(&cert, i);
-        if (ext == nullptr)
-            continue;
+	for (int i = -1;
+	     (i = X509_get_ext_by_NID(&cert, NID_subject_alt_name, i)) >= 0;) {
+		auto ext = X509_get_ext(&cert, i);
+		if (ext == nullptr)
+			continue;
 
-        OpenSSL::UniqueGeneralNames gn(reinterpret_cast<GENERAL_NAMES *>(X509V3_EXT_d2i(ext)));
-        if (!gn)
-            continue;
+		OpenSSL::UniqueGeneralNames gn(reinterpret_cast<GENERAL_NAMES *>(X509V3_EXT_d2i(ext)));
+		if (!gn)
+			continue;
 
-        FillNameList(list, gn);
-    }
+		FillNameList(list, gn);
+	}
 
-    return list;
+	return list;
 }
