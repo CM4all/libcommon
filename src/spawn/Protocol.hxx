7a9fcfbc9da463d4afcc119534671ab4f36b289f
Max Kellermann <mk@cm4all.com>
2018-05-04 11:46:27 +0200

    spawn/Protocol: revised protocol design
    To be able to add more data to namespace requests (e.g. the host name
    for UTS namespaces).

--- src/spawn/Protocol.hxx
+++ src/spawn/Protocol.hxx
@@ -42,10 +42,14 @@
   The Spawn daemon listens on a local seqpacket socket for commands.
 
   Each datagram begins with the 32 bit "magic", followed by a CRC32 of
-  all command packets, followed by one or more command packets
-  (currently, only one is allowed).  Each command packet begins with a
-  header and a variable-length payload.  The payloads are padded to
-  the next multiple of 4 bytes.
+  all command packets, followed by one or more command packets.
+
+  Each command packet begins with a header and a variable-length
+  payload.  The payloads are padded to the next multiple of 4 bytes.
+
+  These command packets belong together; they construct a larger
+  request; for example, the first command may specify the namespace
+  name, and the following packets specify the types of namespaces.
 
   All integers are native endian.  This protocol is designed for
   communication over local sockets (AF_LOCAL), and thus has no need
@@ -69,16 +73,27 @@ enum class RequestCommand : uint16_t {
     NOP = 0,
 
     /**
-     * Create a new namespace with the specified name (or look up the
-     * name in the table of existing managed namespaces).
-     *
-     * Payload is an "uint32_t" with one or more "CLONE_*" namespace
-     * flags, followed by a non-empty variable-length name.
+     * Set the name of namespaces requested by this datagram.  Payload
+     * is a non-empty variable-length name (7 bit ASCII, no null
+     * bytes).
+     */
+    NAME = 1,
+
+    /**
+     * Create a new IPC namespace.  No payload.
      *
      * Response may be #ResponseCommand::NAMESPACE_HANDLES or
      * #ResponseCommand::ERROR.
      */
-    MAKE_NAMESPACES = 1,
+    IPC_NAMESPACE = 2,
+
+    /**
+     * Create a new PID namespace.  No payload.
+     *
+     * Response may be #ResponseCommand::NAMESPACE_HANDLES or
+     * #ResponseCommand::ERROR.
+     */
+    PID_NAMESPACE = 3,
 };
 
 struct RequestHeader {
@@ -95,7 +110,7 @@ enum class ResponseCommand : uint16_t {
     ERROR = 0,
 
     /**
-     * Successful response to #RequestCommand::MAKE_NAMESPACES.
+     * Successful response to #RequestCommand::*_NAMESPACE.
      *
      * Payload is a list of "uint32_t" values, each of them denoting a
      * single "CLONE_*" flag.  The defines the order in which the
