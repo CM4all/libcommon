e4a6c7616ef1ca3acc90fa0cad74c8bcda0a8603
Max Kellermann <mk@cm4all.com>
2018-06-06 20:05:02 +0200

    fake/AllocatorPtr: indent with tabs

--- fake/AllocatorPtr.hxx
+++ fake/AllocatorPtr.hxx
@@ -43,138 +43,138 @@
 #include <string.h>
 
 class Allocator {
-    std::forward_list<std::function<void()>> cleanup;
+	std::forward_list<std::function<void()>> cleanup;
 
 public:
-    Allocator() = default;
-    Allocator(Allocator &&src) = default;
+	Allocator() = default;
+	Allocator(Allocator &&src) = default;
 
-    ~Allocator() {
-        for (auto &i : cleanup)
-            i();
-    }
+	~Allocator() {
+		for (auto &i : cleanup)
+			i();
+	}
 
-    Allocator &operator=(Allocator &&src) = delete;
+	Allocator &operator=(Allocator &&src) = delete;
 
-    void *Allocate(size_t size) {
-        void *p = malloc(size);
-        if (p == nullptr)
-            throw std::bad_alloc();
+	void *Allocate(size_t size) {
+		void *p = malloc(size);
+		if (p == nullptr)
+			throw std::bad_alloc();
 
-        cleanup.emplace_front([p](){ free(p); });
-        return p;
-    }
+		cleanup.emplace_front([p](){ free(p); });
+		return p;
+	}
 
-    char *Dup(const char *src) {
-        char *p = strdup(src);
-        if (p == nullptr)
-            throw std::bad_alloc();
+	char *Dup(const char *src) {
+		char *p = strdup(src);
+		if (p == nullptr)
+			throw std::bad_alloc();
 
-        cleanup.emplace_front([p](){ free(p); });
-        return p;
-    }
+		cleanup.emplace_front([p](){ free(p); });
+		return p;
+	}
 
-    const char *CheckDup(const char *src) {
-        return src != nullptr ? Dup(src) : nullptr;
-    }
+	const char *CheckDup(const char *src) {
+		return src != nullptr ? Dup(src) : nullptr;
+	}
 
-    template<typename... Args>
-    char *Concat(Args&&... args) {
-        const size_t length = ConcatLength(args...);
-        char *result = NewArray<char>(length + 1);
-        *ConcatCopy(result, args...) = 0;
-        return result;
-    }
+	template<typename... Args>
+	char *Concat(Args&&... args) {
+		const size_t length = ConcatLength(args...);
+		char *result = NewArray<char>(length + 1);
+		*ConcatCopy(result, args...) = 0;
+		return result;
+	}
 
-    template<typename T, typename... Args>
-    T *New(Args&&... args) {
-        auto p = new T(std::forward<Args>(args)...);
-        cleanup.emplace_front([p](){ delete p; });
-        return p;
-    }
+	template<typename T, typename... Args>
+	T *New(Args&&... args) {
+		auto p = new T(std::forward<Args>(args)...);
+		cleanup.emplace_front([p](){ delete p; });
+		return p;
+	}
 
-    template<typename T>
-    T *NewArray(size_t n) {
-        auto p = new T[n];
-        cleanup.emplace_front([p](){ delete[] p; });
-        return p;
-    }
+	template<typename T>
+	T *NewArray(size_t n) {
+		auto p = new T[n];
+		cleanup.emplace_front([p](){ delete[] p; });
+		return p;
+	}
 
 private:
-    template<typename... Args>
-    static size_t ConcatLength(const char *s, Args... args) {
-        return strlen(s) + ConcatLength(args...);
-    }
+	template<typename... Args>
+	static size_t ConcatLength(const char *s, Args... args) {
+		return strlen(s) + ConcatLength(args...);
+	}
 
-    template<typename... Args>
-    static constexpr size_t ConcatLength(StringView s, Args... args) {
-        return s.size + ConcatLength(args...);
-    }
+	template<typename... Args>
+	static constexpr size_t ConcatLength(StringView s, Args... args) {
+		return s.size + ConcatLength(args...);
+	}
 
-    static constexpr size_t ConcatLength() {
-        return 0;
-    }
+	static constexpr size_t ConcatLength() {
+		return 0;
+	}
 
-    template<typename... Args>
-    static char *ConcatCopy(char *p, const char *s, Args... args) {
-        return ConcatCopy(stpcpy(p, s), args...);
-    }
+	template<typename... Args>
+	static char *ConcatCopy(char *p, const char *s, Args... args) {
+		return ConcatCopy(stpcpy(p, s), args...);
+	}
 
-    template<typename... Args>
-    static char *ConcatCopy(char *p, StringView s, Args... args) {
-        return ConcatCopy((char *)mempcpy(p, s.data, s.size), args...);
-    }
+	template<typename... Args>
+	static char *ConcatCopy(char *p, StringView s, Args... args) {
+		return ConcatCopy((char *)mempcpy(p, s.data, s.size), args...);
+	}
 
-    template<typename... Args>
-    static char *ConcatCopy(char *p) {
-        return p;
-    }
+	template<typename... Args>
+	static char *ConcatCopy(char *p) {
+		return p;
+	}
 };
 
 class AllocatorPtr {
-    Allocator &allocator;
+	Allocator &allocator;
 
 public:
-    AllocatorPtr(Allocator &_allocator):allocator(_allocator) {}
+	AllocatorPtr(Allocator &_allocator):allocator(_allocator) {}
 
-    const char *Dup(const char *src) {
-        return allocator.Dup(src);
-    }
+	const char *Dup(const char *src) {
+		return allocator.Dup(src);
+	}
 
-    const char *CheckDup(const char *src) {
-        return allocator.CheckDup(src);
-    }
+	const char *CheckDup(const char *src) {
+		return allocator.CheckDup(src);
+	}
 
-    template<typename... Args>
-    char *Concat(Args&&... args) {
-        return allocator.Concat(std::forward<Args>(args)...);
-    }
+	template<typename... Args>
+	char *Concat(Args&&... args) {
+		return allocator.Concat(std::forward<Args>(args)...);
+	}
 
-    template<typename T, typename... Args>
-    T *New(Args&&... args) {
-        return allocator.New<T>(std::forward<Args>(args)...);
-    }
+	template<typename T, typename... Args>
+	T *New(Args&&... args) {
+		return allocator.New<T>(std::forward<Args>(args)...);
+	}
 
-    template<typename T>
-    T *NewArray(size_t n) {
-        return allocator.NewArray<T>(n);
-    }
+	template<typename T>
+	T *NewArray(size_t n) {
+		return allocator.NewArray<T>(n);
+	}
 
-    void *Dup(const void *data, size_t size) {
-        auto p = allocator.Allocate(size);
-        memcpy(p, data, size);
-        return p;
-    }
+	void *Dup(const void *data, size_t size) {
+		auto p = allocator.Allocate(size);
+		memcpy(p, data, size);
+		return p;
+	}
 
-    ConstBuffer<void> Dup(ConstBuffer<void> src);
+	ConstBuffer<void> Dup(ConstBuffer<void> src);
 
-    template<typename T>
-    ConstBuffer<T> Dup(ConstBuffer<T> src) {
-        return ConstBuffer<T>::FromVoid(Dup(src.ToVoid()));
-    }
+	template<typename T>
+	ConstBuffer<T> Dup(ConstBuffer<T> src) {
+		return ConstBuffer<T>::FromVoid(Dup(src.ToVoid()));
+	}
 
-    StringView Dup(StringView src);
-    const char *DupZ(StringView src);
+	StringView Dup(StringView src);
+	const char *DupZ(StringView src);
 };
 
 #endif
