69277b8216b0842cab61301ee58d4a58a1b5e3f1
Max Kellermann <mk@cm4all.com>
2018-10-19 17:59:38 +0200

    test/pg: indent with tabs

--- test/pg/TestDecodeArray.cxx
+++ test/pg/TestDecodeArray.cxx
@@ -38,48 +38,50 @@
 #include <stdio.h>
 #include <string.h>
 
-static void check_decode(const char *input, const char *const* expected) {
-    std::list<std::string> a = Pg::DecodeArray(input);
+static void
+check_decode(const char *input, const char *const* expected)
+{
+	std::list<std::string> a = Pg::DecodeArray(input);
 
-    unsigned i = 0;
-    for (const auto &v : a) {
-        if (expected[i] == NULL) {
-            fprintf(stderr, "decode '%s': too many elements in result ('%s')\n",
-                    input, v.c_str());
-            FAIL();
-        }
+	unsigned i = 0;
+	for (const auto &v : a) {
+		if (expected[i] == NULL) {
+			fprintf(stderr, "decode '%s': too many elements in result ('%s')\n",
+				input, v.c_str());
+			FAIL();
+		}
 
-        if (strcmp(v.c_str(), expected[i]) != 0) {
-            fprintf(stderr, "decode '%s': element %u differs: '%s', but '%s' expected\n",
-                    input, i, v.c_str(), expected[i]);
-            FAIL();
-        }
+		if (strcmp(v.c_str(), expected[i]) != 0) {
+			fprintf(stderr, "decode '%s': element %u differs: '%s', but '%s' expected\n",
+				input, i, v.c_str(), expected[i]);
+			FAIL();
+		}
 
-        ++i;
-    }
+		++i;
+	}
 
-    if (expected[a.size()] != NULL) {
-        fprintf(stderr, "decode '%s': not enough elements in result ('%s')\n",
-                input, expected[a.size()]);
-        FAIL();
-    }
+	if (expected[a.size()] != NULL) {
+		fprintf(stderr, "decode '%s': not enough elements in result ('%s')\n",
+			input, expected[a.size()]);
+		FAIL();
+	}
 }
 
 TEST(PgTest, DecodeArray)
 {
-    const char *zero[] = {NULL};
-    const char *empty[] = {"", NULL};
-    const char *one[] = {"foo", NULL};
-    const char *two[] = {"foo", "bar", NULL};
-    const char *three[] = {"foo", "", "bar", NULL};
-    const char *special[] = {"foo", "\"\\", NULL};
+	const char *zero[] = {NULL};
+	const char *empty[] = {"", NULL};
+	const char *one[] = {"foo", NULL};
+	const char *two[] = {"foo", "bar", NULL};
+	const char *three[] = {"foo", "", "bar", NULL};
+	const char *special[] = {"foo", "\"\\", NULL};
 
-    check_decode("{}", zero);
-    check_decode("{\"\"}", empty);
-    check_decode("{foo}", one);
-    check_decode("{\"foo\"}", one);
-    check_decode("{foo,bar}", two);
-    check_decode("{foo,\"bar\"}", two);
-    check_decode("{foo,,bar}", three);
-    check_decode("{foo,\"\\\"\\\\\"}", special);
+	check_decode("{}", zero);
+	check_decode("{\"\"}", empty);
+	check_decode("{foo}", one);
+	check_decode("{\"foo\"}", one);
+	check_decode("{foo,bar}", two);
+	check_decode("{foo,\"bar\"}", two);
+	check_decode("{foo,,bar}", three);
+	check_decode("{foo,\"\\\"\\\\\"}", special);
 }
--- test/pg/TestEncodeArray.cxx
+++ test/pg/TestEncodeArray.cxx
@@ -40,18 +40,18 @@
 
 TEST(PgTest, EncodeArray)
 {
-    std::list<std::string> a;
-    ASSERT_STREQ(Pg::EncodeArray(a).c_str(), "{}");
+	std::list<std::string> a;
+	ASSERT_STREQ(Pg::EncodeArray(a).c_str(), "{}");
 
-    a.push_back("foo");
-    ASSERT_STREQ(Pg::EncodeArray(a).c_str(), "{\"foo\"}");
+	a.push_back("foo");
+	ASSERT_STREQ(Pg::EncodeArray(a).c_str(), "{\"foo\"}");
 
-    a.push_back("");
-    ASSERT_STREQ(Pg::EncodeArray(a).c_str(), "{\"foo\",\"\"}");
+	a.push_back("");
+	ASSERT_STREQ(Pg::EncodeArray(a).c_str(), "{\"foo\",\"\"}");
 
-    a.push_back("\\");
-    ASSERT_STREQ(Pg::EncodeArray(a).c_str(), "{\"foo\",\"\",\"\\\\\"}");
+	a.push_back("\\");
+	ASSERT_STREQ(Pg::EncodeArray(a).c_str(), "{\"foo\",\"\",\"\\\\\"}");
 
-    a.push_back("\"");
-    ASSERT_STREQ(Pg::EncodeArray(a).c_str(), "{\"foo\",\"\",\"\\\\\",\"\\\"\"}");
+	a.push_back("\"");
+	ASSERT_STREQ(Pg::EncodeArray(a).c_str(), "{\"foo\",\"\",\"\\\\\",\"\\\"\"}");
 }
