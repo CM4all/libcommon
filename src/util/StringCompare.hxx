c35d17c5faf3b6607654915302f4408bf7222d36
Max Kellermann <mk@cm4all.com>
2019-01-04 00:00:44 +0100

    util/StringCompare: add IgnoreCase variants

--- src/util/StringCompare.hxx
+++ src/util/StringCompare.hxx
@@ -55,6 +55,10 @@ gcc_pure
 bool
 StringEndsWith(const char *haystack, const char *needle) noexcept;
 
+gcc_pure gcc_nonnull_all
+bool
+StringEndsWithIgnoreCase(const char *haystack, const char *needle) noexcept;
+
 /**
  * Returns the portion of the string after a prefix.  If the string
  * does not begin with the specified prefix, this function returns
@@ -65,6 +69,22 @@ static inline const char *
 StringAfterPrefix(const char *haystack, StringView needle) noexcept
 {
 	return StringStartsWith(haystack, needle)
+		? haystack + needle.size
+		: nullptr;
+}
+
+gcc_pure gcc_nonnull_all
+static inline bool
+StringStartsWithIgnoreCase(const char *haystack, StringView needle) noexcept
+{
+	return StringIsEqualIgnoreCase(haystack, needle.data, needle.size);
+}
+
+gcc_pure gcc_nonnull_all
+static inline const char *
+StringAfterPrefixIgnoreCase(const char *haystack, StringView needle) noexcept
+{
+	return StringStartsWithIgnoreCase(haystack, needle)
 		? haystack + needle.size
 		: nullptr;
 }
