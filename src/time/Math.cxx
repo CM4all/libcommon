83219ecc84f651d453c9c436995549f4b4e937fc
Max Kellermann <mk@cm4all.com>
2019-02-27 09:32:02 +0100

    time/Math: move from Workshop fc504c2e9f61f367efcd67f440ccd7a7b966404c

---
+++ src/time/Math.cxx
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2006-2018 Content Management AG
+ * All rights reserved.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "Math.hxx"
+#include "time/Calendar.hxx"
+
+#include <time.h>
+
+void
+IncrementDay(struct tm &tm)
+{
+	const unsigned max_day = DaysInMonth(tm.tm_mon + 1, tm.tm_year + 1900);
+
+	++tm.tm_mday;
+
+	if ((unsigned)tm.tm_mday > max_day) {
+		/* roll over to next month */
+		tm.tm_mday = 1;
+		++tm.tm_mon;
+		if (tm.tm_mon >= 12) {
+			/* roll over to next year */
+			tm.tm_mon = 0;
+			++tm.tm_year;
+		}
+	}
+
+	++tm.tm_wday;
+	if (tm.tm_wday >= 7)
+		tm.tm_wday = 0;
+}
