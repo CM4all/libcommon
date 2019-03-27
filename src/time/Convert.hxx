222885a40d30b07b3310291a5a1deff5232d8ec2
Max Kellermann <mk@cm4all.com>
2019-03-27 13:26:55 +0100

    time/Convert: add "pure" attributes

--- src/time/Convert.hxx
+++ src/time/Convert.hxx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007-2018 Content Management AG
+ * Copyright 2007-2019 Content Management AG
  * All rights reserved.
  *
  * author: Max Kellermann <mk@cm4all.com>
@@ -33,6 +33,8 @@
 #ifndef TIME_CONVERT_HXX
 #define TIME_CONVERT_HXX
 
+#include "util/Compiler.h"
+
 #include <chrono>
 
 /**
@@ -54,12 +56,14 @@ LocalTime(std::chrono::system_clock::time_point tp);
 /**
  * Convert a UTC-based "struct tm" to a UTC-based time point.
  */
+gcc_pure
 std::chrono::system_clock::time_point
 TimeGm(struct tm &tm) noexcept;
 
 /**
  * Convert a local "struct tm" to a UTC-based time point.
  */
+gcc_pure
 std::chrono::system_clock::time_point
 MakeTime(struct tm &tm) noexcept;
 
