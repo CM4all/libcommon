53674d2d3c3c76836daf059baf8efa58bd61b84b
Max Kellermann <mk@cm4all.com>
2019-05-08 16:23:08 +0200

    time/Convert: Windows portability

--- src/time/Convert.cxx
+++ src/time/Convert.cxx
@@ -40,7 +40,11 @@ struct tm
 GmTime(std::chrono::system_clock::time_point tp)
 {
 	const time_t t = std::chrono::system_clock::to_time_t(tp);
+#ifdef _WIN32
+	const struct tm *tm = gmtime(&t);
+#else
 	struct tm buffer, *tm = gmtime_r(&t, &buffer);
+#endif
 	if (tm == nullptr)
 		throw std::runtime_error("gmtime_r() failed");
 
@@ -51,7 +55,11 @@ struct tm
 LocalTime(std::chrono::system_clock::time_point tp)
 {
 	const time_t t = std::chrono::system_clock::to_time_t(tp);
+#ifdef _WIN32
+	const struct tm *tm = localtime(&t);
+#else
 	struct tm buffer, *tm = localtime_r(&t, &buffer);
+#endif
 	if (tm == nullptr)
 		throw std::runtime_error("localtime_r() failed");
 
