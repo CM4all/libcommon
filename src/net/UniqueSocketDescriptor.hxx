a939fa89e76c76eada90abb71e3b0339743576c0
Max Kellermann <mk@cm4all.com>
2019-08-26 20:32:02 +0200

    net/UniqueSocketDescriptor: fix copyright owner
    I wrote this class in 2012 for the XCSoar project.

--- src/net/UniqueSocketDescriptor.hxx
+++ src/net/UniqueSocketDescriptor.hxx
@@ -1,8 +1,5 @@
 /*
- * Copyright 2007-2019 Content Management AG
- * All rights reserved.
- *
- * author: Max Kellermann <mk@cm4all.com>
+ * Copyright 2012-2019 Max Kellermann <max.kellermann@gmail.com>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
