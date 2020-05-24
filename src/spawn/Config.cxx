ac9d17f3bdef7942bc225bc6c167667ceb72b282
Max Kellermann <mk@cm4all.com>
2020-05-24 11:30:30 +0200

    spawn/Config: un-inline VerifyUid(), VerifyGid()
    Reduce header dependencies.

--- src/spawn/Config.cxx
+++ src/spawn/Config.cxx
@@ -30,71 +30,19 @@
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#pragma once
+#include "Config.hxx"
+#include "util/RuntimeError.hxx"
 
-#include "UidGid.hxx"
-#include "util/Compiler.h"
+void
+SpawnConfig::VerifyUid(uid_t uid) const
+{
+	if (allowed_uids.find(uid) == allowed_uids.end())
+		throw FormatRuntimeError("uid %u is not allowed", unsigned(uid));
+}
 
-#ifdef HAVE_LIBSYSTEMD
-#include "Systemd.hxx"
-#endif
-
-#include <set>
-#include <string>
-
-/**
- * Configuration for the spawner.
- */
-struct SpawnConfig {
-#ifdef HAVE_LIBSYSTEMD
-	/**
-	 * If non-empty, then a new systemd scope is created for the
-	 * spawner process.
-	 */
-	std::string systemd_scope;
-
-	std::string systemd_scope_description;
-
-	SystemdUnitProperties systemd_scope_properties;
-
-	/**
-	 * If non-empty, then the new systemd scope is created in the
-	 * specified slice.
-	 */
-	std::string systemd_slice;
-#endif
-
-	UidGid default_uid_gid;
-
-	std::set<uid_t> allowed_uids;
-	std::set<gid_t> allowed_gids;
-
-	/**
-	 * Ignore #allowed_uids and #allowed_gids, and allow all uids/gids
-	 * (except for root:root)?  This is a kludge for the Workshop
-	 * project for backwards compatibility with version 1.
-	 */
-	bool allow_any_uid_gid = false;
-
-	void VerifyUid(uid_t uid) const;
-	void VerifyGid(gid_t gid) const;
-
-	template<typename I>
-	void VerifyGroups(I begin, I end) const {
-		for (I i = begin; i != end; ++i) {
-			if (*i == 0)
-				return;
-
-			VerifyGid(*i);
-		}
-	}
-
-	void Verify(const UidGid &uid_gid) const {
-		if (allow_any_uid_gid)
-			return;
-
-		VerifyUid(uid_gid.uid);
-		VerifyGid(uid_gid.gid);
-		VerifyGroups(uid_gid.groups.begin(), uid_gid.groups.end());
-	}
-};
+void
+SpawnConfig::VerifyGid(gid_t gid) const
+{
+	if (allowed_gids.find(gid) == allowed_gids.end())
+		throw FormatRuntimeError("gid %u is not allowed", unsigned(gid));
+}
