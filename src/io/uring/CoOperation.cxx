817acc689320bbe1570af2a1c6f4122514571b08
Max Kellermann <mk@cm4all.com>
2020-05-29 14:47:46 +0200

    io/uring/CoOperation: coroutine integration for io_uring

---
+++ src/io/uring/CoOperation.cxx
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2020 CM4all GmbH
+ * All rights reserved.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "CoOperation.hxx"
+#include "Queue.hxx"
+#include "system/Error.hxx"
+#include "io/UniqueFileDescriptor.hxx"
+
+#include <fcntl.h>
+
+namespace Uring {
+
+void
+CoOperationBase::OnUringCompletion(int res) noexcept
+{
+	value = res;
+	continuation.resume();
+}
+
+UniqueFileDescriptor
+CoOpenOperation::GetValue()
+{
+	if (value < 0)
+		throw MakeErrno(-value, "Failed to open file");
+
+	return UniqueFileDescriptor(std::exchange(value, -1));
+}
+
+static CoOpenOperation
+CoOpen(Queue &queue, FileDescriptor directory_fd, const char *path,
+       int flags, mode_t mode) noexcept
+{
+	auto *s = queue.GetSubmitEntry();
+	assert(s != nullptr); // TODO: what if the submit queue is full?
+
+	io_uring_prep_openat(s, directory_fd.Get(), path,
+			     flags|O_NOCTTY|O_CLOEXEC, mode);
+
+	CoOpenOperation op;
+	queue.Push(*s, op);
+	return op;
+}
+
+CoOpenOperation
+CoOpenReadOnly(Queue &queue, FileDescriptor directory_fd, const char *path) noexcept
+{
+	return CoOpen(queue, directory_fd, path, O_RDONLY, 0);
+}
+
+CoOpenOperation
+CoOpenReadOnly(Queue &queue, const char *path) noexcept
+{
+	return CoOpenReadOnly(queue, FileDescriptor(AT_FDCWD), path);
+}
+
+size_t
+CoReadOperation::GetValue() const
+{
+	if (value < 0)
+		throw MakeErrno(-value, "Failed to read");
+
+	return value;
+}
+
+CoReadOperation
+CoRead(Queue &queue, FileDescriptor fd, void *buffer, size_t size,
+       off_t offset) noexcept
+{
+	auto *s = queue.GetSubmitEntry();
+	assert(s != nullptr); // TODO: what if the submit queue is full?
+
+	io_uring_prep_read(s, fd.Get(), buffer, size, offset);
+
+	CoReadOperation op;
+	queue.Push(*s, op);
+	return op;
+}
+
+size_t
+CoWriteOperation::GetValue() const
+{
+	if (value < 0)
+		throw MakeErrno(-value, "Failed to write");
+
+	return value;
+}
+
+CoWriteOperation
+CoWrite(Queue &queue, FileDescriptor fd, const void *buffer, size_t size,
+	off_t offset) noexcept
+{
+	auto *s = queue.GetSubmitEntry();
+	assert(s != nullptr); // TODO: what if the submit queue is full?
+
+	io_uring_prep_write(s, fd.Get(), buffer, size, offset);
+
+	CoWriteOperation op;
+	queue.Push(*s, op);
+	return op;
+}
+
+} // namespace Uring
---
+++ src/io/uring/CoOperation.hxx
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2020 CM4all GmbH
+ * All rights reserved.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include "Operation.hxx"
+
+#include <coroutine>
+
+#include <sys/types.h>
+
+#ifndef __cpp_impl_coroutine
+#error Need -fcoroutines
+#endif
+
+class FileDescriptor;
+class UniqueFileDescriptor;
+
+namespace Uring {
+
+class Queue;
+
+/**
+ * Coroutine integration for an io_uring #Operation.
+ */
+class CoOperationBase : public Operation {
+public:
+	std::coroutine_handle<> continuation;
+
+protected:
+	int value;
+
+private:
+	/* virtual methods from class Uring::Operation */
+	void OnUringCompletion(int res) noexcept override;
+};
+
+template<typename Op>
+struct CoAwaitable final {
+	Op &op;
+
+	bool await_ready() const noexcept {
+		return !op.IsUringPending();
+	}
+
+	std::coroutine_handle<> await_suspend(std::coroutine_handle<> continuation) noexcept {
+		op.continuation = continuation;
+		return std::noop_coroutine();
+	}
+
+	decltype(auto) await_resume() const {
+		return op.GetValue();
+	}
+};
+
+class CoOpenOperation final : public CoOperationBase {
+public:
+	auto operator co_await() noexcept {
+		return CoAwaitable{*this};
+	}
+
+	UniqueFileDescriptor GetValue();
+};
+
+CoOpenOperation
+CoOpenReadOnly(Queue &queue,
+	       FileDescriptor directory_fd, const char *path) noexcept;
+
+CoOpenOperation
+CoOpenReadOnly(Queue &queue, const char *path) noexcept;
+
+class CoReadOperation final : public CoOperationBase {
+public:
+	auto operator co_await() noexcept {
+		return CoAwaitable{*this};
+	}
+
+	size_t GetValue() const;
+};
+
+CoReadOperation
+CoRead(Queue &queue, FileDescriptor fd, void *buffer, size_t size,
+       off_t offset) noexcept;
+
+class CoWriteOperation final : public CoOperationBase {
+public:
+	auto operator co_await() noexcept {
+		return CoAwaitable{*this};
+	}
+
+	size_t GetValue() const;
+};
+
+CoWriteOperation
+CoWrite(Queue &queue, FileDescriptor fd, const void *buffer, size_t size,
+	off_t offset) noexcept;
+
+} // namespace Uring
