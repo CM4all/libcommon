975aaa580521db647b90fc2121286df9cf4c3ccf
Max Kellermann <mk@cm4all.com>
2020-06-15 17:47:55 +0200

    sodium/SHA256: new class

---
+++ test/sodium/TestSHA256.cxx
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2007-2020 CM4all GmbH
+ * All rights reserved.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "sodium/SHA256.hxx"
+#include "util/HexFormat.h"
+#include "util/StringBuffer.hxx"
+
+#include <gtest/gtest.h>
+
+#include <array>
+#include <cstddef> // for std::byte
+
+template<std::size_t size>
+static auto
+HexFormat(const std::array<std::byte, size> &src) noexcept
+{
+	StringBuffer<size * 2 + 1> dest;
+	for (size_t i = 0; i < src.size(); ++i)
+		format_uint8_hex_fixed(&dest[i * 2], uint8_t(src[i]));
+	dest[size * 2] = 0;
+	return dest;
+}
+
+TEST(TestSHA256, Empty)
+{
+	SHA256State state;
+	const auto hash = HexFormat(state.Final());
+	EXPECT_STREQ(hash, "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
+}
