219ff76044ac9904551eb9063c113386100cf68d
Max Kellermann <mk@cm4all.com>
2020-06-15 17:29:34 +0200

    test/sodium: unit tests for our libsodium wrappers

---
+++ test/sodium/TestGenericHash.cxx
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2007-2020 CM4all GmbH
+ * All rights reserved.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "sodium/GenericHash.hxx"
+#include "util/HexFormat.h"
+#include "util/StringBuffer.hxx"
+
+#include <gtest/gtest.h>
+
+#include <array>
+#include <cstddef> // for std::byte
+
+template<std::size_t size>
+static auto
+HexFormat(const std::array<std::byte, size> &src) noexcept
+{
+	StringBuffer<size * 2 + 1> dest;
+	for (size_t i = 0; i < src.size(); ++i)
+		format_uint8_hex_fixed(&dest[i * 2], uint8_t(src[i]));
+	dest[size * 2] = 0;
+	return dest;
+}
+
+template<std::size_t bits>
+static void
+TGH(const char *expected)
+{
+	constexpr std::size_t size = bits / 8;
+	static_assert(size * 8 == bits);
+	GenericHashState state(size);
+	const auto hash = HexFormat(state.GetFinalT<std::array<std::byte, size>>());
+	EXPECT_STREQ(hash, expected);
+}
+
+TEST(TestGenericHash, Empty384)
+{
+	TGH<384>("b32811423377f52d7862286ee1a72ee540524380fda1724a6f25d7978c6fd3244a6caf0498812673c5e05ef583825100");
+}
+
+TEST(TestGenericHash, Empty512)
+{
+	TGH<512>("786a02f742015903c6c6fd852552d272912f4740e15847618a86e217f71f5419d25e1031afee585313896444934eb04b903a685b1448b755d56f701afe9be2ce");
+}
---
+++ test/sodium/TestSodium.cxx
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2007-2017 Content Management AG
+ * All rights reserved.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "io/ConfigParser.hxx"
+#include "io/FileLineParser.hxx"
+#include "util/ScopeExit.hxx"
+
+#include "util/Compiler.h"
+
+#include <gtest/gtest.h>
+
+#include <string.h>
+#include <stdlib.h>
+
+class MyConfigParser final
+    : public ConfigParser, public std::vector<std::string> {
+public:
+    void ParseLine(FileLineParser &line) override {
+        const char *value = line.NextUnescape();
+        if (value == nullptr)
+            throw LineParser::Error("Quoted value expected");
+        line.ExpectEnd();
+        emplace_back(value);
+    }
+};
+
+static void
+ParseConfigFile(ConfigParser &parser, const char *const*lines)
+{
+    while (*lines != nullptr) {
+        char *line = strdup(*lines++);
+        AtScopeExit(line) { free(line); };
+
+        FileLineParser line_parser({}, line);
+        if (!parser.PreParseLine(line_parser))
+            parser.ParseLine(line_parser);
+    }
+
+    parser.Finish();
+}
+
+static const char *const v_data[] = {
+    "@set foo='bar'",
+    "@set bar=\"${foo}\"",
+    "${foo} ",
+    "'${foo}'",
+    "\"${foo}\"",
+    "\"${bar}\"",
+    " \"a${foo}b\" ",
+    "@set foo=\"with space\"",
+    "\"${foo}\"",
+    "  ${foo}  ",
+    nullptr
+};
+
+static const char *const v_output[] = {
+    "bar",
+    "${foo}",
+    "bar",
+    "bar",
+    "abarb",
+    "with space",
+    "with space",
+    nullptr
+};
+
+TEST(ConfigParserTest, VariableConfigParser)
+{
+    MyConfigParser p;
+    VariableConfigParser v(p);
+
+    ParseConfigFile(v, v_data);
+
+    for (size_t i = 0; v_output[i] != nullptr; ++i) {
+        ASSERT_LT(i, p.size());
+        ASSERT_STREQ(v_output[i], p[i].c_str());
+    }
+}
