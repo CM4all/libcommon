CM4all/libcommon

commit 70ce3b440b9d145e17f608b5727dbefc4b288494

spawn: disable NO_NEW_PRIVS in libseccomp
Previously, there was an option to enable NO_NEW_PRIVS, but it was
effectively always enabled because libseccomp enables it.  This commit
restores the possibility to spawn processes without NO_NEW_PRIVS.

src/spawn/Direct.cxx
@@ -293,6 +293,14 @@ try {
 
 	try {
 		Seccomp::Filter sf(SCMP_ACT_ALLOW);
+
+#ifdef PR_SET_NO_NEW_PRIVS
+		/* don't enable PR_SET_NO_NEW_PRIVS unless the feature
+		   was explicitly enabled */
+		if (!p.no_new_privs)
+			sf.SetAttributeNoThrow(SCMP_FLTATR_CTL_NNP, 0);
+#endif
+
 		sf.AddSecondaryArchs();
 
 		BuildSyscallFilter(sf);

src/spawn/SeccompFilter.hxx
@@ -64,6 +64,10 @@ public:
 
 	void Load() const;
 
+	void SetAttributeNoThrow(enum scmp_filter_attr attr, uint32_t value) noexcept {
+		seccomp_attr_set(ctx, attr, value);
+	}
+
 	void AddArch(uint32_t arch_token);
 	void AddSecondaryArchs() noexcept;
 

commit 0e148d79b22cf301bbc935c360257250e6cd2d6e

spawn/Direct: install system call filter before dropping root privileges
Fixes regression caused by commit
70ce3b440b9d145e17f608b5727dbefc4b288494

The problem was that seccomp without root privileges requires
NO_NEW_PRIVS, so omitting NO_NEW_PRIVS breaks because we have dropped
root privileges alraedy.

src/spawn/Direct.cxx
@@ -245,11 +245,45 @@ try {
 			_exit(SpawnInit(pid, false));
 	}
 
-	if (!p.uid_gid.IsEmpty())
-		p.uid_gid.Apply();
-
 	if (p.no_new_privs)
 		prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0);
+
+	try {
+		Seccomp::Filter sf(SCMP_ACT_ALLOW);
+
+#ifdef PR_SET_NO_NEW_PRIVS
+		/* don't enable PR_SET_NO_NEW_PRIVS unless the feature
+		   was explicitly enabled */
+		if (!p.no_new_privs)
+			sf.SetAttributeNoThrow(SCMP_FLTATR_CTL_NNP, 0);
+#endif
+
+		sf.AddSecondaryArchs();
+
+		BuildSyscallFilter(sf);
+
+		if (p.forbid_user_ns)
+			ForbidUserNamespace(sf);
+
+		if (p.forbid_multicast)
+			ForbidMulticast(sf);
+
+		if (p.forbid_bind)
+			ForbidBind(sf);
+
+		sf.Load();
+	} catch (const std::runtime_error &e) {
+		if (p.HasSyscallFilter())
+			/* filter options have been explicitly enabled, and thus
+			   failure to set up the filter are fatal */
+			throw;
+
+		fprintf(stderr, "Failed to setup seccomp filter for '%s': %s\n",
+			path, e.what());
+	}
+
+	if (!p.uid_gid.IsEmpty())
+		p.uid_gid.Apply();
 
 	if (stderr_fd < 0 && p.stderr_path != nullptr) {
 		stderr_fd = open(p.stderr_path,
@@ -289,40 +323,6 @@ try {
 			perror("Failed to set the controlling terminal");
 			_exit(EXIT_FAILURE);
 		}
-	}
-
-	try {
-		Seccomp::Filter sf(SCMP_ACT_ALLOW);
-
-#ifdef PR_SET_NO_NEW_PRIVS
-		/* don't enable PR_SET_NO_NEW_PRIVS unless the feature
-		   was explicitly enabled */
-		if (!p.no_new_privs)
-			sf.SetAttributeNoThrow(SCMP_FLTATR_CTL_NNP, 0);
-#endif
-
-		sf.AddSecondaryArchs();
-
-		BuildSyscallFilter(sf);
-
-		if (p.forbid_user_ns)
-			ForbidUserNamespace(sf);
-
-		if (p.forbid_multicast)
-			ForbidMulticast(sf);
-
-		if (p.forbid_bind)
-			ForbidBind(sf);
-
-		sf.Load();
-	} catch (const std::runtime_error &e) {
-		if (p.HasSyscallFilter())
-			/* filter options have been explicitly enabled, and thus
-			   failure to set up the filter are fatal */
-			throw;
-
-		fprintf(stderr, "Failed to setup seccomp filter for '%s': %s\n",
-			path, e.what());
 	}
 
 	if (p.exec_function != nullptr) {