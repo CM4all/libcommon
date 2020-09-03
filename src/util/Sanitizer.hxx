0d325603ffc334a9d965e3ca7474474cb28e5eb2
Max Kellermann <mk@cm4all.com>
2020-09-03 13:46:03 +0200

    util/Sanitizer: add detection for clang

--- src/util/Sanitizer.hxx
+++ src/util/Sanitizer.hxx
@@ -35,9 +35,20 @@
 constexpr bool
 HaveAddressSanitizer() noexcept
 {
+#ifdef __clang__
+	/* clang provides __has_feature(address_sanitizer)
+	   (https://clang.llvm.org/docs/AddressSanitizer.html#conditional-compilation-with-has-feature-address-sanitizer) */
+	return __has_feature(address_sanitizer);
+
+#else
+
+	/* GCC defines __SANITIZE_ADDRESS__
+	   (https://gcc.gnu.org/onlinedocs/cpp/Common-Predefined-Macros.html) */
 #ifdef __SANITIZE_ADDRESS__
 	return true;
 #else
 	return false;
+#endif
+
 #endif
 }
