852c2bffdb9062d539d7b40b42cd9da58c21ef35
Max Kellermann <mk@cm4all.com>
2020-09-10 15:51:19 +0200

    io/uring/CancellableOperation: update Operation::cancellable in Replace()

--- src/io/uring/CancellableOperation.hxx
+++ src/io/uring/CancellableOperation.hxx
@@ -69,10 +69,12 @@ public:
 
 	void Replace(Operation &old_operation,
 		     Operation &new_operation) noexcept {
-		(void)old_operation;
 		assert(operation == &old_operation);
+		assert(old_operation.cancellable == this);
 
+		old_operation.cancellable = nullptr;
 		operation = &new_operation;
+		new_operation.cancellable = this;
 	}
 
 	void OnUringCompletion(int res) noexcept {
