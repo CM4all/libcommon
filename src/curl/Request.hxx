3f50367bd6ff8aa9e1b02ca6a22dd9a3ab54a35d
Max Kellermann <mk@cm4all.com>
2020-10-15 16:11:48 +0200

    curl/Request: add SetOption(), SetUrl()

--- src/curl/Request.hxx
+++ src/curl/Request.hxx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008-2018 Max Kellermann <max.kellermann@gmail.com>
+ * Copyright 2008-2020 Max Kellermann <max.kellermann@gmail.com>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -104,6 +104,15 @@ public:
 
 	CURL *Get() noexcept {
 		return easy.Get();
+	}
+
+	template<typename T>
+	void SetOption(CURLoption option, T value) {
+		easy.SetOption(option, value);
+	}
+
+	void SetUrl(const char *url) {
+		easy.SetURL(url);
 	}
 
 	/**
