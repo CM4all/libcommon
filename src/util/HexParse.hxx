9b62736df83017ac38b5e8f0ee9cf1b59f492f4a
Max Kellermann <mk@cm4all.com>
2020-11-17 20:04:51 +0100

    util/HexParse: hard-code the `constexpr` (C++14)

--- src/util/HexParse.hxx
+++ src/util/HexParse.hxx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007-2018 Content Management AG
+ * Copyright 2007-2020 CM4all GmbH
  * All rights reserved.
  *
  * author: Max Kellermann <mk@cm4all.com>
@@ -34,10 +34,7 @@
 
 #include "CharUtil.hxx"
 
-#if __cpp_constexpr >= 201304
-constexpr /* requires C++14 */
-#endif
-inline int
+constexpr int
 ParseHexDigit(char ch) noexcept
 {
 	if (IsDigitASCII(ch))
