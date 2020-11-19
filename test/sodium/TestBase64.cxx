81afd0f8513661208d22a2ac1e78db3f139885a1
Max Kellermann <mk@cm4all.com>
2020-11-19 18:57:22 +0100

    test/sodium/TestBase64: unit test for DecodeUrlSafeBase64()

--- test/sodium/TestBase64.cxx
+++ test/sodium/TestBase64.cxx
@@ -31,6 +31,7 @@
  */
 
 #include "sodium/Base64.hxx"
+#include "util/AllocatedArray.hxx"
 #include "util/AllocatedString.hxx"
 #include "util/StringView.hxx"
 
@@ -60,4 +61,11 @@ TEST(TestBase64, UrlSafeBase64)
 {
 	const auto b64 = UrlSafeBase64(leviathan);
 	EXPECT_STREQ(b64.c_str(), "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4");
+}
+
+TEST(TestBase64, DecodeUrlSafeBase64)
+{
+	const auto d = DecodeUrlSafeBase64("TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4");
+	EXPECT_EQ(std::string_view(leviathan),
+		  std::string_view((const char *)d.data(), d.size()));
 }
