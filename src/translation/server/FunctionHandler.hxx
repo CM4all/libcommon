27a27a8638f1234836ba834de1027b08026ff67d
Max Kellermann <mk@cm4all.com>
2020-12-10 19:56:48 +0100

    translation/server/FunctionHandler: new class

---
+++ src/translation/server/FunctionHandler.hxx
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2020 CM4all GmbH
+ * All rights reserved.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include "Handler.hxx"
+
+#include <functional>
+
+namespace Translation::Server {
+
+class Response;
+
+using HandlerFunction = std::function<Response(const Request &)>;
+
+/**
+ * An implementation of #Handler which calls a std::function.
+ */
+class FunctionHandler : public Handler {
+	HandlerFunction function;
+
+public:
+	template<typename F>
+	explicit FunctionHandler(F &&_function) noexcept
+		:function(std::forward<F>(_function)) {}
+
+	/* virtual methods from class Translation::Server::Handler */
+	bool OnTranslationRequest(Connection &connection,
+				  const Request &request,
+				  CancellablePointer &cancel_ptr) noexcept final;
+};
+
+} // namespace Translation::Server
