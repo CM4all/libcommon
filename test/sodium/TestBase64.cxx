4352d1cd7b4292d87d2dfeb93b1d1e20ba71a3c0
Max Kellermann <mk@cm4all.com>
2021-03-17 18:21:56 +0100

    test/sodium/TestBase64: add sodium_base64_VARIANT_ORIGINAL test

--- test/sodium/TestBase64.cxx
+++ test/sodium/TestBase64.cxx
@@ -45,16 +45,22 @@ TEST(TestBase64, Empty)
 
 static constexpr char leviathan[] = "Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure.";
 
-TEST(TestBase64, Leviathan)
+TEST(TestBase64, FixedLeviathan)
 {
 	const auto b64 = FixedBase64<sizeof(leviathan) - 1, sodium_base64_VARIANT_ORIGINAL>(leviathan);
 	EXPECT_STREQ(b64, "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=");
 }
 
-TEST(TestBase64, LeviathanNoPadding)
+TEST(TestBase64, FixedLeviathanNoPadding)
 {
 	const auto b64 = FixedBase64<sizeof(leviathan) - 1, sodium_base64_VARIANT_ORIGINAL_NO_PADDING>(leviathan);
 	EXPECT_STREQ(b64, "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4");
+}
+
+TEST(TestBase64, Leviathan)
+{
+	const auto b64 = SodiumBase64(leviathan);
+	EXPECT_STREQ(b64.c_str(), "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=");
 }
 
 TEST(TestBase64, UrlSafeBase64)
