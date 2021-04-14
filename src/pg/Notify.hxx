73c2695f29a5dcccb6bcfd6bafdf26f81402589c
Max Kellermann <mk@cm4all.com>
2021-04-14 14:24:17 +0200

    pg/Notify: add `noexcept`

--- src/pg/Notify.hxx
+++ src/pg/Notify.hxx
@@ -45,34 +45,39 @@ class Notify {
 	PGnotify *notify;
 
 public:
-	Notify():notify(nullptr) {}
-	explicit Notify(PGnotify *_notify):notify(_notify) {}
+	Notify() noexcept:notify(nullptr) {}
+	explicit Notify(PGnotify *_notify) noexcept
+		:notify(_notify) {}
 
 	Notify(const Notify &other) = delete;
-	Notify(Notify &&other):notify(other.notify) {
+
+	Notify(Notify &&other) noexcept
+		:notify(other.notify)
+	{
 		other.notify = nullptr;
 	}
 
-	~Notify() {
+	~Notify() noexcept {
 		if (notify != nullptr)
 			PQfreemem(notify);
 	}
 
-	operator bool() const {
+	operator bool() const noexcept {
 		return notify != nullptr;
 	}
 
 	Notify &operator=(const Notify &other) = delete;
-	Notify &operator=(Notify &&other) {
+
+	Notify &operator=(Notify &&other) noexcept {
 		std::swap(notify, other.notify);
 		return *this;
 	}
 
-	const PGnotify &operator*() const {
+	const PGnotify &operator*() const noexcept {
 		return *notify;
 	}
 
-	const PGnotify *operator->() const {
+	const PGnotify *operator->() const noexcept {
 		return notify;
 	}
 };
