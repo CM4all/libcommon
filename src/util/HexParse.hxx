2a658bcb78f0252b38ee1da70db3b8f9c6fcea4d
Max Kellermann <mk@cm4all.com>
2021-07-19 16:25:23 +0200

    util/HexParse: add ParseLowerHexFixed()

--- src/util/HexParse.hxx
+++ src/util/HexParse.hxx
@@ -34,6 +34,10 @@
 
 #include "CharUtil.hxx"
 
+#include <array>
+#include <string_view>
+#include <type_traits>
+
 constexpr int
 ParseHexDigit(char ch) noexcept
 {
@@ -56,4 +60,64 @@ ParseLowerHexDigit(char ch) noexcept
 		return ch - 'a' + 0xa;
 	else
 		return -1;
+}
+
+/**
+ * Parse the hex digits of a fixed-length string to the given integer
+ * reference.
+ *
+ * @return the end of the parsed string on success, nullptr on error
+ */
+template<typename T,
+	 typename=std::enable_if_t<std::is_integral_v<T> &&
+				   std::is_unsigned_v<T>>>
+const char *
+ParseLowerHexFixed(const char *input, T &output) noexcept
+{
+	T value{};
+
+	for (std::size_t j = 0; j < sizeof(T) * 2; ++j) {
+		int digit = ParseLowerHexDigit(*input++);
+		if (digit < 0)
+			return nullptr;
+
+		value = (value << 4) | digit;
+	}
+
+	output = value;
+	return input;
+}
+
+inline const char *
+ParseLowerHexFixed(const char *input, std::byte &_output) noexcept
+{
+	auto &output = (std::uint8_t &)_output;
+	return ParseLowerHexFixed(input, output);
+}
+
+/**
+ * Parse the hex digits of a fixed-length string to the given integer
+ * array.
+ *
+ * @return the end of the parsed string on success, nullptr on error
+ */
+template<typename T, std::size_t size>
+const char *
+ParseLowerHexFixed(const char *input, std::array<T, size> &output) noexcept
+{
+	for (auto &i : output) {
+		input = ParseLowerHexFixed(input, i);
+		if (input == nullptr)
+			return nullptr;
+	}
+
+	return input;
+}
+
+template<typename T>
+bool
+ParseLowerHexFixed(std::string_view input, T &output) noexcept
+{
+	return input.size() == sizeof(output) * 2 &&
+		ParseLowerHexFixed(input.data(), output) != nullptr;
 }
