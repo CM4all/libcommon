04c20241bd7d8310d0ffd48a72c7967eb8f41d92
Max Kellermann <mk@cm4all.com>
2021-08-12 10:27:57 +0200

    ssl/SHA3: move span<byte> prototype before the template overload
    When the prototype is not yet known, the template keeps calling
    itself, leading to a stack overflow.

--- src/ssl/SHA3.hxx
+++ src/ssl/SHA3.hxx
@@ -36,15 +36,15 @@
 #include <span>
 #include <string_view>
 
+std::array<char, 64>
+EvpSHA3_256_Hex(std::span<const std::byte> input);
+
 template<typename T>
 std::array<char, 64>
 EvpSHA3_256_Hex(std::span<T> input)
 {
 	return EvpSHA3_256_Hex(std::as_bytes(input));
 }
-
-std::array<char, 64>
-EvpSHA3_256_Hex(std::span<const std::byte> input);
 
 inline std::array<char, 64>
 EvpSHA3_256_Hex(std::string_view input)
