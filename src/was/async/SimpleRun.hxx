1e80447a567cd2a90e6559e2ee65e10f81efbbc4
Max Kellermann <mk@cm4all.com>
2021-09-16 13:34:01 +0200

    was/async/SimpleRun: add API documentation

--- src/was/async/SimpleRun.hxx
+++ src/was/async/SimpleRun.hxx
@@ -38,6 +38,23 @@ namespace Was {
 
 class SimpleRequestHandler;
 
+/**
+ * Accept incoming WAS requests using the given #EventLoop and let the
+ * given #SimpleRequestHandler handle them.
+ *
+ * This function auto-detects how this process was launched:
+ *
+ * - classic WAS (single WAS connection on fds 0,1,3)
+ * - Multi-WAS (socket on fd=0)
+ *
+ * This function does not return until the client closes the initial
+ * connection.
+ *
+ * @param event_loop the #EventLoop which is used to dispatch all
+ * events
+ * @param request_handler this class handles all requests
+ * asynchronously
+ */
 void
 Run(EventLoop &event_loop, SimpleRequestHandler &request_handler);
 
