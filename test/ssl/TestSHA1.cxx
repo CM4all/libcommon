311056b45843441cd6df554e3314583d782fc62d
Max Kellermann <mk@cm4all.com>
2021-10-01 17:03:18 +0200

    test/ssl/TestSHA1: new unit test

---
+++ test/ssl/TestSHA1.cxx
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2019-2021 CM4all GmbH
+ * All rights reserved.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "ssl/SHA1.hxx"
+
+#include <gtest/gtest.h>
+
+TEST(TestSHA1, StringView)
+{
+	auto hex = EvpSHA1_Hex("");
+	EXPECT_EQ(std::string_view(hex.data(), hex.size()),
+		  "da39a3ee5e6b4b0d3255bfef95601890afd80709");
+
+	hex = EvpSHA1_Hex("The quick brown fox jumps over the lazy dog");
+	EXPECT_EQ(std::string_view(hex.data(), hex.size()),
+		  "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12");
+}
