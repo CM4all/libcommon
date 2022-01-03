821e9d80be5be3397f7cf1730c11729ddbea390b
Max Kellermann <mk@cm4all.com>
2022-01-03 15:22:58 +0100

    time/Convert: move GetTimeZoneOffset() to Zone.cxx

--- src/time/Zone.cxx
+++ src/time/Zone.cxx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007-2021 CM4all GmbH
+ * Copyright 2007-2022 CM4all GmbH
  * All rights reserved.
  *
  * author: Max Kellermann <mk@cm4all.com>
@@ -30,67 +30,21 @@
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "Convert.hxx"
 #include "Zone.hxx"
 
-#include <stdexcept>
-
 #include <time.h>
-#include <sys/time.h> /* for struct timeval */
 
-struct tm
-GmTime(std::chrono::system_clock::time_point tp)
+int
+GetTimeZoneOffset() noexcept
 {
-	const time_t t = std::chrono::system_clock::to_time_t(tp);
+	time_t t = 1234567890;
 #ifdef _WIN32
-	const struct tm *tm = gmtime(&t);
+	struct tm *p = gmtime(&t);
 #else
-	struct tm buffer, *tm = gmtime_r(&t, &buffer);
-#endif
-	if (tm == nullptr)
-		throw std::runtime_error("gmtime_r() failed");
-
-	return *tm;
-}
-
-struct tm
-LocalTime(std::chrono::system_clock::time_point tp)
-{
-	const time_t t = std::chrono::system_clock::to_time_t(tp);
-#ifdef _WIN32
-	const struct tm *tm = localtime(&t);
-#else
-	struct tm buffer, *tm = localtime_r(&t, &buffer);
-#endif
-	if (tm == nullptr)
-		throw std::runtime_error("localtime_r() failed");
-
-	return *tm;
-}
-
-std::chrono::system_clock::time_point
-TimeGm(struct tm &tm) noexcept
-{
-#ifdef __GLIBC__
-	/* timegm() is a GNU extension */
-	const auto t = timegm(&tm);
-#else
+	struct tm tm;
 	tm.tm_isdst = 0;
-	const auto t = mktime(&tm) + GetTimeZoneOffset();
-#endif /* !__GLIBC__ */
-
-	return std::chrono::system_clock::from_time_t(t);
-}
-
-std::chrono::system_clock::time_point
-MakeTime(struct tm &tm) noexcept
-{
-	return std::chrono::system_clock::from_time_t(mktime(&tm));
-}
-
-std::chrono::steady_clock::duration
-ToSteadyClockDuration(const struct timeval &tv) noexcept
-{
-	return std::chrono::steady_clock::duration(std::chrono::seconds(tv.tv_sec)) +
-		std::chrono::steady_clock::duration(std::chrono::microseconds(tv.tv_usec));
+	struct tm *p = &tm;
+	gmtime_r(&t, p);
+#endif
+	return t - mktime(p);
 }
