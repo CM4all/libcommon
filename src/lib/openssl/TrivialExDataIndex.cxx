068753a78fdf326709335c5eda4bce17da8adc2c
Max Kellermann <mk@cm4all.com>
2022-02-01 17:42:35 +0100

    lib/openssl/TrivialExDataIndex: new class

---
+++ src/lib/openssl/TrivialExDataIndex.cxx
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2022 CM4all GmbH
+ * All rights reserved.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "TrivialExDataIndex.hxx"
+#include "Error.hxx"
+
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+namespace OpenSSL {
+
+TrivialExDataIndex::TrivialExDataIndex()
+	:idx(SSL_get_ex_new_index(0, nullptr, nullptr, nullptr, nullptr))
+{
+	if (idx < 0)
+		throw SslError("SSL_get_ex_new_index() failed");
+}
+
+void
+TrivialExDataIndex::Set(SSL &ssl, void *value) const noexcept
+{
+	SSL_set_ex_data(&ssl, idx, value);
+}
+
+void *
+TrivialExDataIndex::Get(SSL &ssl) const noexcept
+{
+	return SSL_get_ex_data(&ssl, idx);
+}
+
+} // namespace OpenSSL
