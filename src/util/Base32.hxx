3d4efd073fef31cb6692b88b3f389f6048f4805b
Max Kellermann <mk@cm4all.com>
2022-02-18 10:43:17 +0100

    util/Base32: new library

---
+++ src/util/Base32.hxx
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2022 CM4all GmbH
+ * All rights reserved.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include <type_traits>
+
+constexpr char base32_digits[] = "0123456789abcdefghijklmnopqrstuv";
+
+static_assert(sizeof(base32_digits) == 33);
+
+/**
+ * Convert an integer to base32.  Both the alphabet and the order is
+ * arbitrary, optimized to be fast, but reproducible.
+ *
+ * The buffer must be large enough to hold the formatted string.  This
+ * function will not null-terminate it.
+ *
+ * @return a pointer to one after the last character
+ */
+template<typename I>
+constexpr char *
+FormatIntBase32(char *buffer, I _value) noexcept
+{
+	static_assert(std::is_integral_v<I>);
+
+	using U = std::make_unsigned_t<I>;
+	U value(_value);
+
+	do {
+		*buffer++ = base32_digits[value & 0x1f];
+		value >>= 5;
+	} while (value);
+
+	return buffer;
+}
