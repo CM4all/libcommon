CM4all/libcommon

commit c9b186776c1998c9d954e71995df921115f4cbac

net/MultiReceiveMessage: use class AllocatedArray

src/net/MultiReceiveMessage.cxx
@@ -43,14 +43,11 @@ MultiReceiveMessage::MultiReceiveMessage(size_t _allocated_datagrams,
 					 size_t _max_fds)
 	:allocated_datagrams(_allocated_datagrams),
 	 max_payload_size(_max_payload_size), max_cmsg_size(_max_cmsg_size),
-	 max_fds(_max_fds),
 	 buffer(allocated_datagrams * (max_payload_size + max_cmsg_size
 				       + sizeof(struct mmsghdr)
 				       + sizeof(struct sockaddr_storage)
 				       + sizeof(struct iovec))),
-	 fds(max_fds > 0
-	     ? new UniqueFileDescriptor[allocated_datagrams * max_fds]
-	     : nullptr),
+	 fds(_max_fds),
 	 datagrams(new Datagram[allocated_datagrams])
 {
 	auto *m = GetMmsg();
@@ -125,7 +122,7 @@ MultiReceiveMessage::Receive(SocketDescriptor s)
 
 				for (unsigned ii = 0; ii < nn; ++ii) {
 					FileDescriptor fd(f[ii]);
-					if (d.fds.size < max_fds) {
+					if (d.fds.size < fds.size()) {
 						fds[n_fds++] = UniqueFileDescriptor(fd);
 						++d.fds.size;
 					} else

src/net/MultiReceiveMessage.hxx
@@ -35,6 +35,7 @@
 #include "SocketAddress.hxx"
 #include "system/LargeAllocation.hxx"
 #include "io/UniqueFileDescriptor.hxx"
+#include "util/AllocatedArray.hxx"
 #include "util/OffsetPointer.hxx"
 #include "util/WritableBuffer.hxx"
 
@@ -49,12 +50,12 @@ class SocketDescriptor;
  */
 class MultiReceiveMessage {
 	const size_t allocated_datagrams;
-	const size_t max_payload_size, max_cmsg_size, max_fds;
+	const size_t max_payload_size, max_cmsg_size;
 	size_t n_datagrams = 0;
 
 	LargeAllocation buffer;
 
-	std::unique_ptr<UniqueFileDescriptor[]> fds;
+	AllocatedArray<UniqueFileDescriptor> fds;
 	size_t n_fds = 0;
 
 public:

commit 8a515bfa969b0d353ebdb61048e21edf9c8b81e0

net/MultiReceiveMessage: use AllocatedArray::data() instead of operator[]
Fixes assertion failure (regression by commit c9b186776c1).

src/net/MultiReceiveMessage.cxx
@@ -95,6 +95,9 @@ MultiReceiveMessage::Receive(SocketDescriptor s)
 
 	n_datagrams = result;
 
+	auto *fds_p = fds.data();
+	std::size_t remaining_fds = fds.size();
+
 	for (size_t i = 0; i < n_datagrams; ++i) {
 		auto &mh = m[i].msg_hdr;
 		auto &d = datagrams[i];
@@ -102,7 +105,7 @@ MultiReceiveMessage::Receive(SocketDescriptor s)
 					  mh.msg_namelen);
 		d.payload = {GetPayload(i), m[i].msg_len};
 		d.cred = nullptr;
-		d.fds.data = &fds[n_fds];
+		d.fds.data = fds_p;
 		d.fds.size = 0;
 
 #ifdef __clang__
@@ -122,8 +125,9 @@ MultiReceiveMessage::Receive(SocketDescriptor s)
 
 				for (unsigned ii = 0; ii < nn; ++ii) {
 					FileDescriptor fd(f[ii]);
-					if (n_fds < fds.size()) {
-						fds[n_fds++] = UniqueFileDescriptor(fd);
+					if (remaining_fds > 0) {
+						*fds_p++ = UniqueFileDescriptor(fd);
+						--remaining_fds;
 						++d.fds.size;
 					} else
 						fd.Close();