CM4all/libcommon

commit 0b3294f01f366c66f4c73085527b1c32d796e3e9

spawn/Direct: use CLONE_INTO_CGROUP

src/spawn/Direct.cxx
@@ -381,14 +381,6 @@ SpawnChildProcess(PreparedChildProcess &&params,
 	uint_least64_t clone_flags = CLONE_CLEAR_SIGHAND|CLONE_PIDFD;
 	clone_flags = params.ns.GetCloneFlags(clone_flags);
 
-	if (params.cgroup != nullptr && params.cgroup->IsDefined())
-		/* postpone creating the new cgroup namespace until
-		   after this process has been moved to the new
-		   cgroup, or else it won't have the required
-		   permissions to do so, because the destination
-		   cgroup won't be visible from his namespace */
-		clone_flags &= ~CLONE_NEWCGROUP;
-
 	const char *path = params.Finish();
 
 	/**
@@ -491,6 +483,24 @@ SpawnChildProcess(PreparedChildProcess &&params,
 	ca.flags = clone_flags;
 	ca.pidfd = (intptr_t)&_pidfd;
 	ca.exit_signal = SIGCHLD;
+
+	UniqueFileDescriptor cgroup_fd;
+	if (params.cgroup != nullptr) {
+		cgroup_fd = params.cgroup->Create2(cgroup_state);
+		if (cgroup_fd.IsDefined()) {
+			ca.flags |= CLONE_INTO_CGROUP;
+			ca.cgroup = cgroup_fd.Get();
+			params.cgroup = nullptr;
+		}
+	}
+
+	if (params.cgroup != nullptr && params.cgroup->IsDefined())
+		/* postpone creating the new cgroup namespace until
+		   after this process has been moved to the new
+		   cgroup, or else it won't have the required
+		   permissions to do so, because the destination
+		   cgroup won't be visible from his namespace */
+		ca.flags &= ~CLONE_NEWCGROUP;
 
 	long pid = clone3(&ca, sizeof(ca));
 	if (pid < 0)

src/spawn/CgroupOptions.cxx
@@ -129,6 +129,31 @@ MkdirOpenPath(const FileDescriptor parent_fd,
 	return OpenPath(parent_fd, name);
 }
 
+UniqueFileDescriptor
+CgroupOptions::Create2(const CgroupState &state) const
+{
+	if (name == nullptr)
+		return {};
+
+	if (!state.IsEnabled())
+		throw std::runtime_error("Control groups are disabled");
+
+	if (!state.IsV2())
+		return {};
+
+	assert(state.memory_v2);
+
+	auto fd = MakeCgroup(state.mounts.front().fd, name);
+
+	for (const auto &s : set)
+		WriteCgroupFile(state, fd, s.name, s.value);
+
+	if (session != nullptr)
+		return MkdirOpenPath(fd, session);
+	else
+		return fd;
+}
+
 static UniqueFileDescriptor
 MoveToNewCgroup(const FileDescriptor group_fd,
 		const char *sub_group, const char *session_group,
@@ -157,6 +182,9 @@ CgroupOptions::Apply(const CgroupState &state, unsigned _pid) const
 	char pid_buffer[32];
 	const std::string_view pid(pid_buffer,
 				   sprintf(pid_buffer, "%u", _pid));
+
+	/* TODO drop support for cgroup1 and hybrid, use only
+	   Create2() and CLONE_INTO_CGROUP */
 
 	std::map<std::string, UniqueFileDescriptor> fds;
 	for (const auto &mount : state.mounts)

src/spawn/CgroupOptions.hxx
@@ -36,6 +36,7 @@
 #include "util/ShallowCopy.hxx"
 
 class AllocatorPtr;
+class UniqueFileDescriptor;
 struct StringView;
 struct CgroupState;
 
@@ -96,6 +97,15 @@ struct CgroupOptions {
 
 	void Set(AllocatorPtr alloc,
 		 StringView name, StringView value) noexcept;
+
+	/**
+	 * Create a cgroup2 group.  Returns an undefined
+	 * #UniqueFileDescriptor if this instance is not enabled or
+	 * this is not a cgroup2-only host.
+	 *
+	 * Throws on error.
+	 */
+	UniqueFileDescriptor Create2(const CgroupState &state) const;
 
 	/**
 	 * Throws std::runtime_error on error.

commit 1b03b3cfb804e47b4aace2c42ca660d8852ad472

spawn/Direct: fix init process name regression
The init process name was set to the cgroup name, but commit
0b3294f01f366 broke this when it assigned nullptr to params.cgroup.
We're fixing this now by copying the cgroup name before clearing it.

src/spawn/Direct.cxx
@@ -145,6 +145,7 @@ WaitForPipe(FileDescriptor r) noexcept
 [[noreturn]]
 static void
 Exec(const char *path, PreparedChildProcess &&p,
+     const char *name,
      UniqueFileDescriptor &&userns_map_pipe_r,
      UniqueFileDescriptor &&userns_create_pipe_w,
      UniqueFileDescriptor &&wait_pipe_r,
@@ -256,10 +257,6 @@ try {
 
 	if (p.ns.enable_pid && p.ns.pid_namespace == nullptr) {
 		setsid();
-
-		const char *name = nullptr;
-		if (p.cgroup != nullptr)
-			name = p.cgroup->name;
 
 		const auto pid = SpawnInitFork(name);
 		assert(pid >= 0);
@@ -484,6 +481,14 @@ SpawnChildProcess(PreparedChildProcess &&params,
 	ca.pidfd = (intptr_t)&_pidfd;
 	ca.exit_signal = SIGCHLD;
 
+	/* if a cgroup name is specified, it is used as the name for
+	   the "init" process; we need to extract it now, because the
+	   CLONE_INTO_CGROUP code below will clear the CgroupOptions
+	   pointer */
+	const char *const name = params.cgroup != nullptr
+		? params.cgroup->name
+		: nullptr;
+
 #ifdef CLONE_INTO_CGROUP
 	UniqueFileDescriptor cgroup_fd;
 	if (params.cgroup != nullptr) {
@@ -515,6 +520,7 @@ SpawnChildProcess(PreparedChildProcess &&params,
 		error_pipe_r.Close();
 
 		Exec(path, std::move(params),
+		     name,
 		     std::move(userns_map_pipe_r),
 		     std::move(userns_create_pipe_w),
 		     std::move(wait_pipe_r),