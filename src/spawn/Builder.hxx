CM4all/libcommon

commit 4c9e0ba67d47a4a2dfa9ef7e974a76e678c1a61d

io/Iovec, ...: migrate from ConstBuffer to std::span

src/spawn/Builder.hxx
@@ -40,11 +40,12 @@
 #include "system/Error.hxx"
 #include "io/FileDescriptor.hxx"
 #include "io/Iovec.hxx"
-#include "util/ConstBuffer.hxx"
 #include "util/StaticArray.hxx"
 
 #include <array>
 #include <cstddef>
+#include <span>
+#include <string_view>
 
 #include <assert.h>
 #include <stdint.h>
@@ -92,30 +93,34 @@ public:
 			Write(cmd);
 	}
 
-	void Write(ConstBuffer<void> value) {
-		if (size + value.size > buffer.size())
+	void Write(std::span<const std::byte> value) {
+		if (size + value.size() > buffer.size())
 			throw SpawnPayloadTooLargeError();
 
-		memcpy(buffer.data() + size, value.data, value.size);
-		size += value.size;
+		std::copy(value.begin(), value.end(),
+			  std::next(buffer.begin(), size));
+		size += value.size();
+	}
+
+	template<typename T>
+	void Write(std::span<const T> value) noexcept {
+		Write(std::as_bytes(value));
 	}
 
 	template<typename T>
 	void WriteT(const T &value) {
-		Write(ConstBuffer<void>(&value, sizeof(value)));
+		Write(std::span{&value, 1});
 	}
 
 	void WriteInt(int value) {
 		WriteT(value);
 	}
 
-	void WriteString(const char *value) {
-		assert(value != nullptr);
-
-		Write(ConstBuffer<void>(value, strlen(value) + 1));
+	void WriteString(std::string_view value) {
+		Write(std::span{value});
 	}
 
-	void WriteString(SpawnExecCommand cmd, const char *value) {
+	void WriteString(SpawnExecCommand cmd, std::string_view value) {
 		Write(cmd);
 		WriteString(value);
 	}
@@ -140,19 +145,19 @@ public:
 			WriteFd(cmd, fd);
 	}
 
-	ConstBuffer<void> GetPayload() const {
+	std::span<const std::byte> GetPayload() const {
 		return {buffer.data(), size};
 	}
 
-	ConstBuffer<FileDescriptor> GetFds() const noexcept {
+	std::span<const FileDescriptor> GetFds() const noexcept {
 		return fds;
 	}
 };
 
 template<size_t MAX_FDS>
 static void
-Send(SocketDescriptor s, ConstBuffer<void> payload,
-     ConstBuffer<FileDescriptor> fds)
+Send(SocketDescriptor s, std::span<const std::byte> payload,
+     std::span<const FileDescriptor> fds)
 {
 	assert(s.IsDefined());
 

src/spawn/daemon/Builder.hxx
@@ -48,30 +48,36 @@ public:
 	DatagramBuilder() noexcept {
 		header.magic = SpawnDaemon::MAGIC;
 
-		AppendRaw({&header, sizeof(header)});
+		AppendRaw(std::span{&header, 1});
 	}
 
 	DatagramBuilder(const DatagramBuilder &) = delete;
 	DatagramBuilder &operator=(const DatagramBuilder &) = delete;
 
-	void AppendRaw(ConstBuffer<void> b) noexcept {
-		v.append() = MakeIovec(b);
+	template<typename T>
+	void AppendRaw(std::span<T> s) noexcept {
+		v.append() = MakeIovec(s);
 	}
 
-	void AppendPadded(ConstBuffer<void> b) noexcept {
+	void AppendPadded(std::span<const std::byte> b) noexcept {
 		AppendRaw(b);
 
-		const size_t padding_size = (-b.size) & 3;
-		static constexpr uint8_t padding[] = {0, 0, 0};
-		AppendRaw({padding, padding_size});
+		const size_t padding_size = (-b.size()) & 3;
+		static constexpr std::byte padding[3]{};
+		AppendRaw(std::span{padding, padding_size});
+	}
+
+	template<typename T>
+	void AppendPadded(std::span<const T> s) noexcept {
+		AppendPadded(std::as_bytes(s));
 	}
 
 	void Append(const SpawnDaemon::RequestHeader &rh) noexcept {
-		AppendRaw({&rh, sizeof(rh)});
+		AppendRaw(std::as_bytes(std::span{&rh, 1}));
 	}
 
 	void Append(const SpawnDaemon::ResponseHeader &rh) noexcept {
-		AppendRaw({&rh, sizeof(rh)});
+		AppendRaw(std::span{&rh, 1});
 	}
 
 	MessageHeader Finish() noexcept {

src/io/Iovec.hxx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007-2021 CM4all GmbH
+ * Copyright 2007-2022 CM4all GmbH
  * All rights reserved.
  *
  * author: Max Kellermann <mk@cm4all.com>
@@ -32,32 +32,28 @@
 
 #pragma once
 
-#include "util/ConstBuffer.hxx"
-#include "util/WritableBuffer.hxx"
+#include <span>
 
 #include <sys/uio.h>
 
-template<typename T>
 constexpr struct iovec
-MakeIovec(ConstBuffer<T> t) noexcept
+MakeIovec(std::span<const std::byte> s) noexcept
 {
-	auto b = t.ToVoid();
-	return { const_cast<void *>(b.data), b.size };
+	return { const_cast<std::byte *>(s.data()), s.size() };
 }
 
 template<typename T>
 constexpr struct iovec
-MakeIovec(WritableBuffer<T> t) noexcept
+MakeIovec(std::span<T> s) noexcept
 {
-	auto b = t.ToVoid();
-	return { b.data, b.size };
+	return MakeIovec(std::as_bytes(s));
 }
 
 template<typename T>
 constexpr struct iovec
 MakeIovecT(const T &t) noexcept
 {
-	return { const_cast<T *>(&t), sizeof(t) };
+	return MakeIovec(std::span{&t, 1});
 }
 
 template<typename T, T _value>

src/io/Logger.cxx
@@ -45,9 +45,9 @@ LoggerDetail::ParamWrapper<std::exception_ptr>::ParamWrapper(std::exception_ptr 
 	:ParamWrapper<std::string>(GetFullMessage(ep)) {}
 
 static constexpr struct iovec
-MakeIovec(StringView s) noexcept
+MakeIovec(std::string_view s) noexcept
 {
-	return MakeIovec(s.ToVoid());
+	return MakeIovec(std::span{s});
 }
 
 void

src/net/MsgHdr.hxx
@@ -34,16 +34,17 @@
 
 #include "SocketAddress.hxx"
 #include "StaticSocketAddress.hxx"
-#include "util/ConstBuffer.hxx"
+
+#include <span>
 
 #include <sys/socket.h>
 
 inline constexpr struct msghdr
-MakeMsgHdr(ConstBuffer<struct iovec> iov) noexcept
+MakeMsgHdr(std::span<const struct iovec> iov) noexcept
 {
 	struct msghdr mh{};
-	mh.msg_iov = const_cast<struct iovec *>(iov.data);
-	mh.msg_iovlen = iov.size;
+	mh.msg_iov = const_cast<struct iovec *>(iov.data());
+	mh.msg_iovlen = iov.size();
 	return mh;
 }
 
@@ -53,37 +54,37 @@ MakeMsgHdr(ConstBuffer<struct iovec> iov) noexcept
  * actually be writable.
  */
 inline constexpr struct msghdr
-MakeMsgHdr(SocketAddress name, ConstBuffer<struct iovec> iov,
-	   ConstBuffer<void> control) noexcept
+MakeMsgHdr(SocketAddress name, std::span<const struct iovec> iov,
+	   std::span<const std::byte> control) noexcept
 {
 	auto mh = MakeMsgHdr(iov);
 	mh.msg_name = const_cast<struct sockaddr *>(name.GetAddress());
 	mh.msg_namelen = name.GetSize();
-	mh.msg_control = const_cast<void *>(control.data);
-	mh.msg_controllen = control.size;
+	mh.msg_control = const_cast<std::byte *>(control.data());
+	mh.msg_controllen = control.size();
 	return mh;
 }
 
 inline constexpr struct msghdr
-MakeMsgHdr(StaticSocketAddress &name, ConstBuffer<struct iovec> iov,
-	   ConstBuffer<void> control) noexcept
+MakeMsgHdr(StaticSocketAddress &name, std::span<const struct iovec> iov,
+	   std::span<const std::byte> control) noexcept
 {
 	auto mh = MakeMsgHdr(iov);
 	mh.msg_name = name;
 	mh.msg_namelen = name.GetCapacity();
-	mh.msg_control = const_cast<void *>(control.data);
-	mh.msg_controllen = control.size;
+	mh.msg_control = const_cast<std::byte *>(control.data());
+	mh.msg_controllen = control.size();
 	return mh;
 }
 
 inline constexpr struct msghdr
-MakeMsgHdr(struct sockaddr_storage &name, ConstBuffer<struct iovec> iov,
-	   ConstBuffer<void> control) noexcept
+MakeMsgHdr(struct sockaddr_storage &name, std::span<const struct iovec> iov,
+	   std::span<const std::byte> control) noexcept
 {
 	auto mh = MakeMsgHdr(iov);
 	mh.msg_name = static_cast<struct sockaddr *>(static_cast<void *>(&name));
 	mh.msg_namelen = sizeof(name);
-	mh.msg_control = const_cast<void *>(control.data);
-	mh.msg_controllen = control.size;
+	mh.msg_control = const_cast<std::byte *>(control.data());
+	mh.msg_controllen = control.size();
 	return mh;
 }

src/net/MultiReceiveMessage.cxx
@@ -59,9 +59,9 @@ MultiReceiveMessage::MultiReceiveMessage(size_t _allocated_datagrams,
 	for (size_t i = 0; i < allocated_datagrams; ++i) {
 		v[i] = {GetPayload(i), max_payload_size};
 
-		ConstBuffer<void> cmsg = nullptr;
+		std::span<const std::byte> cmsg{};
 		if (max_cmsg_size > 0)
-			cmsg = {GetCmsg(i), max_cmsg_size};
+			cmsg = {(const std::byte *)GetCmsg(i), max_cmsg_size};
 
 		m[i].msg_hdr = MakeMsgHdr(*a, {v + i, 1}, cmsg);
 	}
@@ -103,10 +103,10 @@ MultiReceiveMessage::Receive(SocketDescriptor s)
 		auto &d = datagrams[i];
 		d.address = SocketAddress((const struct sockaddr *)mh.msg_name,
 					  mh.msg_namelen);
-		d.payload = {GetPayload(i), m[i].msg_len};
+		d.payload = {(std::byte *)GetPayload(i), m[i].msg_len};
 		d.cred = nullptr;
-		d.fds.data = fds_p;
-		d.fds.size = 0;
+
+		d.fds = {fds_p, 0};
 
 #ifdef __clang__
 #pragma GCC diagnostic push
@@ -128,7 +128,7 @@ MultiReceiveMessage::Receive(SocketDescriptor s)
 					if (remaining_fds > 0) {
 						*fds_p++ = UniqueFileDescriptor(fd);
 						--remaining_fds;
-						++d.fds.size;
+						d.fds = {d.fds.data(), d.fds.size() + 1};
 					} else
 						fd.Close();
 				}

src/net/MultiReceiveMessage.hxx
@@ -37,9 +37,9 @@
 #include "io/UniqueFileDescriptor.hxx"
 #include "util/AllocatedArray.hxx"
 #include "util/OffsetPointer.hxx"
-#include "util/WritableBuffer.hxx"
 
 #include <memory>
+#include <span>
 
 class SocketDescriptor;
 
@@ -61,9 +61,9 @@ class MultiReceiveMessage {
 public:
 	struct Datagram {
 		SocketAddress address;
-		WritableBuffer<void> payload;
+		std::span<std::byte> payload;
 		const struct ucred *cred;
-		WritableBuffer<UniqueFileDescriptor> fds;
+		std::span<UniqueFileDescriptor> fds;
 
 		/*
 		constexpr Datagram(SocketAddress _address,

src/net/ReceiveMessage.hxx
@@ -37,8 +37,8 @@
 #include "MsgHdr.hxx"
 #include "SocketError.hxx"
 #include "io/UniqueFileDescriptor.hxx"
-#include "util/ConstBuffer.hxx"
 
+#include <span>
 #include <vector>
 
 #include <stdint.h>
@@ -47,7 +47,7 @@ template<size_t PAYLOAD_SIZE, size_t CMSG_SIZE>
 struct ReceiveMessageBuffer {
 	StaticSocketAddress address;
 
-	uint8_t payload[PAYLOAD_SIZE];
+	std::byte payload[PAYLOAD_SIZE];
 
 	static constexpr size_t CMSG_BUFFER_SIZE = CMSG_SPACE(CMSG_SIZE);
 	static constexpr size_t CMSG_N_LONGS = (CMSG_BUFFER_SIZE + sizeof(long) - 1) / sizeof(long);
@@ -57,7 +57,7 @@ struct ReceiveMessageBuffer {
 struct ReceiveMessageResult {
 	SocketAddress address;
 
-	ConstBuffer<void> payload = nullptr;
+	std::span<const std::byte> payload{};
 
 	const struct ucred *cred = nullptr;
 
@@ -80,7 +80,7 @@ ReceiveMessage(SocketDescriptor s,
 	iov.iov_len = sizeof(buffer.payload);
 
 	auto msg = MakeMsgHdr(buffer.address, {&iov, 1},
-			      {buffer.cmsg, sizeof(buffer.cmsg)});
+			      {(const std::byte *)buffer.cmsg, sizeof(buffer.cmsg)});
 
 	auto nbytes = recvmsg(s.Get(), &msg, flags);
 	if (nbytes < 0)

commit 4b952ef83824bde40e594ced2165d2bbf97cea05

spawn/Builder: write missing null terminator
Regression from commit 4c9e0ba67d47a4a2dfa9ef7e974a76e678c1a61d

src/spawn/Builder.hxx
@@ -118,6 +118,7 @@ public:
 
 	void WriteString(std::string_view value) {
 		Write(std::span{value});
+		WriteU8(0);
 	}
 
 	void WriteString(SpawnExecCommand cmd, std::string_view value) {