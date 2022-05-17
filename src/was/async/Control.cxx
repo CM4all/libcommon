CM4all/libcommon

commit fec0b02e042197c24c6d4eb151c70dac806bf7ee

was/async: use std::span instead of ConstBuffer

src/was/async/Control.cxx
@@ -33,7 +33,6 @@
 #include "Control.hxx"
 #include "Error.hxx"
 #include "system/Error.hxx"
-#include "util/ConstBuffer.hxx"
 
 #include <was/protocol.h>
 
@@ -109,7 +108,7 @@ Control::OnBufferedData()
 			return BufferedResult::MORE;
 		}
 
-		const void *payload = header + 1;
+		const std::byte *payload = (const std::byte *)header + 1;
 
 		socket.KeepConsumed(sizeof(*header) + header->length);
 
@@ -264,7 +263,7 @@ Control::SendPair(enum was_command cmd, std::string_view name,
 
 bool
 Control::SendArray(enum was_command cmd,
-		   ConstBuffer<const char *> values) noexcept
+		   std::span<const char *const> values) noexcept
 {
 	for (auto value : values) {
 		assert(value != nullptr);

src/was/async/Control.hxx
@@ -37,9 +37,9 @@
 
 #include <was/protocol.h>
 
+#include <cstddef>
+#include <span>
 #include <string_view>
-
-template<typename T> struct ConstBuffer;
 
 namespace Was {
 
@@ -51,7 +51,7 @@ public:
 	 * @return false if the object was closed
 	 */
 	virtual bool OnWasControlPacket(enum was_command cmd,
-					ConstBuffer<void> payload) noexcept = 0;
+					std::span<const std::byte> payload) noexcept = 0;
 
 	/**
 	 * Called after a group of control packets have been handled, and
@@ -129,7 +129,7 @@ public:
 	}
 
 	bool SendArray(enum was_command cmd,
-		       ConstBuffer<const char *> values) noexcept;
+		       std::span<const char *const> values) noexcept;
 
 	void Done() noexcept;
 

src/was/async/SimpleServer.cxx
@@ -106,7 +106,7 @@ SimpleServer::AbortProtocolError(const char *msg) noexcept
 
 bool
 SimpleServer::OnWasControlPacket(enum was_command cmd,
-				 ConstBuffer<void> payload) noexcept
+				 std::span<const std::byte> payload) noexcept
 {
 	switch (cmd) {
 	case WAS_COMMAND_NOP:
@@ -133,13 +133,13 @@ SimpleServer::OnWasControlPacket(enum was_command cmd,
 			return false;
 		}
 
-		if (payload.size != sizeof(request.request->method)) {
+		if (payload.size() != sizeof(request.request->method)) {
 			AbortProtocolError("malformed METHOD packet");
 			return false;
 		}
 
 		{
-			auto method = *(const http_method_t *)payload.data;
+			auto method = *(const http_method_t *)payload.data();
 			if (request.request->method != HTTP_METHOD_GET &&
 			    method != request.request->method) {
 				/* sending that packet twice is illegal */
@@ -164,32 +164,32 @@ SimpleServer::OnWasControlPacket(enum was_command cmd,
 			return false;
 		}
 
-		request.request->uri.assign((const char *)payload.data,
-					    payload.size);
+		request.request->uri.assign((const char *)payload.data(),
+					    payload.size());
 		break;
 
 	case WAS_COMMAND_SCRIPT_NAME:
 		if (request.state != Request::State::HEADERS)
 			AbortProtocolError("misplaced SCRIPT_NAME packet");
 
-		request.request->script_name.assign((const char *)payload.data,
-						    payload.size);
+		request.request->script_name.assign((const char *)payload.data(),
+						    payload.size());
 		break;
 
 	case WAS_COMMAND_PATH_INFO:
 		if (request.state != Request::State::HEADERS)
 			AbortProtocolError("misplaced PATH_INFO packet");
 
-		request.request->path_info.assign((const char *)payload.data,
-						    payload.size);
+		request.request->path_info.assign((const char *)payload.data(),
+						    payload.size());
 		break;
 
 	case WAS_COMMAND_QUERY_STRING:
 		if (request.state != Request::State::HEADERS)
 			AbortProtocolError("misplaced QUERY_STRING packet");
 
-		request.request->query_string.assign((const char *)payload.data,
-						     payload.size);
+		request.request->query_string.assign((const char *)payload.data(),
+						     payload.size());
 		break;
 
 	case WAS_COMMAND_HEADER:
@@ -257,8 +257,8 @@ SimpleServer::OnWasControlPacket(enum was_command cmd,
 		}
 
 		{
-			auto length_p = (const uint64_t *)payload.data;
-			if (payload.size != sizeof(*length_p)) {
+			auto length_p = (const uint64_t *)payload.data();
+			if (payload.size() != sizeof(*length_p)) {
 				AbortProtocolError("malformed LENGTH packet");
 				return false;
 			}
@@ -282,8 +282,8 @@ SimpleServer::OnWasControlPacket(enum was_command cmd,
 
 	case WAS_COMMAND_PREMATURE:
 		{
-			auto length_p = (const uint64_t *)payload.data;
-			if (payload.size != sizeof(*length_p)) {
+			auto length_p = (const uint64_t *)payload.data();
+			if (payload.size() != sizeof(*length_p)) {
 				AbortError(std::make_exception_ptr("malformed PREMATURE packet"));
 				return false;
 			}
@@ -302,8 +302,8 @@ SimpleServer::OnWasControlPacket(enum was_command cmd,
 		if (request.state != Request::State::HEADERS)
 			AbortProtocolError("misplaced REMOTE_HOST packet");
 
-		request.request->remote_host.assign((const char *)payload.data,
-						    payload.size);
+		request.request->remote_host.assign((const char *)payload.data(),
+						    payload.size());
 		break;
 	}
 

src/was/async/SimpleServer.hxx
@@ -135,7 +135,7 @@ private:
 
 	/* virtual methods from class Was::ControlHandler */
 	bool OnWasControlPacket(enum was_command cmd,
-				ConstBuffer<void> payload) noexcept override;
+				std::span<const std::byte> payload) noexcept override;
 	bool OnWasControlDrained() noexcept override;
 	void OnWasControlDone() noexcept override;
 	void OnWasControlError(std::exception_ptr ep) noexcept override;

commit 046d8d1f3255db9f452bc439a294364731e299eb

was/async/Control: add missing parantheses
Broken by commit fec0b02e042197c24c6d4eb151c70dac806bf7ee

src/was/async/Control.cxx
@@ -108,7 +108,7 @@ Control::OnBufferedData()
 			return BufferedResult::MORE;
 		}
 
-		const std::byte *payload = (const std::byte *)header + 1;
+		const std::byte *payload = (const std::byte *)(header + 1);
 
 		socket.KeepConsumed(sizeof(*header) + header->length);
 