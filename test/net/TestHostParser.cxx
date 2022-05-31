cae63e0f9279f70d87cd53f7aa1619c1f39f1e4e
Max Kellermann <max@musicpd.org>
2022-05-31 13:33:33 +0200

    net/HostParser: use std::string_view instead of StringView

--- test/net/TestHostParser.cxx
+++ test/net/TestHostParser.cxx
@@ -38,8 +38,8 @@ TEST(HostParserTest, Name)
 {
     const auto input = "foo";
     const auto eh = ExtractHost(input);
-    ASSERT_EQ(eh.host.data, input);
-    ASSERT_EQ(eh.host.size, 3u);
+    ASSERT_EQ(eh.host.data(), input);
+    ASSERT_EQ(eh.host.size(), 3u);
     ASSERT_EQ(eh.end, input + 3);
 }
 
@@ -47,8 +47,8 @@ TEST(HostParserTest, NamePort)
 {
     const auto input = "foo:80";
     const auto eh = ExtractHost(input);
-    ASSERT_EQ(eh.host.data, input);
-    ASSERT_EQ(eh.host.size, 3u);
+    ASSERT_EQ(eh.host.data(), input);
+    ASSERT_EQ(eh.host.size(), 3u);
     ASSERT_EQ(eh.end, input + 3);
 }
 
@@ -56,8 +56,8 @@ TEST(HostParserTest, IPv4)
 {
     const auto input = "1.2.3.4";
     const auto eh = ExtractHost(input);
-    ASSERT_EQ(eh.host.data, input);
-    ASSERT_EQ(eh.host.size, 7u);
+    ASSERT_EQ(eh.host.data(), input);
+    ASSERT_EQ(eh.host.size(), 7u);
     ASSERT_EQ(eh.end, input + 7);
 }
 
@@ -65,8 +65,8 @@ TEST(HostParserTest, IPv6Wildcard)
 {
     const auto input = "::";
     const auto eh = ExtractHost(input);
-    ASSERT_EQ(eh.host.data, input);
-    ASSERT_EQ(eh.host.size, 2u);
+    ASSERT_EQ(eh.host.data(), input);
+    ASSERT_EQ(eh.host.size(), 2u);
     ASSERT_EQ(eh.end, input + 2);
 }
 
@@ -74,8 +74,8 @@ TEST(HostParserTest, IPv6Local)
 {
     const auto input = "::1";
     const auto eh = ExtractHost(input);
-    ASSERT_EQ(eh.host.data, input);
-    ASSERT_EQ(eh.host.size, 3u);
+    ASSERT_EQ(eh.host.data(), input);
+    ASSERT_EQ(eh.host.size(), 3u);
     ASSERT_EQ(eh.end, input + 3);
 }
 
@@ -83,8 +83,8 @@ TEST(HostParserTest, IPv6Static)
 {
     const auto input = "2001:affe::";
     const auto eh = ExtractHost(input);
-    ASSERT_EQ(eh.host.data, input);
-    ASSERT_EQ(eh.host.size, 11u);
+    ASSERT_EQ(eh.host.data(), input);
+    ASSERT_EQ(eh.host.size(), 11u);
     ASSERT_EQ(eh.end, input + 11);
 }
 
@@ -92,8 +92,8 @@ TEST(HostParserTest, IPv6StaticPort)
 {
     const auto input = "[2001:affe::]:80";
     const auto eh = ExtractHost(input);
-    ASSERT_EQ(eh.host.data, input + 1);
-    ASSERT_EQ(eh.host.size, 11u);
+    ASSERT_EQ(eh.host.data(), input + 1);
+    ASSERT_EQ(eh.host.size(), 11u);
     ASSERT_EQ(eh.end, input + 13);
 }
 
@@ -101,8 +101,8 @@ TEST(HostParserTest, IPv6Scope)
 {
     const auto input = "2001:affe::%eth0";
     const auto eh = ExtractHost(input);
-    ASSERT_EQ(eh.host.data, input);
-    ASSERT_EQ(eh.host.size, 16u);
+    ASSERT_EQ(eh.host.data(), input);
+    ASSERT_EQ(eh.host.size(), 16u);
     ASSERT_EQ(eh.end, input + 16);
 }
 
@@ -110,7 +110,7 @@ TEST(HostParserTest, IPv6ScopePort)
 {
     const auto input = "[2001:affe::%eth0]:80";
     const auto eh = ExtractHost(input);
-    ASSERT_EQ(eh.host.data, input + 1);
-    ASSERT_EQ(eh.host.size, 16u);
+    ASSERT_EQ(eh.host.data(), input + 1);
+    ASSERT_EQ(eh.host.size(), 16u);
     ASSERT_EQ(eh.end, input + 18);
 }
