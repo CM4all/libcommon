192d71dbd0a378f6bd92e2b106d25b5d59ffc539
Max Kellermann <mk@cm4all.com>
2022-06-28 11:34:38 +0200

    lib/openssl/GeneralName: add `noexcept`

--- src/lib/openssl/GeneralName.hxx
+++ src/lib/openssl/GeneralName.hxx
@@ -50,37 +50,37 @@ class GeneralName {
 
 public:
 	GeneralName() = default;
-	constexpr GeneralName(GENERAL_NAME *_value):value(_value) {}
+	constexpr GeneralName(GENERAL_NAME *_value) noexcept:value(_value) {}
 
-	friend void swap(GeneralName &a, GeneralName &b) {
+	friend void swap(GeneralName &a, GeneralName &b) noexcept {
 		std::swap(a.value, b.value);
 	}
 
-	constexpr operator bool() const {
+	constexpr operator bool() const noexcept {
 		return value != nullptr;
 	}
 
-	constexpr GENERAL_NAME *get() {
+	constexpr GENERAL_NAME *get() const noexcept {
 		return value;
 	}
 
-	GENERAL_NAME *release() {
+	GENERAL_NAME *release() noexcept {
 		return std::exchange(value, nullptr);
 	}
 
-	void clear() {
+	void clear() noexcept {
 		assert(value != nullptr);
 
 		GENERAL_NAME_free(release());
 	}
 
-	int GetType() const {
+	int GetType() const noexcept {
 		assert(value != nullptr);
 
 		return value->type;
 	}
 
-	StringView GetDnsName() const {
+	StringView GetDnsName() const noexcept {
 		assert(value != nullptr);
 		assert(value->type == GEN_DNS);
 
@@ -102,24 +102,25 @@ public:
 class UniqueGeneralName : public GeneralName {
 public:
 	UniqueGeneralName() = default;
-	explicit UniqueGeneralName(GENERAL_NAME *_value):GeneralName(_value) {}
+	explicit UniqueGeneralName(GENERAL_NAME *_value) noexcept
+		:GeneralName(_value) {}
 
-	UniqueGeneralName(GeneralName &&src)
+	UniqueGeneralName(GeneralName &&src) noexcept
 		:GeneralName(src.release()) {}
 
-	~UniqueGeneralName() {
+	~UniqueGeneralName() noexcept {
 		if (*this)
 			clear();
 	}
 
-	GeneralName &operator=(GeneralName &&src) {
+	GeneralName &operator=(GeneralName &&src) noexcept {
 		swap(*this, src);
 		return *this;
 	}
 };
 
 static inline UniqueGeneralName
-ToDnsName(const char *value)
+ToDnsName(const char *value) noexcept
 {
 	return UniqueGeneralName(a2i_GENERAL_NAME(nullptr, nullptr, nullptr,
 						  GEN_DNS,
@@ -130,43 +131,44 @@ class GeneralNames {
 	GENERAL_NAMES *value = nullptr;
 
 public:
-	constexpr GeneralNames(GENERAL_NAMES *_value):value(_value) {}
+	constexpr GeneralNames(GENERAL_NAMES *_value) noexcept
+		:value(_value) {}
 
-	friend void swap(GeneralNames &a, GeneralNames &b) {
+	friend void swap(GeneralNames &a, GeneralNames &b) noexcept {
 		std::swap(a.value, b.value);
 	}
 
-	constexpr operator bool() const {
+	constexpr operator bool() const noexcept {
 		return value != nullptr;
 	}
 
-	constexpr GENERAL_NAMES *get() {
+	constexpr GENERAL_NAMES *get() noexcept {
 		return value;
 	}
 
-	GENERAL_NAMES *release() {
+	GENERAL_NAMES *release() noexcept {
 		return std::exchange(value, nullptr);
 	}
 
-	void clear() {
+	void clear() noexcept {
 		assert(value != nullptr);
 
 		GENERAL_NAMES_free(release());
 	}
 
-	size_t size() const {
+	size_t size() const noexcept {
 		assert(value != nullptr);
 
 		return sk_GENERAL_NAME_num(value);
 	}
 
-	GeneralName operator[](size_t i) const {
+	GeneralName operator[](size_t i) const noexcept {
 		assert(value != nullptr);
 
 		return sk_GENERAL_NAME_value(value, i);
 	}
 
-	void push_back(UniqueGeneralName &&n) {
+	void push_back(UniqueGeneralName &&n) noexcept {
 		sk_GENERAL_NAME_push(value, n.release());
 	}
 
@@ -175,36 +177,37 @@ public:
 		size_t i;
 
 	public:
-		constexpr const_iterator(const GeneralNames &_sk, size_t _i)
+		constexpr const_iterator(const GeneralNames &_sk,
+					 std::size_t _i) noexcept
 			:sk(&_sk), i(_i) {}
 
-		const_iterator &operator++() {
+		const_iterator &operator++() noexcept {
 			++i;
 			return *this;
 		}
 
-		constexpr bool operator==(const_iterator other) const {
+		constexpr bool operator==(const_iterator other) const noexcept {
 			return sk == other.sk && i == other.i;
 		}
 
-		constexpr bool operator!=(const_iterator other) const {
+		constexpr bool operator!=(const_iterator other) const noexcept {
 			return !(*this == other);
 		}
 
-		GeneralName operator*() const {
+		GeneralName operator*() const noexcept {
 			return (*sk)[i];
 		}
 
-		GeneralName operator->() const {
+		GeneralName operator->() const noexcept {
 			return (*sk)[i];
 		}
 	};
 
-	const_iterator begin() const {
+	const_iterator begin() const noexcept {
 		return const_iterator(*this, 0);
 	}
 
-	const_iterator end() const {
+	const_iterator end() const noexcept {
 		return const_iterator(*this, size());
 	}
 };
@@ -214,20 +217,21 @@ public:
  */
 class UniqueGeneralNames : public GeneralNames {
 public:
-	UniqueGeneralNames()
+	UniqueGeneralNames() noexcept
 		:GeneralNames(sk_GENERAL_NAME_new_null()) {}
 
-	explicit UniqueGeneralNames(GENERAL_NAMES *_value):GeneralNames(_value) {}
+	explicit UniqueGeneralNames(GENERAL_NAMES *_value) noexcept
+		:GeneralNames(_value) {}
 
-	UniqueGeneralNames(GeneralNames &&src)
+	UniqueGeneralNames(GeneralNames &&src) noexcept
 		:GeneralNames(src.release()) {}
 
-	~UniqueGeneralNames() {
+	~UniqueGeneralNames() noexcept {
 		if (*this)
 			clear();
 	}
 
-	UniqueGeneralNames &operator=(UniqueGeneralNames &&src) {
+	UniqueGeneralNames &operator=(UniqueGeneralNames &&src) noexcept {
 		swap(*this, src);
 		return *this;
 	}
