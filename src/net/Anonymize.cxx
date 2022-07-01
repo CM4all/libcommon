d5c98b5d5efe5079d9919c8d3f752dd4e5ef634c
Max Kellermann <mk@cm4all.com>
2022-07-01 16:24:19 +0200

    net/Anonymize: eliminate StringView usage

--- src/net/Anonymize.cxx
+++ src/net/Anonymize.cxx
@@ -32,46 +32,61 @@
 
 #include "Anonymize.hxx"
 #include "util/CharUtil.hxx"
-#include "util/StringView.hxx"
+#include "util/StringSplit.hxx"
 
 using std::string_view_literals::operator""sv;
 
 std::pair<std::string_view, std::string_view>
 AnonymizeAddress(std::string_view value) noexcept
 {
-	if (const auto [_, rest] = StringView{value}.Split('.');
-	    rest != nullptr && IsDigitASCII(value.front()) &&
+	if (const auto [_, rest] = Split(value, '.');
+	    rest.data() != nullptr && IsDigitASCII(value.front()) &&
 	    IsDigitASCII(value.back())) {
 		/* IPv4: zero the last octet */
 
-		const char *q = rest.FindLast('.');
-		if (q != nullptr)
-			return {StringView{value.data(), q + 1}, "0"sv};
+		const auto q = rest.rfind('.');
+		if (q != rest.npos)
+			return {
+				value.substr(0, std::distance(value.data(),
+							      rest.data() + q + 1)),
+				"0"sv,
+			};
 	}
 
-	if (auto [_, rest] = StringView{value}.Split(':');
-	    rest != nullptr &&
+	if (auto [_, rest] = Split(value, ':');
+	    rest.data() != nullptr &&
 	    (IsHexDigit(value.front()) || value.front() == ':') &&
 	    (IsHexDigit(value.back()) || value.back() == ':')) {
 		/* IPv6: truncate after the first 40 bit */
 
 		for (unsigned i = 1; i < 2; ++i) {
-			const char *q = rest.Find(':');
-			if (q == rest.data)
-				return {StringView{value.data(), q + 1}, {}};
+			const auto q = rest.find(':');
+			if (q == 0)
+				return {
+					value.substr(0, std::distance(value.data(),
+								      rest.data() + q + 1)),
+					{},
+				};
 
-			if (q == nullptr)
+			if (q == rest.npos)
 				return {value, {}};
 
 			rest = rest.substr(q + 1);
 		}
 
 		/* clear the low 8 bit of the third segment */
-		const auto third_segment = rest.Split(':').first;
-		if (third_segment.size > 2)
-			return {StringView{value.data(), third_segment.end() - 2}, "00::"sv};
+		const auto third_segment = Split(rest, ':').first;
+		if (third_segment.size() > 2)
+			return {
+				value.substr(0, std::distance(value.begin(),
+							      std::prev(third_segment.end(), 2))),
+				"00::"sv,
+			};
 
-		return {StringView{value.data(), rest.data}, ":"sv};
+		return {
+			value.substr(0, std::distance(value.data(), rest.data())),
+			":"sv,
+		};
 	}
 
 	return {value, {}};
