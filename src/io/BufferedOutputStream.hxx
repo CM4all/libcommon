40a8991623c5b6c290243a8c74ea55548fb38eb5
Max Kellermann <max.kellermann@gmail.com>
2022-07-13 12:49:16 +0200

    io/BufferedOutputStream: add libfmt support

--- src/io/BufferedOutputStream.hxx
+++ src/io/BufferedOutputStream.hxx
@@ -7,6 +7,11 @@
 #include "util/Compiler.h"
 #include "util/DynamicFifoBuffer.hxx"
 
+#include <fmt/core.h>
+#if FMT_VERSION >= 80000 && FMT_VERSION < 90000
+#include <fmt/format.h>
+#endif
+
 #include <cstddef>
 
 #ifdef _UNICODE
@@ -65,6 +70,20 @@ public:
 	 */
 	gcc_printf(2,3)
 	void Format(const char *fmt, ...);
+
+	void VFmt(fmt::string_view format_str, fmt::format_args args);
+
+	template<typename S, typename... Args>
+	void Fmt(const S &format_str, Args&&... args) {
+#if FMT_VERSION >= 90000
+		VFmt(format_str,
+		     fmt::make_format_args(args...));
+#else
+		VFmt(fmt::to_string_view(format_str),
+		     fmt::make_args_checked<Args...>(format_str,
+						     args...));
+#endif
+	}
 
 #ifdef _UNICODE
 	/**
