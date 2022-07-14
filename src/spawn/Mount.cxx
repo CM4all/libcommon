CM4all/libcommon

commit 2850ba73ebc1685c4338743cb4e61159e6613e94

spawn/Mount: eliminate StringView usage

src/spawn/Mount.cxx
@@ -36,7 +36,7 @@
 #include "system/Error.hxx"
 #include "io/UniqueFileDescriptor.hxx"
 #include "util/RuntimeError.hxx"
-#include "util/StringView.hxx"
+#include "util/StringSplit.hxx"
 #include "AllocatorPtr.hxx"
 
 #if TRANSLATION_ENABLE_EXPAND
@@ -146,9 +146,7 @@ Mount::ApplyBindMountFile(VfsBuilder &vfs_builder) const
 	} else {
 		/* target does not exist: first ensure that its parent
 		   directory exists, then create an empty target */
-		const char *slash = strrchr(target, '/');
-		assert(slash != nullptr);
-		const StringView parent{target, slash};
+		const auto parent = Split(std::string_view{target}, '/').first;
 		vfs_builder.MakeDirectory(parent);
 
 		UniqueFileDescriptor fd;

src/spawn/daemon/Client.cxx
@@ -39,7 +39,6 @@
 #include "system/Error.hxx"
 #include "util/CRC32.hxx"
 #include "util/RuntimeError.hxx"
-#include "util/StringView.hxx"
 
 #include <sched.h>
 

commit 55e6c4ece8efbef333b8c86ec24744d974f7dd45

spawn/Mount: fix bind mount on files
Commit 2850ba73ebc1685c4338743cb4e61159e6613e94 broke mkdir() on the
parent directory of articial file bind mount points, because it
changed strrchr() to Split() instead of SplitLast().

src/spawn/Mount.cxx
@@ -146,7 +146,7 @@ Mount::ApplyBindMountFile(VfsBuilder &vfs_builder) const
 	} else {
 		/* target does not exist: first ensure that its parent
 		   directory exists, then create an empty target */
-		const auto parent = Split(std::string_view{target}, '/').first;
+		const auto parent = SplitLast(std::string_view{target}, '/').first;
 		vfs_builder.MakeDirectory(parent);
 
 		UniqueFileDescriptor fd;