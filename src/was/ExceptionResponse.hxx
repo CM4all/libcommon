c24f2e71e55486d411f65137f2768e0d9383720d
Max Kellermann <mk@cm4all.com>
2022-09-20 17:42:58 +0200

    was/ExceptionResponse: common base class for all exception types
    Allow applications to rethrow those exceptions to WasLoop() more
    easily.

--- src/was/ExceptionResponse.hxx
+++ src/was/ExceptionResponse.hxx
@@ -37,17 +37,34 @@
 namespace Was {
 
 /**
+ * The base class for all exceptions in this header.
+ *
+ * These will be handled by WasLoop() and therefore should usually not
+ * be handled by the request handler, but rethrown to WasLoop()
+ * instead.
+ */
+struct Exception {};
+
+/**
  * Indicates that was_simple_abort() shall be called.  This can also
  * be used after an I/O or protocol error to bail out.
  */
-struct AbortResponse {};
+struct AbortResponse : Exception {};
 
-struct NotFound {
+struct NotFound : Exception {
 	std::string_view body = "Not Found\n";
+
+	NotFound() noexcept = default;
+	constexpr NotFound(std::string_view _body) noexcept
+		:body(_body) {}
 };
 
-struct BadRequest {
+struct BadRequest : Exception {
 	std::string_view body = "Bad Request\n";
+
+	BadRequest() noexcept = default;
+	constexpr BadRequest(std::string_view _body) noexcept
+		:body(_body) {}
 };
 
 } // namespace Was
