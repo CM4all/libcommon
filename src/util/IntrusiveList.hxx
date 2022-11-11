bf97895bb5b11e40e9d459106cfc6574000d98bd
Max Kellermann <max.kellermann@gmail.com>
2022-11-11 16:32:54 +0100

    util/IntrusiveList: use `auto`
    Fixes typos in the const_iterator return types, and fixes returning
    references to stack values.

--- src/util/IntrusiveList.hxx
+++ src/util/IntrusiveList.hxx
@@ -393,17 +393,17 @@ public:
 			return Cast(cursor);
 		}
 
-		iterator &operator++() noexcept {
+		auto &operator++() noexcept {
 			cursor = cursor->next;
 			return *this;
 		}
 
-		iterator &operator--() noexcept {
+		auto &operator--() noexcept {
 			cursor = cursor->prev;
 			return *this;
 		}
 
-		iterator &operator--(int) noexcept {
+		auto operator--(int) noexcept {
 			auto old = *this;
 			cursor = cursor->prev;
 			return old;
@@ -458,17 +458,17 @@ public:
 			return Cast(cursor);
 		}
 
-		const_iterator &operator++() noexcept {
+		auto &operator++() noexcept {
 			cursor = cursor->next;
 			return *this;
 		}
 
-		iterator &operator--() noexcept {
+		auto &operator--() noexcept {
 			cursor = cursor->prev;
 			return *this;
 		}
 
-		iterator &operator--(int) noexcept {
+		auto operator--(int) noexcept {
 			auto old = *this;
 			cursor = cursor->prev;
 			return old;
