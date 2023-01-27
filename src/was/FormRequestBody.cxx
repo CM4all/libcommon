eb32a4aeacacfa1a673f12c39d86d38ad6faa8d0
Max Kellermann <mk@cm4all.com>
2023-01-27 13:16:53 +0100

    was/FormRequestBody: catch and translate std::invalid_argument

--- src/was/FormRequestBody.cxx
+++ src/was/FormRequestBody.cxx
@@ -31,9 +31,12 @@
  */
 
 #include "FormRequestBody.hxx"
+#include "ExceptionResponse.hxx"
 #include "ExpectRequestBody.hxx"
 #include "StringRequestBody.hxx"
 #include "uri/MapQueryString.hxx"
+
+#include <stdexcept>
 
 namespace Was {
 
@@ -43,7 +46,12 @@ FormRequestBodyToMap(was_simple *w, std::string::size_type limit)
 	ExpectRequestBody(w, "application/x-www-form-urlencoded");
 
 	const auto raw = RequestBodyToString(w, limit);
-	return MapQueryString(raw);
+
+	try {
+		return MapQueryString(raw);
+	} catch (const std::invalid_argument &e) {
+		throw BadRequest{"Malformed request body\n"};
+	}
 }
 
 } // namespace Was
