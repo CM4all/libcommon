1c492000a479d76e1889e7112fc7b9d42249bad2
Max Kellermann <mk@cm4all.com>
2023-01-27 14:01:16 +0100

    lib/curl/String*: new libraries

---
+++ src/lib/curl/StringGlue.cxx
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2023 CM4all GmbH
+ * All rights reserved.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "StringGlue.hxx"
+#include "StringHandler.hxx"
+#include "Adapter.hxx"
+#include "Easy.hxx"
+
+StringCurlResponse
+StringCurlRequest(CurlEasy easy)
+{
+	StringCurlResponseHandler handler;
+	CurlResponseHandlerAdapter adapter{handler};
+	adapter.Install(easy);
+
+	CURLcode code = curl_easy_perform(easy.Get());
+	adapter.Done(code);
+
+	if (code != CURLE_OK)
+		throw std::runtime_error(curl_easy_strerror(code));
+
+	return std::move(handler).GetResponse();
+}
---
+++ src/lib/curl/StringHandler.hxx
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2023 CM4all GmbH
+ * All rights reserved.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include "Handler.hxx"
+#include "StringResponse.hxx"
+#include "util/SpanCast.hxx"
+
+/**
+ * A #CurlResponseHandler implementation which stores the response
+ * body in a std::string.
+ */
+class StringCurlResponseHandler : public CurlResponseHandler {
+	StringCurlResponse response;
+
+	std::exception_ptr error;
+
+public:
+	StringCurlResponse GetResponse() && {
+		if (error)
+			std::rethrow_exception(error);
+
+		return std::move(response);
+	}
+
+public:
+	/* virtual methods from class CurlResponseHandler */
+
+	void OnHeaders(HttpStatus status, Curl::Headers &&headers) override {
+		response.status = status;
+		response.headers = std::move(headers);
+	}
+
+	void OnData(std::span<const std::byte> data) override {
+		response.body.append(ToStringView(data));
+	}
+
+	void OnEnd() override {
+	}
+
+	void OnError(std::exception_ptr e) noexcept override {
+		error = std::move(e);
+	}
+};
