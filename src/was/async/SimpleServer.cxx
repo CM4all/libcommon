d829a36783166a44a10636ee285cc759e6d94446
Max Kellermann <mk@cm4all.com>
2023-02-28 13:04:17 +0100

    was/async/SimpleServer: ignore METRIC packets (for now)

--- src/was/async/SimpleServer.cxx
+++ src/was/async/SimpleServer.cxx
@@ -306,6 +306,10 @@ SimpleServer::OnWasControlPacket(enum was_command cmd,
 		request.request->remote_host.assign((const char *)payload.data(),
 						    payload.size());
 		break;
+
+	case WAS_COMMAND_METRIC:
+		// TODO implemnet
+		break;
 	}
 
 	return true;
