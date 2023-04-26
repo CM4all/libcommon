5848b5cab85d70a26bf0ea72507063b1d337f9e9
Max Kellermann <mk@cm4all.com>
2023-04-26 15:24:37 +0200

    system/Mount: add wrappers for the new mount API

--- src/system/Mount.cxx
+++ src/system/Mount.cxx
@@ -4,9 +4,171 @@
 
 #include "Mount.hxx"
 #include "Error.hxx"
+#include "io/UniqueFileDescriptor.hxx"
 
-#include <sys/mount.h>
 #include <errno.h>
+
+#ifndef FSOPEN_CLOEXEC
+/* fallback definitions for glibc < 2.36 */
+
+#include <sys/syscall.h>
+
+#define FSOPEN_CLOEXEC 0x00000001
+
+static int
+fsopen(const char *__fs_name, unsigned int __flags)
+{
+	long result = syscall(__NR_fsopen, __fs_name, __flags);
+	if (result < 0) {
+		errno = -result;
+		return -1;
+	}
+
+	return 0;
+}
+
+#define FSMOUNT_CLOEXEC 0x00000001
+
+static int
+fsmount(int __fd, unsigned int __flags, unsigned int __ms_flags)
+{
+	long result = syscall(__NR_fsmount, __fd, __flags, __ms_flags);
+	if (result < 0) {
+		errno = -result;
+		return -1;
+	}
+
+	return 0;
+}
+
+static int
+fsconfig(int __fd, unsigned int __cmd, const char *__key,
+	 const void *__value, int __aux)
+{
+	long result = syscall(__NR_fsconfig, __fd, __cmd,
+			      __key, __value, __aux);
+	if (result < 0) {
+		errno = -result;
+		return -1;
+	}
+
+	return 0;
+}
+
+static int
+move_mount(int __from_dfd, const char *__from_pathname,
+	   int __to_dfd, const char *__to_pathname,
+	   unsigned int flags)
+{
+	long result = syscall(__NR_move_mount,
+			      __from_dfd, __from_pathname,
+			      __to_dfd, __to_pathname,
+			      flags);
+	if (result < 0) {
+		errno = -result;
+		return -1;
+	}
+
+	return 0;
+}
+
+#endif // FSOPEN_CLOEXEC
+
+#ifndef MOUNT_ATTR_SIZE_VER0
+/* fallback definitions for glibc < 2.36 */
+
+struct mount_attr
+{
+  uint64_t attr_set;
+  uint64_t attr_clr;
+  uint64_t propagation;
+  uint64_t userns_fd;
+};
+
+#define MOUNT_ATTR_SIZE_VER0 32
+
+static int
+mount_setattr(int __dfd, const char *__path, unsigned int __flags,
+	      struct mount_attr *__uattr, std::size_t __usize)
+{
+	long result = syscall(__NR_mount_setattr,
+			      __dfd, __path, __flags,
+			      __uattr, __usize);
+	if (result < 0) {
+		errno = -result;
+		return -1;
+	}
+
+	return 0;
+}
+
+#endif // MOUNT_ATTR_SIZE_VER0
+
+UniqueFileDescriptor
+FSOpen(const char *fsname)
+{
+	int fs = fsopen(fsname, FSOPEN_CLOEXEC);
+	if (fs < 0)
+		throw MakeErrno("fsopen() failed");
+
+	return UniqueFileDescriptor{fs};
+}
+
+void
+FSConfig(FileDescriptor fs, unsigned cmd,
+	 const char *key, const char *value, int aux)
+{
+	if (fsconfig(fs.Get(), cmd, key, value, aux) < 0)
+		throw MakeErrno("fsconfig() failed");
+}
+
+UniqueFileDescriptor
+FSMount(FileDescriptor fs, unsigned flags)
+{
+	int mount = fsmount(fs.Get(), FSMOUNT_CLOEXEC, flags);
+	if (mount < 0)
+		throw MakeErrno("fsmount() failed");
+
+	return UniqueFileDescriptor{mount};
+}
+
+void
+MoveMount(FileDescriptor from_directory, const char *from_path,
+	  FileDescriptor to_directory, const char *to_path,
+	  unsigned flags)
+{
+	if (move_mount(from_directory.Get(), from_path,
+		       to_directory.Get(), to_path,
+		       flags) < 0)
+		throw MakeErrno("move_mount() failed");
+}
+
+void
+MountSetAttr(FileDescriptor directory, const char *path, unsigned flags,
+	     const struct mount_attr *uattr, std::size_t usize)
+{
+	if (mount_setattr(directory.Get(), path, flags,
+			  const_cast<struct mount_attr *>(uattr),
+			  usize) < 0)
+		throw MakeErrno("mount_setattr() failed");
+}
+
+void
+MountSetAttr(FileDescriptor directory, const char *path, unsigned flags,
+	     uint_least64_t attr_set, uint_least64_t attr_clr,
+	     uint_least64_t propagation)
+{
+	const struct mount_attr attr{
+		.attr_set = attr_set,
+		.attr_clr = attr_clr,
+		.propagation = propagation,
+	};
+
+	static_assert(sizeof(attr) >= MOUNT_ATTR_SIZE_VER0);
+
+	MountSetAttr(directory, path, flags,
+		     &attr, MOUNT_ATTR_SIZE_VER0);
+}
 
 void
 MountOrThrow(const char *source, const char *target,
--- src/system/Mount.hxx
+++ src/system/Mount.hxx
@@ -4,6 +4,61 @@
 
 #pragma once
 
+#include <sys/mount.h>
+
+#include <cstddef> // for std::size_t
+#include <cstdint> // for uint_least64_t
+
+#ifndef MOUNT_ATTR_RDONLY
+/* fallback definitions for glibc < 2.36 */
+#define MOUNT_ATTR_RDONLY 0x00000001
+#define MOUNT_ATTR_NOSUID 0x00000002
+#define MOUNT_ATTR_NODEV 0x00000004
+#define MOUNT_ATTR_NOEXEC 0x00000008
+#define FSCONFIG_SET_STRING 1
+#define FSCONFIG_CMD_CREATE 6
+#define MOVE_MOUNT_F_EMPTY_PATH 0x00000004
+#endif
+
+#ifndef AT_SYMLINK_NOFOLLOW
+#define AT_SYMLINK_NOFOLLOW 0x100
+#endif
+
+#ifndef AT_NO_AUTOMOUNT
+#define AT_NO_AUTOMOUNT 0x800
+#endif
+
+#ifndef AT_RECURSIVE
+#define AT_RECURSIVE 0x8000
+#endif
+
+class FileDescriptor;
+class UniqueFileDescriptor;
+
+UniqueFileDescriptor
+FSOpen(const char *fsname);
+
+void
+FSConfig(FileDescriptor fs, unsigned cmd,
+	 const char *key, const char *value, int aux=0);
+
+UniqueFileDescriptor
+FSMount(FileDescriptor fs, unsigned flags);
+
+void
+MoveMount(FileDescriptor from_directory, const char *from_path,
+	  FileDescriptor to_directory, const char *to_path,
+	  unsigned flags);
+
+void
+MountSetAttr(FileDescriptor directory, const char *path, unsigned flags,
+	     const struct mount_attr *uattr, std::size_t usize);
+
+void
+MountSetAttr(FileDescriptor directory, const char *path, unsigned flags,
+	     uint_least64_t attr_set, uint_least64_t attr_clr,
+	     uint_least64_t propagation=0);
+
 void
 MountOrThrow(const char *source, const char *target,
 	     const char *filesystemtype, unsigned long mountflags,
