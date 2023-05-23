eabbed376f975d7d47c4ff9c2e23e3d42adb4215
Max Kellermann <mk@cm4all.com>
2023-05-23 10:12:38 +0200

    was/Splice: add SpliceFromWas()

--- src/was/Splice.cxx
+++ src/was/Splice.cxx
@@ -19,6 +19,57 @@ extern "C" {
 #include <stdio.h>
 
 bool
+SpliceFromWas(was_simple *w, FileDescriptor out_fd) noexcept
+{
+	const FileDescriptor in_fd(was_simple_input_fd(w));
+	constexpr std::size_t max_len = 1 << 30;
+
+	while (true) {
+		const auto poll_result = was_simple_input_poll(w, -1);
+		switch (poll_result) {
+		case WAS_SIMPLE_POLL_SUCCESS:
+			break;
+
+		case WAS_SIMPLE_POLL_ERROR:
+			fprintf(stderr, "Error reading HTTP request body.\n");
+			return false;
+
+		case WAS_SIMPLE_POLL_TIMEOUT:
+			fprintf(stderr, "Timeout reading HTTP request body.\n");
+			return false;
+
+		case WAS_SIMPLE_POLL_END:
+			return true;
+
+		case WAS_SIMPLE_POLL_CLOSED:
+			fprintf(stderr, "Client has closed the PUT request body.\n");
+			return false;
+		}
+
+		const auto remaining = was_simple_input_remaining(w);
+		auto nbytes = splice(in_fd.Get(), nullptr,
+				     out_fd.Get(), nullptr,
+				     remaining > 0
+				     ? std::min<uint_least64_t>(remaining, max_len)
+				     : max_len,
+				     SPLICE_F_MOVE|SPLICE_F_NONBLOCK);
+		if (nbytes < 0) {
+			if (errno == EAGAIN)
+				continue;
+
+			fprintf(stderr, "Error copying HTTP request body: %s\n",
+				strerror(errno));
+			return false;
+		}
+
+		if (!was_simple_received(w, nbytes)) {
+			fprintf(stderr, "Error receiving HTTP request body\n");
+			return false;
+		}
+	}
+}
+
+bool
 SpliceToWas(was_simple *w, FileDescriptor in_fd, uint_least64_t remaining) noexcept
 {
 	if (remaining == 0)
--- src/was/Splice.hxx
+++ src/was/Splice.hxx
@@ -10,4 +10,7 @@ struct was_simple;
 class FileDescriptor;
 
 bool
+SpliceFromWas(was_simple *w, FileDescriptor out_fd) noexcept;
+
+bool
 SpliceToWas(was_simple *w, FileDescriptor in_fd, uint_least64_t size) noexcept;
