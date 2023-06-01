027e6b5cf2ff7dae437ce4f014b08eb906fe0e0d
Max Kellermann <mk@cm4all.com>
2023-06-01 09:07:06 +0200

    lib/fmt/TimeFormatter: new library

---
+++ src/lib/fmt/TimeFormatter.hxx
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// author: Max Kellermann <max.kellermann@gmail.com>
+
+#pragma once
+
+#include "time/ISO8601.hxx"
+
+#include <fmt/format.h>
+
+template<>
+struct fmt::formatter<std::chrono::system_clock::time_point> : formatter<const char *>
+{
+	template<typename FormatContext>
+	auto format(std::chrono::system_clock::time_point tp, FormatContext &ctx) {
+		return formatter<const char *>::format(FormatISO8601(tp).c_str(), ctx);
+	}
+};
