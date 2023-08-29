ef8d32b784c678eee0ffa0a32feff6a1b575d644
Max Kellermann <mk@cm4all.com>
2023-08-29 15:11:03 +0200

    util/djb_hash: add `hot` attributes

--- src/util/djb_hash.cxx
+++ src/util/djb_hash.cxx
@@ -6,13 +6,14 @@
 
 #include <cassert>
 
-[[gnu::always_inline]]
+[[gnu::always_inline]] [[gnu::hot]]
 static constexpr std::size_t
 djb_hash_update(std::size_t hash, std::byte b) noexcept
 {
 	return (hash * 33) ^ static_cast<std::size_t>(b);
 }
 
+[[gnu::hot]]
 std::size_t
 djb_hash(std::span<const std::byte> src) noexcept
 {
@@ -24,6 +25,7 @@ djb_hash(std::span<const std::byte> src) noexcept
 	return hash;
 }
 
+[[gnu::hot]]
 std::size_t
 djb_hash_string(const char *p) noexcept
 {
