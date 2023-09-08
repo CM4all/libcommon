c9aba74afb22716a111d305021d2e0410d048729
Max Kellermann <mk@cm4all.com>
2023-09-08 19:16:03 +0200

    pg/SharedConnection: new class

---
+++ src/pg/SharedConnection.cxx
@@ -0,0 +1,148 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// Copyright CM4all GmbH
+// author: Max Kellermann <mk@cm4all.com>
+
+#include "SharedConnection.hxx"
+
+namespace Pg {
+
+void
+SharedConnection::ScheduleQuery(SharedConnectionQuery &query) noexcept
+{
+	assert(!query.IsScheduled());
+
+	const bool was_empty = queries.empty();
+	queries.push_back(query);
+
+	/* connect if we're not already connected, or reconnect really
+	   soon if reconnect is pending (skip the reconnect delay) */
+	connection.MaybeScheduleConnect();
+
+	if (was_empty && connection.IsDefined() && connection.IsIdle()) {
+		assert(!submitted);
+		defer_submit_next.Schedule();
+	}
+}
+
+void
+SharedConnection::CancelQuery(SharedConnectionQuery &query) noexcept
+{
+	assert(!queries.empty());
+	assert(query.IsScheduled());
+
+	const bool was_submitted = WasSubmitted(query);
+	submitted = false;
+
+	queries.erase(queries.iterator_to(query));
+
+	if (was_submitted) {
+		/* if the query currently "owns" the connection, it is
+		   usually not idle, but maybe it's waiting for
+		   something else inbetween two queries, so we need to
+		   check anyway */
+		if (!connection.IsIdle())
+			connection.RequestCancel();
+
+		/* submit the next query (outside of this caller
+		   chain, using the DeferEvent) */
+		if (!queries.empty())
+			defer_submit_next.Schedule();
+	}
+}
+
+void
+SharedConnection::SubmitNext() noexcept
+{
+	assert(connection.IsDefined());
+	assert(connection.IsIdle());
+	assert(!submitted);
+	assert(!queries.empty());
+
+	defer_submit_next.Cancel();
+
+	auto &query = queries.front();
+
+	try {
+		query.OnPgConnectionAvailable(connection);
+		submitted = true;
+	} catch (...) {
+		queries.erase(queries.iterator_to(query));
+		query.OnPgError(std::current_exception());
+
+		if (!queries.empty() && connection.IsIdle())
+			/* this one failed for some reason, but the
+			   connection is still alive - submit the next
+			   one */
+			defer_submit_next.Schedule();
+	}
+}
+
+void
+SharedConnection::OnConnect()
+{
+	assert(connection.IsDefined());
+	assert(connection.IsIdle());
+	assert(!defer_submit_next.IsPending());
+	assert(!submitted);
+
+	if (!queries.empty())
+		SubmitNext();
+}
+
+void
+SharedConnection::OnIdle()
+{
+	assert(connection.IsDefined());
+	assert(connection.IsIdle());
+}
+
+void
+SharedConnection::OnDisconnect() noexcept
+{
+	defer_submit_next.Cancel();
+
+	if (submitted) {
+		/* just in case the current query hasn't cancelled
+		   itself yet */
+		// TODO convert to assert(!submitted)
+		assert(!queries.empty());
+		queries.pop_front();
+		submitted = false;
+	}
+}
+
+void
+SharedConnection::OnNotify(const char *name)
+{
+	// TODO support notifications
+	(void)name;
+}
+
+void
+SharedConnection::OnError(std::exception_ptr e) noexcept
+{
+	defer_submit_next.Cancel();
+
+	if (submitted) {
+		/* the query was already submitted, thus we don't need
+		   to call SharedConnectionQuery::OnPgError(); the
+		   class will receive error information via
+		   AsyncResultHandler */
+		assert(!queries.empty());
+		queries.pop_front();
+		submitted = false;
+	} else if (!queries.empty()) {
+		/* the query was not yet submitted; abort it */
+		auto &query = queries.pop_front();
+		query.OnPgError(e);
+
+		/* note that we don't cancel more queries that may
+		   still be queued; AsyncConnection will now schedule
+		   a reconnect timer and the other queries will run
+		   later */
+	}
+
+	handler.OnPgError(std::move(e));
+}
+
+} /* namespace Pg */
---
+++ src/pg/SharedConnection.hxx
@@ -0,0 +1,128 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// Copyright CM4all GmbH
+// author: Max Kellermann <mk@cm4all.com>
+
+#pragma once
+
+#include "AsyncConnection.hxx"
+#include "event/DeferEvent.hxx"
+#include "util/IntrusiveList.hxx"
+
+namespace Pg {
+
+/**
+ * Handler class for #SharedConnection.
+ */
+class SharedConnectionHandler {
+public:
+	/**
+	 * An error has occurred (may be fatal or not), and the
+	 * handler can implement this method to log the error
+	 * condition.
+	 *
+	 * If this was a fatal error which closed previously
+	 * successful connection, then OnDisconnect() will be called
+	 * right after this method.
+	 */
+	virtual void OnPgError(std::exception_ptr e) noexcept = 0;
+};
+
+class SharedConnection;
+
+/**
+ * A query (or multiple queries); derive from this class and pass it
+ * to SharedConnection::ScheduleQuery().  As soon as the connection
+ * becomes available, OnPgConnectionAvailable() is invoked, or
+ * OnPgError().  This class may then send queries and must call
+ * Cancel() to release the connection.
+ */
+class SharedConnectionQuery {
+	friend class SharedConnection;
+	IntrusiveListHook<IntrusiveHookMode::TRACK> shared_connection_query_siblings;
+
+	SharedConnection &shared_connection;
+
+public:
+	explicit SharedConnectionQuery(SharedConnection &_shared_connection) noexcept
+		:shared_connection(_shared_connection) {}
+
+	~SharedConnectionQuery() noexcept {
+		Cancel();
+	}
+
+	bool IsScheduled() const noexcept {
+		return shared_connection_query_siblings.is_linked();
+	}
+
+	void Cancel() noexcept;
+
+	/**
+	 * The connection has become available.  This method may
+	 * submit the actual query to the #AsyncConnection and wait
+	 * for results.  Call Cancel() (or destruct this object) when
+	 * you're done.
+	 *
+	 * Exceptions thrown by this method will be passed to
+	 * OnPgError().
+	 */
+	virtual void OnPgConnectionAvailable(AsyncConnection &connection) = 0;
+
+	virtual void OnPgError(std::exception_ptr error) noexcept = 0;
+};
+
+/**
+ * An extension of #AsyncConnection which manages a queue of queries
+ * to be submitted.
+ */
+class SharedConnection : public AsyncConnectionHandler {
+	AsyncConnection connection;
+
+	DeferEvent defer_submit_next;
+
+	SharedConnectionHandler &handler;
+
+	IntrusiveList<SharedConnectionQuery,
+		      IntrusiveListMemberHookTraits<&SharedConnectionQuery::shared_connection_query_siblings>> queries;
+
+	bool submitted = false;
+
+public:
+	SharedConnection(EventLoop &event_loop,
+			 const char *conninfo, const char *schema,
+			 SharedConnectionHandler &_handler) noexcept
+		:connection(event_loop, conninfo, schema, *this),
+		 defer_submit_next(event_loop, BIND_THIS_METHOD(SubmitNext)),
+		 handler(_handler)
+	{
+	}
+
+	auto &GetEventLoop() const noexcept {
+		return connection.GetEventLoop();
+	}
+
+	void ScheduleQuery(SharedConnectionQuery &query) noexcept;
+	void CancelQuery(SharedConnectionQuery &query) noexcept;
+
+private:
+	bool WasSubmitted(const SharedConnectionQuery &query) const noexcept {
+		return submitted && &query == &queries.front();
+	}
+
+	void SubmitNext() noexcept;
+
+	/* virtual methods from class Pg::AsyncConnectionHandler */
+	void OnConnect() override;
+	void OnIdle() override;
+	void OnDisconnect() noexcept override;
+	void OnNotify(const char *name) override;
+	void OnError(std::exception_ptr e) noexcept override;
+};
+
+inline void
+SharedConnectionQuery::Cancel() noexcept
+{
+	if (IsScheduled())
+		shared_connection.CancelQuery(*this);
+}
+
+} /* namespace Pg */
