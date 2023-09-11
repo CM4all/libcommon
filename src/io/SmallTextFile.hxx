e0c2033a2dee35df4ac5eeecabc57e47e5b897ec
Max Kellermann <mk@cm4all.com>
2023-09-11 14:27:12 +0200

    io/SmallTextFile: move code to class SmallTextFileBuffer

--- src/io/SmallTextFile.hxx
+++ src/io/SmallTextFile.hxx
@@ -8,8 +8,30 @@
 #include "system/Error.hxx"
 #include "util/IterableSplitString.hxx"
 
+#include <array>
 #include <concepts>
 #include <string_view>
+
+template<std::size_t buffer_size>
+class SmallTextFileBuffer {
+	std::array<char, buffer_size> buffer;
+	std::size_t fill;
+
+public:
+	explicit SmallTextFileBuffer(auto &&file) {
+		fill = WithReadOnly(file, [this](auto fd){
+			const auto nbytes = fd.ReadAt(0, buffer.data(), buffer.size());
+			if (nbytes < 0)
+				throw MakeErrno("Failed to read file");
+
+			return static_cast<std::size_t>(nbytes);
+		});
+	}
+
+	operator std::string_view() const noexcept {
+		return {buffer.data(), fill};
+	}
+};
 
 /**
  * Read the specified file into a small stack buffer and pass it as
@@ -22,16 +44,8 @@ template<std::size_t buffer_size>
 decltype(auto)
 WithSmallTextFile(auto &&file, std::invocable<std::string_view> auto f)
 {
-	char buffer[buffer_size];
-	std::size_t n = WithReadOnly(file, [&buffer](auto fd){
-		const auto nbytes = fd.ReadAt(0, buffer, buffer_size);
-		if (nbytes < 0)
-			throw MakeErrno("Failed to read file");
-
-		return static_cast<std::size_t>(nbytes);
-	});
-
-	return f(std::string_view{buffer, n});
+	const SmallTextFileBuffer<buffer_size> buffer{file};
+	return f(std::string_view{buffer});
 }
 
 /**
