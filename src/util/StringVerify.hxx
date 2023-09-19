576351da5420cf009e564e9f9be5c50332ed9fc0
Max Kellermann <mk@cm4all.com>
2023-09-19 14:56:00 +0200

    util/StringVerify: use std::predicate

--- src/util/StringVerify.hxx
+++ src/util/StringVerify.hxx
@@ -5,33 +5,31 @@
 #pragma once
 
 #include <algorithm>
+#include <concepts>
 #include <string_view>
 
 /**
  * Does the given string consist only of characters allowed by the
  * given function?
  */
-template<typename F>
 constexpr bool
-CheckChars(std::string_view s, F &&f) noexcept
+CheckChars(std::string_view s, std::predicate<char> auto f) noexcept
 {
-	return std::all_of(s.begin(), s.end(), std::forward<F>(f));
+	return std::all_of(s.begin(), s.end(), f);
 }
 
 /**
  * Is the given string non-empty and consists only of characters
  * allowed by the given function?
  */
-template<typename F>
 constexpr bool
-CheckCharsNonEmpty(std::string_view s, F &&f) noexcept
+CheckCharsNonEmpty(std::string_view s, std::predicate<char> auto f) noexcept
 {
-	return !s.empty() && CheckChars(s, std::forward<F>(f));
+	return !s.empty() && CheckChars(s, f);
 }
 
-template<typename F>
 constexpr bool
-CheckCharsNonEmpty(const char *s, F &&f) noexcept
+CheckCharsNonEmpty(const char *s, std::predicate<char> auto f) noexcept
 {
 	do {
 		if (!f(*s))
