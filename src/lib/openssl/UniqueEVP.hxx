840fdf90d13db575e6a9d10394a6666bb559e61a
Max Kellermann <mk@cm4all.com>
2023-10-27 21:35:34 +0200

    lib/openssl/UniqueEVP: add EVP_CIPHER_CTX wrapper

--- src/lib/openssl/UniqueEVP.hxx
+++ src/lib/openssl/UniqueEVP.hxx
@@ -16,10 +16,15 @@ struct EVPDeleter {
 	void operator()(EVP_PKEY_CTX *key) noexcept {
 		EVP_PKEY_CTX_free(key);
 	}
+
+	void operator()(EVP_CIPHER_CTX *key) noexcept {
+		EVP_CIPHER_CTX_free(key);
+	}
 };
 
 using UniqueEVP_PKEY = std::unique_ptr<EVP_PKEY, EVPDeleter>;
 using UniqueEVP_PKEY_CTX = std::unique_ptr<EVP_PKEY_CTX, EVPDeleter>;
+using UniqueEVP_CIPHER_CTX = std::unique_ptr<EVP_CIPHER_CTX, EVPDeleter>;
 
 static inline auto
 UpRef(EVP_PKEY &key) noexcept
