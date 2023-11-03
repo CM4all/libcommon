0a661c7de0d55a33f53b821ff904921556d45fbe
Max Kellermann <mk@cm4all.com>
2023-11-03 19:05:46 +0100

    jwt/OsslJWK: move code to RSAToJWK()

--- src/jwt/OsslJWK.cxx
+++ src/jwt/OsslJWK.cxx
@@ -18,11 +18,10 @@
 
 using std::string_view_literals::operator""sv;
 
-nlohmann::json
-ToJWK(const EVP_PKEY &key)
+static nlohmann::json
+RSAToJWK(const EVP_PKEY &key)
 {
-	if (EVP_PKEY_get_base_id(&key) != EVP_PKEY_RSA)
-		throw std::invalid_argument{"RSA key expected"};
+	assert(EVP_PKEY_get_base_id(&key) == EVP_PKEY_RSA);
 
 	BIGNUM *n = nullptr;
 	if (!EVP_PKEY_get_bn_param(&key, OSSL_PKEY_PARAM_RSA_N, &n))
@@ -41,4 +40,16 @@ ToJWK(const EVP_PKEY &key)
 		{ "kty"sv, "RSA"sv },
 		{ "n"sv, UrlSafeBase64(SslBuffer{*n}.get()).c_str() },
 	};
+}
+
+nlohmann::json
+ToJWK(const EVP_PKEY &key)
+{
+	switch (EVP_PKEY_get_base_id(&key)) {
+	case EVP_PKEY_RSA:
+		return RSAToJWK(key);
+
+	default:
+		throw std::invalid_argument{"RSA key expected"};
+	}
 }
