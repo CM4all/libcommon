dc5d32597610f1cc7c9cf898b653799e97c2bf2d
Max Kellermann <mk@cm4all.com>
2024-02-21 22:19:00 +0100

    system/PidFD: add my_pidfd_open()

--- src/system/PidFD.h
+++ src/system/PidFD.h
@@ -9,6 +9,12 @@
 #include <unistd.h>
 
 static inline int
+my_pidfd_open(pid_t pid, unsigned int flags) noexcept
+{
+	return syscall(__NR_pidfd_open, pid, flags);
+}
+
+static inline int
 my_pidfd_send_signal(int pidfd, int sig, siginfo_t *info,
 		     unsigned int flags) noexcept
 {
