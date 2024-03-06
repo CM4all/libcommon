316062b43fae545ab76bcf20785fdf1657031e45
Max Kellermann <mk@cm4all.com>
2024-03-06 15:12:54 +0100

    lib/sodium/Box: wrapper for crypto_box_seal()

---
+++ src/lib/sodium/Box.hxx
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// Copyright CM4all GmbH
+// author: Max Kellermann <mk@cm4all.com>
+
+#pragma once
+
+#include "BoxKey.hxx"
+
+#include <sodium/crypto_box.h>
+
+static inline void
+crypto_box_seal(std::byte *c,
+		std::span<const std::byte> m,
+		CryptoBoxPublicKeyView pk)
+{
+	crypto_box_seal(reinterpret_cast<unsigned char *>(c),
+			reinterpret_cast<const unsigned char *>(m.data()),
+			m.size(),
+			reinterpret_cast<const unsigned char *>(pk.data()));
+}
