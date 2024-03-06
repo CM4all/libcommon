2aef58a33ae7e08ab631cf9fe04b7e7d16df83db
Max Kellermann <mk@cm4all.com>
2024-03-06 12:00:31 +0100

    lua/sodium/RandomBytes: suppress bogus GCC warning

--- src/lua/sodium/RandomBytes.cxx
+++ src/lua/sodium/RandomBytes.cxx
@@ -25,6 +25,12 @@ randombytes(lua_State *L)
 	luaL_argcheck(L, size >= 1, 1, "Size is too small");
 	luaL_argcheck(L, size <= 1024 * 1024, 1, "Size is too large");
 
+	/* GCC 12 gets confused by the range checks above and emits a
+	   bogus warning */
+#if defined(__GNUC__) && !defined(__clang__)
+#pragma GCC diagnostic ignored "-Walloc-size-larger-than="
+#endif
+
 	auto dest = std::make_unique<char[]>(size);
 	randombytes_buf(dest.get(), size);
 
