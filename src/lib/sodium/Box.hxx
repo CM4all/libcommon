09dd3a5783cff86cc9e4221031a15f2445d9df6e
Max Kellermann <mk@cm4all.com>
2024-03-11 17:36:23 +0100

    lib/sodium/Box: add wrappers for crypto_box_keypair(), crypto_box_seal_open()

--- src/lib/sodium/Box.hxx
+++ src/lib/sodium/Box.hxx
@@ -9,6 +9,14 @@
 #include <sodium/crypto_box.h>
 
 static inline void
+crypto_box_keypair(CryptoBoxPublicKeyBuffer pk,
+		   CryptoBoxSecretKeyBuffer sk) noexcept
+{
+	crypto_box_keypair(reinterpret_cast<unsigned char *>(pk.data()),
+			   reinterpret_cast<unsigned char *>(sk.data()));
+}
+
+static inline void
 crypto_box_seal(std::byte *c,
 		std::span<const std::byte> m,
 		CryptoBoxPublicKeyView pk) noexcept
@@ -17,4 +25,18 @@ crypto_box_seal(std::byte *c,
 			reinterpret_cast<const unsigned char *>(m.data()),
 			m.size(),
 			reinterpret_cast<const unsigned char *>(pk.data()));
+}
+
+[[nodiscard]]
+static inline int
+crypto_box_seal_open(std::byte *m,
+		     std::span<const std::byte> c,
+		     CryptoBoxPublicKeyView pk,
+		     CryptoBoxSecretKeyView sk) noexcept
+{
+	return crypto_box_seal_open(reinterpret_cast<unsigned char *>(m),
+				    reinterpret_cast<const unsigned char *>(c.data()),
+				    c.size(),
+				    reinterpret_cast<const unsigned char *>(pk.data()),
+				    reinterpret_cast<const unsigned char *>(sk.data()));
 }
