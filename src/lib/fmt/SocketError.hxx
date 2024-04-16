7350978f267312c28fd977eeceeef1db668eb921
Max Kellermann <mk@cm4all.com>
2024-04-16 12:38:00 +0200

    lib/fmt: require libfmt 9
    Version 9 added the "debug format" which I'd like to use.

--- src/lib/fmt/SocketError.hxx
+++ src/lib/fmt/SocketError.hxx
@@ -24,14 +24,8 @@ std::system_error
 FmtSocketError(socket_error_t code,
 	       const S &format_str, Args&&... args) noexcept
 {
-#if FMT_VERSION >= 90000
 	return VFmtSocketError(code, format_str,
 			       fmt::make_format_args(args...));
-#else
-	return VFmtSocketError(code, fmt::to_string_view(format_str),
-			       fmt::make_args_checked<Args...>(format_str,
-							       args...));
-#endif
 }
 
 template<typename S, typename... Args>
