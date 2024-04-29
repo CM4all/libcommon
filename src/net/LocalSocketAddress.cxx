CM4all/libcommon

commit 3948e08be93e6bc7984b70456bc9cb13416fe9be

net/SocketAddress: use std::string_view::find() instead of std::memchr()

src/net/SocketAddress.cxx
@@ -59,7 +59,7 @@ SocketAddress::GetLocalPath() const noexcept
 		/* must be null-terminated */
 		raw.back() == 0 &&
 		/* there must not be any other null byte */
-		std::memchr(raw.data(), 0, raw.size() - 1) == nullptr
+		raw.find('\0') == raw.npos
 		? raw.data()
 		: nullptr;
 }

commit bca956b3df50efd76e0b789a48a32e044da1875e

net/SocketAddress: fix off-by-one bug in GetLocalPath()
Regression by commit 3948e08be93e6bc7984b70456bc9cb13416fe9be

src/net/LocalSocketAddress.cxx
@@ -10,11 +10,9 @@ LocalSocketAddress::GetLocalPath() const noexcept
 	return !raw.empty() &&
 		/* must be an absolute path */
 		raw.front() == '/' &&
-		/* must be null-terminated */
-		raw.back() == 0 &&
-		/* there must not be any other null byte */
-		raw.find('\0') == raw.npos
+		/* must be null-terminated and there must not be any
+		   other null byte */
+		raw.find('\0') == raw.size()
 		? raw.data()
 		: nullptr;
 }
-

src/net/SocketAddress.cxx
@@ -56,10 +56,9 @@ SocketAddress::GetLocalPath() const noexcept
 	return !raw.empty() &&
 		/* must be an absolute path */
 		raw.front() == '/' &&
-		/* must be null-terminated */
-		raw.back() == 0 &&
-		/* there must not be any other null byte */
-		raw.find('\0') == raw.npos
+		/* must be null-terminated and there must not be any
+		   other null byte */
+		raw.find('\0') == raw.size()
 		? raw.data()
 		: nullptr;
 }