5507d3519832828456b3f54d611bf93a912937af
Max Kellermann <mk@cm4all.com>
2024-07-02 09:56:31 +0200

    lib/fmt/ExceptionFormatter: un-inline to reduce header dependencies

---
+++ src/lib/fmt/ExceptionFormatter.cxx
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// author: Max Kellermann <max.kellermann@gmail.com>
+
+#include "ExceptionFormatter.hxx"
+#include "util/Exception.hxx"
+
+auto fmt::formatter<std::exception_ptr>::format(std::exception_ptr e, format_context &ctx)
+ -> format_context::iterator
+{
+	return formatter<string_view>::format(GetFullMessage(e), ctx);
+}
