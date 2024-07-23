7bf9d30853697ffa1de42c9f0776a641035d8807
Max Kellermann <max.kellermann@ionos.com>
2024-07-23 10:01:11 +0200

    lib/fmt/TimeFormatter: add `const` for fmt 11 support

--- src/lib/fmt/TimeFormatter.hxx
+++ src/lib/fmt/TimeFormatter.hxx
@@ -12,7 +12,7 @@ template<>
 struct fmt::formatter<std::chrono::system_clock::time_point> : formatter<const char *>
 {
 	template<typename FormatContext>
-	auto format(std::chrono::system_clock::time_point tp, FormatContext &ctx) {
+	auto format(std::chrono::system_clock::time_point tp, FormatContext &ctx) const {
 		return formatter<const char *>::format(FormatISO8601(tp).c_str(), ctx);
 	}
 };
