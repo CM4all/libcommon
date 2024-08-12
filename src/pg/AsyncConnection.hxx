CM4all/libcommon

commit e6ec15fc6408d7458799e7daa75ae84a39de80c4

pg/AsyncConnection: add method IsBusy()

src/pg/AsyncConnection.hxx
@@ -218,6 +218,16 @@ public:
 	}
 
 	/**
+	 * Returns true if a query is currently in progress.  In this
+         * case, RequestCancel() may be called.
+	 *
+	 * Note that this is not the strict opposite of IsIdle().
+	 */
+	bool IsBusy() const noexcept {
+		return result_handler != nullptr;
+	}
+
+	/**
 	 * Call this after catching a fatal connection error.  This
 	 * will close the connection, notify the handler and schedule
 	 * a reconnect.

commit d1de30e18dc39d0def503ad8a56cf2f0e67fb0dc

pg/AsyncConnection: rename IsBusy() to IsRequestPending()
Fixes a regression by commit e6ec15fc6408d74 which added an IsBusy()
method which shadowed Connection::IsBusy().

src/pg/AsyncConnection.hxx
@@ -223,7 +223,7 @@ public:
 	 *
 	 * Note that this is not the strict opposite of IsIdle().
 	 */
-	bool IsBusy() const noexcept {
+	bool IsRequestPending() const noexcept {
 		return result_handler != nullptr;
 	}
 

src/pg/SharedConnection.cxx
@@ -39,7 +39,7 @@ SharedConnection::CancelQuery(SharedConnectionQuery &query) noexcept
 		   usually not idle, but maybe it's waiting for
 		   something else inbetween two queries, so we need to
 		   check anyway */
-		if (connection.IsBusy())
+		if (connection.IsRequestPending())
 			connection.RequestCancel();
 
 		/* submit the next query (outside of this caller