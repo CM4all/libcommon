CM4all/libcommon

commit e6e4805a694017acd16ccb1b453cc0b56d25bfcf

spawn/Direct: retry after clone3()==ENOENT
Yes, clone3() can fail with ENOENT due to this piece of code in
kernel/cgroup/cgroup.c:

    if (!css || !css_tryget_online(css))
        css = ERR_PTR(-ENOENT);

If that happens (or setting up cgroup controller settings fails with
ENOENT), retry twice and hope the spawn-reaper doesn't do it again
(which is extremely unlikely because it won't get a notification for
the new-but-still-empty cgroup).

src/spawn/Direct.cxx
@@ -515,30 +515,49 @@ SpawnChildProcess(PreparedChildProcess &&params,
 		? params.cgroup->name
 		: nullptr;
 
-	UniqueFileDescriptor cgroup_fd;
-	if (params.cgroup != nullptr) {
-		const ScopeUmask scope_umask{
-			cgroups_group_writable ? mode_t{0002} : mode_t{0022}
-		};
+	long pid;
+	for (unsigned retries = 2;;) {
+		try {
+			UniqueFileDescriptor cgroup_fd;
+			if (params.cgroup != nullptr) {
+				const ScopeUmask scope_umask{
+					cgroups_group_writable ? mode_t{0002} : mode_t{0022}
+				};
 
-		cgroup_fd = params.cgroup->Create2(cgroup_state,
-						   params.cgroup_session);
-		if (cgroup_fd.IsDefined()) {
-			ca.flags |= CLONE_INTO_CGROUP;
-			ca.cgroup = cgroup_fd.Get();
+				cgroup_fd = params.cgroup->Create2(cgroup_state,
+								   params.cgroup_session);
+				if (cgroup_fd.IsDefined()) {
+					ca.flags |= CLONE_INTO_CGROUP;
+					ca.cgroup = cgroup_fd.Get();
+				}
+			}
 
 			if (params.return_cgroup.IsDefined())
-				EasySendMessage(params.return_cgroup,
-						cgroup_fd);
+				params.return_cgroup.Close();
+
+			pid = clone3(&ca, sizeof(ca));
+			if (pid < 0)
+				throw MakeErrno("clone() failed");
+
+			if (cgroup_fd.IsDefined() && params.return_cgroup.IsDefined())
+				EasySendMessage(params.return_cgroup, cgroup_fd);
+
+			break;
+		} catch (const std::system_error &e) {
+			if (IsFileNotFound(e) && retries-- > 0)
+				/* this can happen if the existing
+                                   cgroup was deleted by the
+                                   spawn-reaper daemon after it was
+                                   opened by us; setting up the cgroup
+                                   or even clone3() can fail with this
+                                   error (even though ENOENT is not
+                                   documented for clone3()) -
+                                   workaround: retry this part */
+				continue;
+
+			throw;
 		}
 	}
-
-	if (params.return_cgroup.IsDefined())
-		params.return_cgroup.Close();
-
-	long pid = clone3(&ca, sizeof(ca));
-	if (pid < 0)
-		throw MakeErrno("clone() failed");
 
 	if (pid == 0) {
 		userns_map_pipe_w.Close();

commit 268c2e9497c9769304d4deddbbc4d536dab68700

spawn/Direct: send return_cgroup message only in parent
This fixes another regression by commit
e6e4805a694017acd16ccb1b453cc0b56d25bfcf

src/spawn/Direct.cxx
@@ -560,7 +560,7 @@ SpawnChildProcess(PreparedChildProcess &&params,
 			if (pid < 0)
 				throw MakeErrno("clone() failed");
 
-			if (cgroup_fd.IsDefined() && params.return_cgroup.IsDefined())
+			if (pid > 0 && cgroup_fd.IsDefined() && params.return_cgroup.IsDefined())
 				EasySendMessage(params.return_cgroup, cgroup_fd);
 
 			break;