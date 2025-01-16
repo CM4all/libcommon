4b9807104a864912ac081b2f0752179d0b40a35c
Max Kellermann <max.kellermann@ionos.com>
2025-01-16 12:52:12 +0100

    event/net/BufferedSocket: set IOSQE_ASYNC for io_uring receive operations

--- src/event/net/BufferedSocket.cxx
+++ src/event/net/BufferedSocket.cxx
@@ -75,6 +75,11 @@ BufferedSocket::UringReceive::Start()
 
 	auto &s = queue.RequireSubmitEntry();
 	io_uring_prep_recv(&s, parent.GetSocket().Get(), w.data(), w.size(), 0);
+
+	/* always go async; this way, the overhead for the operation
+           does not cause latency in the main thread */
+	io_uring_sqe_set_flags(&s, IOSQE_ASYNC);
+
 	queue.Push(s, *this);
 }
 
