fec4ac2fb2a4470ed281930ca636363d28934c59
Max Kellermann <max.kellermann@ionos.com>
2025-01-17 12:23:27 +0100

    util/DecimalFormat: remove unused function format_uint64()
    fmt::format_int is better.

--- src/util/DecimalFormat.hxx
+++ src/util/DecimalFormat.hxx
@@ -23,24 +23,3 @@ format_4digit(char *dest, uint_least32_t number) noexcept
 	dest[2] = (char)('0' + (number / 10) % 10);
 	dest[3] = (char)('0' + number % 10);
 }
-
-/**
- * Format a 64 bit unsigned integer into a decimal string.
- */
-inline std::size_t
-format_uint64(char dest[32], uint_least64_t number) noexcept
-{
-	char *p = dest + 32 - 1;
-
-	*p = 0;
-	do {
-		--p;
-		*p = '0' + (char)(number % 10);
-		number /= 10;
-	} while (number != 0);
-
-	if (p > dest)
-		memmove(dest, p, dest + 32 - p);
-
-	return dest + 32 - p - 1;
-}
