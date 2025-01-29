08c06b1a78645744d6855cd50d9445cad77839d2
Max Kellermann <max.kellermann@ionos.com>
2025-01-29 11:55:56 +0100

    lib/openssl/EvpParam: use std::make_unique_for_overwrite()

--- src/lib/openssl/EvpParam.cxx
+++ src/lib/openssl/EvpParam.cxx
@@ -11,7 +11,7 @@ GetStringParam(const EVP_PKEY &key, const char *name)
 	if (!EVP_PKEY_get_utf8_string_param(&key, name, nullptr, 0, &length))
 		throw SslError{};
 
-	auto result = std::make_unique<char[]>(length + 1);
+	auto result = std::make_unique_for_overwrite<char[]>(length + 1);
 
 	if (!EVP_PKEY_get_utf8_string_param(&key, name, result.get(), length + 1, &length))
 		throw SslError{};
