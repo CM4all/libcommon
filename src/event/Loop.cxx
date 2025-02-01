4a1f1646ed1ddc8b66d2704164581040042789a3
Max Kellermann <max.kellermann@ionos.com>
2025-02-01 19:27:18 +0100

    event/Loop: manually disable io_uring in dtor to fix assertion failure

--- src/event/Loop.cxx
+++ src/event/Loop.cxx
@@ -36,6 +36,13 @@ EventLoop::EventLoop(
 
 EventLoop::~EventLoop() noexcept
 {
+#if defined(HAVE_URING) && !defined(NDEBUG)
+	/* if Run() was never called (maybe because startup failed and
+	   an exception is pending), we need to destruct the
+	   Uring::Manager here or else the assertions below fail */
+	uring.reset();
+#endif
+
 	assert(defer.empty());
 	assert(idle.empty());
 	assert(next.empty());
