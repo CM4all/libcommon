4c8c0c0cee41ccd1072015119514ba3b812649a9
Max Kellermann <max.kellermann@ionos.com>
2025-05-08 09:48:55 +0200

    lua/Chrono: add Push(duration)

--- src/lua/Chrono.hxx
+++ src/lua/Chrono.hxx
@@ -12,6 +12,24 @@ extern "C" {
 namespace std::chrono {
 
 /**
+ * Push an arbitrary duration to the Lua stack as a number of seconds.
+ * This may require rounding or casting to floating point.
+ */
+template<class Rep, class Period>
+static inline void
+Push(lua_State *L, duration<Rep, Period> value) noexcept
+{
+	if constexpr (std::is_integral_v<Rep> && Period::den == 1)
+		/* if it's an integer and the denominator is 1, it can
+		   be represented as a lua_Integer */
+		// TODO what about overflow?
+		lua_pushinteger(L, duration_cast<duration<lua_Integer>>(value).count());
+	else
+		/* everything else gets casted to floating point */
+		lua_pushnumber(L, duration_cast<duration<lua_Number>>(value).count());
+}
+
+/**
  * Push the time point to the Lua stack in the usual Lua time stamp
  * format (i.e. seconds since epoch).
  */
@@ -25,6 +43,12 @@ Push(lua_State *L, system_clock::time_point value) noexcept
 } // namespace std::chrono
 
 namespace Lua {
+
+static inline std::chrono::duration<lua_Number>
+ToDuration(lua_State *L, int idx)
+{
+	return std::chrono::duration<lua_Number>(lua_tonumber(L, idx));
+}
 
 static inline std::chrono::system_clock::time_point
 ToSystemTimePoint(lua_State *L, int idx)
--- test/lua/TestChrono.cxx
+++ test/lua/TestChrono.cxx
@@ -14,6 +14,27 @@ extern "C" {
 
 using namespace Lua;
 
+TEST(Chrono, Duration)
+{
+	const State main{luaL_newstate()};
+	const auto L = main.get();
+	const ScopeCheckStack check_stack{L};
+
+	Push(L, std::chrono::microseconds{5});
+	Push(L, std::chrono::milliseconds{4});
+	Push(L, std::chrono::hours{3});
+	Push(L, std::chrono::minutes{2});
+	Push(L, std::chrono::seconds{1});
+
+	EXPECT_EQ(ToDuration(L, -1), std::chrono::seconds{1});
+	EXPECT_EQ(ToDuration(L, -2), std::chrono::minutes{2});
+	EXPECT_EQ(ToDuration(L, -3), std::chrono::hours{3});
+	EXPECT_EQ(ToDuration(L, -4), std::chrono::milliseconds{4});
+	EXPECT_EQ(ToDuration(L, -5), std::chrono::microseconds{5});
+
+	lua_pop(L, 5);
+}
+
 TEST(Chrono, SystemClock)
 {
 	const State main{luaL_newstate()};
