ccc243dc32c05d73af737b3f269b3a5a4152ac9d
Max Kellermann <max.kellermann@ionos.com>
2025-05-19 20:44:36 +0200

    thread/Worker: configure 128 kB stack on ARM64
    The pthread_attr_setstacksize() call fails on ARM64 because 64 kB is
    smaller than PTHREAD_STACK_MIN.  glibc then ignores the call
    completely and allocates 8 MB stack size.  This is surprising because
    pthread_attr_setstacksize() is only supposed to set the minimum stack
    size, and it seems more reasonable to simply use PTHREAD_STACK_MIN if
    the parameter is smaller than that.  Duh!

--- src/thread/Worker.cxx
+++ src/thread/Worker.cxx
@@ -38,7 +38,12 @@ ThreadWorker::ThreadWorker(ThreadQueue &_queue)
 	AtScopeExit(&attr) { pthread_attr_destroy(&attr); };
 
 	/* 64 kB stack ought to be enough */
+#ifdef __aarch64__
+	/* PTHREAD_STACK_MIN is 128 kB on ARM64 */
+	pthread_attr_setstacksize(&attr, 128 * 1024);
+#else
 	pthread_attr_setstacksize(&attr, 65536);
+#endif
 
 	int error = pthread_create(&thread, &attr, Run, this);
 	if (error != 0)
