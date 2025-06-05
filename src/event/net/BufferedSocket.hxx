CM4all/libcommon

commit ee9a9fdc8908e59b9f04d1ecc5c3bdba8ce15878

event/net/BufferedSocket: add method AfterConsumed()
Fixes assertion failure in several users which call DisposeConsumed(),
leaving an empty input buffer.

src/event/net/BufferedSocket.hxx
@@ -528,6 +528,11 @@ public:
 	 * Dispose the specified number of bytes from the input
 	 * buffer.  Call this after ReadBuffer().  It may be called
 	 * repeatedly.
+	 *
+	 * If you call this outside of the
+	 * BufferedSocketHandler::OnBufferedData() callback, you need
+	 * to call AfterConsumed() when you are done, to update the
+	 * internals.
 	 */
 	void DisposeConsumed(std::size_t nbytes) noexcept;
 
@@ -536,8 +541,25 @@ public:
 	 * "consumed".  Call this after ReadBuffer().  Note that
 	 * this method does not invalidate the buffer obtained from
 	 * ReadBuffer().  It may be called repeatedly.
+	 *
+	 * If you call this outside of the
+	 * BufferedSocketHandler::OnBufferedData() callback, you need
+	 * to call AfterConsumed() when you are done, to update the
+	 * internals.
 	 */
 	void KeepConsumed(std::size_t nbytes) noexcept;
+
+	/**
+	 * Update internals after DisposeConsumed() or KeepConsumed()
+	 * has been called outside of
+	 * BufferedSocketHandler::OnBufferedData().
+	 */
+	void AfterConsumed() noexcept {
+		/* defer a read call which will check for "ended"; we
+		   can't do that here because this method is not
+		   allowed to fail */
+		DeferRead(false);
+	}
 
 	/**
 	 * Provides direct access to the input buffer.  The caller may

src/event/net/BufferedSocket.cxx
@@ -411,7 +411,14 @@ BufferedSocket::TryRead2() noexcept
 	assert(reading);
 
 	if (!IsConnected()) {
-		assert(!IsEmpty());
+		if (IsEmpty()) {
+			/* this can only happen if
+			   DisposeConsumed()/KeepConsumed() was called
+			   outside of the OnBufferedData() callback;
+			   now's the time for the "ended" check */
+			Ended();
+			return false;
+		}
 
 		SubmitFromBuffer();
 		return false;

commit 71fac175e026189bd21969950529a82596c7d44a

event/net/BufferedSocket: defer read from AfterConsumed() only if necessary
Commit ee9a9fdc8908 has introduced AfterConsumed() as a fix for an
assertion failure, but the implementation was too broad - this method
is only supposed to ensure that "ended" is checked, and that is only
necessary and relevant if we're not connected anymore.

This commit removes some unnecessary overhead by skipping excessive
OnBufferedData() calls.

src/event/net/BufferedSocket.hxx
@@ -574,7 +574,8 @@ public:
 		/* defer a read call which will check for "ended"; we
 		   can't do that here because this method is not
 		   allowed to fail */
-		DeferRead();
+		if (!IsConnected())
+			DeferRead();
 	}
 
 	/**