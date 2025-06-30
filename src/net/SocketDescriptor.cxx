f39f265a0ef4c46f57187fcaa15ed0f9d4613e29
Max Kellermann <max.kellermann@ionos.com>
2025-06-30 11:22:37 +0200

    net/*: include Features.hxx with "net/" prefix
    This is necessary if the header was generated at configure time.

--- src/net/SocketDescriptor.cxx
+++ src/net/SocketDescriptor.cxx
@@ -8,6 +8,7 @@
 #include "IPv6Address.hxx"
 #include "UniqueSocketDescriptor.hxx"
 #include "PeerCredentials.hxx"
+#include "net/Features.hxx" // for HAVE_TCP, HAVE_IPV6, HAVE_UN, ...
 
 #ifdef __linux__
 #include "io/UniqueFileDescriptor.hxx"
@@ -44,7 +45,7 @@ SocketDescriptor::IsStream() const noexcept
 	return GetType() == SOCK_STREAM;
 }
 
-#ifdef  __linux__
+#ifdef __linux__
 
 int
 SocketDescriptor::GetProtocol() const noexcept
