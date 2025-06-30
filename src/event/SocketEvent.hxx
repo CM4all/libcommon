e4fc8b3520b2bb0705c0d4e79a3798c86406f8c6
Max Kellermann <max.kellermann@ionos.com>
2025-06-30 11:03:36 +0200

    event/SocketEvent: add `#ifdef USE_EPOLL`

--- src/event/SocketEvent.hxx
+++ src/event/SocketEvent.hxx
@@ -4,6 +4,7 @@
 #pragma once
 
 #include "BackendEvents.hxx"
+#include "event/Features.h" // for USE_EPOLL
 #include "net/SocketDescriptor.hxx"
 #include "util/BindMethod.hxx"
 #include "util/IntrusiveList.hxx"
@@ -175,6 +176,7 @@ public:
 		Schedule(IMPLICIT_FLAGS);
 	}
 
+#ifdef USE_EPOLL
 	/**
 	 * Cancel READ but schedule READ_HANGUP.  This is useful to be
 	 * able to detect hangup while we're not interested in reading
@@ -184,6 +186,7 @@ public:
 	void CancelReadAndScheduleReadHangup() noexcept {
 		Schedule((GetScheduledFlags() & ~READ) | READ_HANGUP);
 	}
+#endif
 
 	bool IsReadPending() const noexcept {
 		return GetScheduledFlags() & READ;
