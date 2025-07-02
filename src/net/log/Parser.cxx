0e6d69a35192070e838935c38dc18ee92093a4f0
Max Kellermann <max.kellermann@ionos.com>
2025-07-02 18:19:21 +0200

    net/log/Parser: stop when unknown attribute is encountered

--- src/net/log/Parser.cxx
+++ src/net/log/Parser.cxx
@@ -98,94 +98,97 @@ log_server_apply_attributes(Deserializer d)
 
 		switch (attr) {
 		case Attribute::NOP:
-			break;
+			continue;
 
 		case Attribute::TIMESTAMP:
 			datagram.timestamp = TimePoint{Duration{d.ReadU64()}};
-			break;
+			continue;
 
 		case Attribute::REMOTE_HOST:
 			datagram.remote_host = d.ReadString();
-			break;
+			continue;
 
 		case Attribute::FORWARDED_TO:
 			datagram.forwarded_to = d.ReadString();
-			break;
+			continue;
 
 		case Attribute::HOST:
 			datagram.host = d.ReadString();
-			break;
+			continue;
 
 		case Attribute::SITE:
 			datagram.site = d.ReadString();
-			break;
+			continue;
 
 		case Attribute::HTTP_METHOD:
 			datagram.http_method = static_cast<HttpMethod>(d.ReadByte());
 			if (!http_method_is_valid(datagram.http_method))
 				throw ProtocolError();
 
-			break;
+			continue;
 
 		case Attribute::HTTP_URI:
 			datagram.http_uri = d.ReadStringView();
-			break;
+			continue;
 
 		case Attribute::HTTP_REFERER:
 			datagram.http_referer = d.ReadStringView();
-			break;
+			continue;
 
 		case Attribute::USER_AGENT:
 			datagram.user_agent = d.ReadStringView();
-			break;
+			continue;
 
 		case Attribute::MESSAGE:
 			datagram.message = d.ReadStringView();
-			break;
+			continue;
 
 		case Attribute::HTTP_STATUS:
 			datagram.http_status = static_cast<HttpStatus>(d.ReadU16());
 			if (!http_status_is_valid(datagram.http_status))
 				throw ProtocolError();
 
-			break;
+			continue;
 
 		case Attribute::LENGTH:
 			datagram.length = d.ReadU64();
 			datagram.valid_length = true;
-			break;
+			continue;
 
 		case Attribute::TRAFFIC:
 			datagram.traffic_received = d.ReadU64();
 			datagram.traffic_sent = d.ReadU64();
 			datagram.valid_traffic = true;
-			break;
+			continue;
 
 		case Attribute::DURATION:
 			datagram.duration = Duration(d.ReadU64());
 			datagram.valid_duration = true;
-			break;
+			continue;
 
 		case Attribute::TYPE:
 			datagram.type = static_cast<Type>(d.ReadByte());
-			break;
+			continue;
 
 		case Attribute::JSON:
 			datagram.json = d.ReadStringView();
-			break;
+			continue;
 
 		case Attribute::ANALYTICS_ID:
 			datagram.analytics_id = d.ReadString();
-			break;
+			continue;
 
 		case Attribute::GENERATOR:
 			datagram.generator = d.ReadString();
-			break;
+			continue;
 
 		case Attribute::CONTENT_TYPE:
 			datagram.content_type = static_cast<ContentType>(d.ReadByte());
-			break;
+			continue;
 		}
+
+		/* unrecognized attribute - stop here */
+		break;
 	}
 
 	FixUp(datagram);
