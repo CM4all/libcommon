86a6806c415ca2d3049ddec2191c63d15a7d2e16
Max Kellermann <max.kellermann@ionos.com>
2025-08-01 17:51:17 +0200

    system/Arch: add ToString(Arch)

--- src/system/Arch.hxx
+++ src/system/Arch.hxx
@@ -28,6 +28,31 @@ enum class Arch : uint_least8_t {
 };
 
 /**
+ * Convert the #Arch to a string.
+ *
+ * Returns nullptr as a special case for #NONE (because there is no
+ * string representation of this special value).
+ */
+constexpr std::string_view
+ToString(Arch arch) noexcept
+{
+	using std::string_view_literals::operator""sv;
+
+	switch (arch) {
+	case Arch::NONE:
+		break;
+
+	case Arch::AMD64:
+		return "amd64"sv;
+
+	case Arch::ARM64:
+		return "arm64"sv;
+	}
+
+	return {};
+}
+
+/**
  * Parse a string to an #Arch.  Returns #NONE if the string is not
  * recognized.
  */
