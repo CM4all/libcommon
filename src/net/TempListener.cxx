59b37ca43c131953081a853d60ea97722dde8c25
Max Kellermann <max.kellermann@ionos.com>
2025-09-03 19:54:17 +0200

    net/TempListener: new library

---
+++ src/net/TempListener.cxx
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// Copyright CM4all GmbH
+// author: Max Kellermann <max.kellermann@ionos.com>
+
+#include "TempListener.hxx"
+#include "ConnectSocket.hxx"
+#include "SocketError.hxx"
+#include "UniqueSocketDescriptor.hxx"
+#include "io/RuntimeDirectory.hxx"
+
+#include <unistd.h> // for unlink()
+#include <sys/stat.h> // for fchmod()
+
+static const char *
+make_child_socket_path(struct sockaddr_un *address)
+{
+	address->sun_family = AF_LOCAL;
+
+	return MakePrivateRuntimeDirectoryTemp(std::span{address->sun_path},
+					       "temp-socket-XXXXXX",
+					       "temp-socket-XXXXXX");
+}
+
+TempListener::~TempListener() noexcept
+{
+	if (IsDefined())
+		unlink(address.sun_path);
+}
+
+UniqueSocketDescriptor
+TempListener::Create(int socket_type, int backlog)
+{
+	make_child_socket_path(&address);
+
+	unlink(address.sun_path);
+
+	UniqueSocketDescriptor fd;
+	if (!fd.Create(AF_LOCAL, socket_type, 0))
+		throw MakeSocketError("failed to create local socket");
+
+	/* allow only beng-proxy to connect to it */
+	fchmod(fd.Get(), 0600);
+
+	if (!fd.Bind(GetAddress()))
+		throw MakeSocketError("failed to bind local socket");
+
+	if (!fd.Listen(backlog))
+		throw MakeSocketError("failed to listen on local socket");
+
+	return fd;
+}
+
+UniqueSocketDescriptor
+TempListener::Connect() const
+{
+	return CreateConnectSocketNonBlock(GetAddress(), SOCK_STREAM);
+}
