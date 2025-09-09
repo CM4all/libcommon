89763ed636345791ea3b8206f34f8ad31dbacfd1
Max Kellermann <max.kellermann@ionos.com>
2025-09-09 08:57:29 +0200

    event/net/UdpHandler: various API doc updates

--- src/event/net/UdpHandler.hxx
+++ src/event/net/UdpHandler.hxx
@@ -11,14 +11,13 @@ class SocketAddress;
 class UniqueFileDescriptor;
 
 /**
- * Handler for a #UdpListener.
- *
- * This is a class for a smooth API transition away from #UdpHandler
- * to an interface which allows receiving file descriptors.
+ * Handler for #UdpListener and #MultiUdpListener.
  */
 class UdpHandler {
 public:
 	/**
+	 * A datagram was received.
+	 * 
 	 * Exceptions thrown by this method will be passed to OnUdpError().
 	 *
 	 * @param uid the peer process uid, or -1 if unknown
@@ -29,8 +28,13 @@ public:
 				   SocketAddress address, int uid) = 0;
 
 	/**
-	 * The peer has hung up the (SOCK_SEQPACKET) connection.  This
-	 * method can be used to throw an exception.
+	 * The peer has hung up the (SOCK_SEQPACKET) connection.  The
+	 * implementation has three choices:
+	 *
+	 * 1. return true and handle packets that may remain in the
+	 *    receive queue
+	 * 2. delete the connection and return false
+	 * 3. throw exception (will be passed to OnUdpError())
 	 *
 	 * @return false if the #UdpHandler was destroyed inside this method
 	 */
