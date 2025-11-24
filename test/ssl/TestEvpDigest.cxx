9dbadc2d2e821d3273cd3a934863b24162ef7863
Max Kellermann <max.kellermann@ionos.com>
2025-11-24 18:05:03 +0100

    test/ssl/TestEvpDigest: use std::string_view literals

--- test/ssl/TestEvpDigest.cxx
+++ test/ssl/TestEvpDigest.cxx
@@ -7,14 +7,16 @@
 
 #include <gtest/gtest.h>
 
+using std::string_view_literals::operator""sv;
+
 TEST(TestEvpDigest, SHA1)
 {
 	EvpSHA1Context ctx;
-	ctx.Update(std::string_view{"The quick brown fox"});
-	ctx.Update(std::string_view{" jumps over the lazy dog"});
+	ctx.Update("The quick brown fox"sv);
+	ctx.Update(" jumps over the lazy dog"sv);
 
 	const auto digest = ctx.Final();
 	const auto hex = HexFormat(std::span{digest});
 	EXPECT_EQ(std::string_view(hex.data(), hex.size()),
-		  "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12");
+		  "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12"sv);
 }
