CM4all/libcommon

commit 3403e454a14fc994ed3cc03b6715f2e1ecab6d62

was/async/SimpleOutput: move basic pipe handling to class Output

src/was/async/SimpleOutput.cxx
@@ -3,11 +3,7 @@
 // author: Max Kellermann <max.kellermann@ionos.com>
 
 #include "SimpleOutput.hxx"
-#include "Buffer.hxx"
-#include "io/UniqueFileDescriptor.hxx"
 #include "system/Error.hxx"
-#include "net/SocketProtocolError.hxx"
-#include "util/DisposableBuffer.hxx"
 
 #include <cassert>
 #include <cerrno>
@@ -15,18 +11,13 @@
 namespace Was {
 
 SimpleOutput::SimpleOutput(EventLoop &event_loop, UniqueFileDescriptor &&pipe,
-			 SimpleOutputHandler &_handler) noexcept
-	:event(event_loop, BIND_THIS_METHOD(OnPipeReady), pipe.Release()),
-	 defer_write(event_loop, BIND_THIS_METHOD(OnDeferredWrite)),
+			   SimpleOutputHandler &_handler) noexcept
+	:output(event_loop, std::move(pipe), *this),
 	 handler(_handler)
 {
-	event.ScheduleImplicit();
 }
 
-SimpleOutput::~SimpleOutput() noexcept
-{
-	event.Close();
-}
+SimpleOutput::~SimpleOutput() noexcept = default;
 
 void
 SimpleOutput::Activate(DisposableBuffer _buffer) noexcept
@@ -37,45 +28,33 @@ SimpleOutput::Activate(DisposableBuffer _buffer) noexcept
 		return;
 
 	buffer = std::move(_buffer);
-	position = 0;
-
-	defer_write.Schedule();
+	output.Activate(*this);
+	output.DeferWrite();
 }
 
 void
-SimpleOutput::OnPipeReady(unsigned events) noexcept
-try {
-	if (events & SocketEvent::DEAD_MASK)
-		throw SocketClosedPrematurelyError("Hangup on WAS pipe");
-
-	TryWrite();
-} catch (...) {
-	handler.OnWasOutputError(std::current_exception());
+SimpleOutput::OnWasOutputError(std::exception_ptr &&error) noexcept
+{
+	handler.OnWasOutputError(std::move(error));
 }
 
-inline void
-SimpleOutput::OnDeferredWrite() noexcept
-try {
-	TryWrite();
-} catch (...) {
-	handler.OnWasOutputError(std::current_exception());
-}
-
-inline void
-SimpleOutput::TryWrite()
+std::size_t
+SimpleOutput::OnWasOutputReady(FileDescriptor pipe)
 {
 	assert(buffer);
+
+	std::size_t position = output.GetPosition();
 	assert(position < buffer.size());
 
 	std::span<const std::byte> r = buffer;
 	r = r.subspan(position);
 	assert(!r.empty());
 
-	auto nbytes = GetPipe().Write(r);
+	auto nbytes = pipe.Write(r);
 	if (nbytes <= 0) {
 		if (nbytes == 0 || errno == EAGAIN) {
-			event.ScheduleWrite();
-			return;
+			output.ScheduleWrite();
+			return 0;
 		} else
 			throw MakeErrno("Write error on WAS pipe");
 	}
@@ -84,10 +63,11 @@ SimpleOutput::TryWrite()
 
 	if (position == buffer.size()) {
 		/* done */
-		buffer = {};
-		event.ScheduleImplicit();
+		output.Stop();
 	} else
-		event.ScheduleWrite();
+		output.ScheduleWrite();
+
+	return nbytes;
 }
 
 } // namespace Was

src/was/async/Output.cxx
@@ -1,0 +1,78 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// Copyright CM4all GmbH
+// author: Max Kellermann <max.kellermann@ionos.com>
+
+#include "Output.hxx"
+#include "net/SocketProtocolError.hxx"
+#include "io/UniqueFileDescriptor.hxx"
+
+#include <cassert>
+
+namespace Was {
+
+Output::Output(EventLoop &event_loop, UniqueFileDescriptor &&pipe,
+	       OutputHandler &_handler) noexcept
+	:event(event_loop, BIND_THIS_METHOD(OnPipeReady), pipe.Release()),
+	 defer_write(event_loop, BIND_THIS_METHOD(OnDeferredWrite)),
+	 handler(_handler)
+{
+	event.ScheduleImplicit();
+}
+
+Output::~Output() noexcept
+{
+	event.Close();
+}
+
+void
+Output::Activate(OutputProducer &_producer) noexcept
+{
+	assert(producer == nullptr);
+
+	producer = &_producer;
+	position = 0;
+
+	defer_write.Schedule();
+}
+
+void
+Output::Deactivate() noexcept
+{
+	assert(producer != nullptr);
+
+	producer = nullptr;
+	CancelWrite();
+}
+
+inline void
+Output::TryWrite()
+{
+	assert(producer);
+
+	const std::size_t nbytes = producer->OnWasOutputReady(GetPipe());
+
+	/* if nbytes==0, this object might be deleted already */
+	if (nbytes > 0)
+		position += nbytes;
+}
+
+void
+Output::OnPipeReady(unsigned events) noexcept
+try {
+	if (events & SocketEvent::DEAD_MASK)
+		throw SocketClosedPrematurelyError("Hangup on WAS pipe");
+
+	TryWrite();
+} catch (...) {
+	handler.OnWasOutputError(std::current_exception());
+}
+
+inline void
+Output::OnDeferredWrite() noexcept
+try {
+	TryWrite();
+} catch (...) {
+	handler.OnWasOutputError(std::current_exception());
+}
+
+} // namespace Was

src/was/async/Output.hxx
@@ -1,0 +1,111 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// Copyright CM4all GmbH
+// author: Max Kellermann <max.kellermann@ionos.com>
+
+#pragma once
+
+#include "event/PipeEvent.hxx"
+#include "event/DeferEvent.hxx"
+
+#include <exception> // for std::exception_ptr
+
+class UniqueFileDescriptor;
+
+namespace Was {
+
+class OutputHandler {
+public:
+	virtual void OnWasOutputError(std::exception_ptr &&error) noexcept = 0;
+};
+
+class OutputProducer {
+public:
+	virtual std::size_t OnWasOutputReady(FileDescriptor pipe) = 0;
+};
+
+/**
+ * Generic class for a non-blocking WAS output.
+ */
+class Output final {
+	PipeEvent event;
+	DeferEvent defer_write;
+
+	OutputHandler &handler;
+
+	OutputProducer *producer = nullptr;
+
+	std::size_t position;
+
+public:
+	Output(EventLoop &event_loop, UniqueFileDescriptor &&pipe,
+	       OutputHandler &_handler) noexcept;
+	~Output() noexcept;
+
+	auto &GetEventLoop() const noexcept {
+		return event.GetEventLoop();
+	}
+
+	void Close() noexcept {
+		event.Close();
+		defer_write.Cancel();
+	}
+
+	bool IsActive() const noexcept {
+		return producer != nullptr;
+	}
+
+	void Activate(OutputProducer &_producer) noexcept;
+	void Deactivate() noexcept;
+
+	std::size_t GetPosition() const noexcept {
+		return position;
+	}
+
+	/**
+	 * Set the "position" field to zero to allow calling Stop()
+	 * without Activate(), in cases where there is no request
+	 * body.
+	 */
+	void ResetPosition() noexcept {
+		position = 0;
+	}
+
+	/**
+	 * Handle a STOP command.  Returns the number of bytes already
+	 * written to the pipe.  This may be called even after writing
+	 * has completed (because the `position` field does not get
+	 * cleared).
+	 */
+	std::size_t Stop() noexcept {
+		Deactivate();
+		return position;
+	}
+
+	void ScheduleWrite() noexcept {
+		event.ScheduleWrite();
+	}
+
+	void DeferWrite() noexcept {
+		defer_write.Schedule();
+	}
+
+	void DeferNextWrite() noexcept {
+		defer_write.ScheduleNext();
+	}
+
+	void CancelWrite() noexcept {
+		event.ScheduleImplicit();
+		defer_write.Cancel();
+	}
+
+private:
+	FileDescriptor GetPipe() const noexcept {
+		return event.GetFileDescriptor();
+	}
+
+	void TryWrite();
+	void OnDeferredWrite() noexcept;
+	void OnPipeReady(unsigned events) noexcept;
+};
+
+} // namespace Was

src/was/async/SimpleOutput.hxx
@@ -4,8 +4,7 @@
 
 #pragma once
 
-#include "event/PipeEvent.hxx"
-#include "event/DeferEvent.hxx"
+#include "Output.hxx"
 #include "util/DisposableBuffer.hxx"
 
 #include <exception> // for std::exception_ptr
@@ -19,15 +18,12 @@ public:
 	virtual void OnWasOutputError(std::exception_ptr error) noexcept = 0;
 };
 
-class SimpleOutput final {
-	PipeEvent event;
-	DeferEvent defer_write;
+class SimpleOutput final : OutputHandler, OutputProducer {
+	Output output;
 
 	SimpleOutputHandler &handler;
 
 	DisposableBuffer buffer;
-
-	std::size_t position;
 
 public:
 	SimpleOutput(EventLoop &event_loop, UniqueFileDescriptor &&pipe,
@@ -35,12 +31,11 @@ public:
 	~SimpleOutput() noexcept;
 
 	auto &GetEventLoop() const noexcept {
-		return event.GetEventLoop();
+		return output.GetEventLoop();
 	}
 
 	void Close() noexcept {
-		event.Close();
-		defer_write.Cancel();
+		output.Close();
 	}
 
 	bool IsActive() const noexcept {
@@ -55,7 +50,7 @@ public:
 	 * body.
 	 */
 	void ResetPosition() noexcept {
-		position = 0;
+		output.ResetPosition();
 	}
 
 	/**
@@ -66,19 +61,15 @@ public:
 	 */
 	std::size_t Stop() noexcept {
 		buffer = {};
-		event.ScheduleImplicit();
-		defer_write.Cancel();
-		return position;
+		return output.Stop();
 	}
 
 private:
-	FileDescriptor GetPipe() const noexcept {
-		return event.GetFileDescriptor();
-	}
+	// virtual methods from class OutputHandler
+	void OnWasOutputError(std::exception_ptr &&error) noexcept override;
 
-	void TryWrite();
-	void OnDeferredWrite() noexcept;
-	void OnPipeReady(unsigned events) noexcept;
+	// virtual methods from class OutputProducer
+	std::size_t OnWasOutputReady(FileDescriptor pipe) override;
 };
 
 } // namespace Was

src/was/async/meson.build
@@ -20,6 +20,7 @@ endif
 was_async = static_library(
   'was_async',
   'Control.cxx',
+  'Output.cxx',
   'SimpleInput.cxx',
   'SimpleOutput.cxx',
   'MultiClient.cxx',

commit 196dd8d7b52a587b6273e5bd70e45265468f5ba4

was/async/SimpleOutput: clear the buffer at the end
Fixes regression by commit 3403e454a14fc994ed3cc03b6715f2e1ecab6d62
which caused protocol errors.

src/was/async/SimpleOutput.cxx
@@ -63,6 +63,7 @@ SimpleOutput::OnWasOutputReady()
 
 	if (output.GetPosition() == buffer.size()) {
 		/* done */
+		buffer = {};
 		output.Deactivate();
 	} else
 		output.ScheduleWrite();