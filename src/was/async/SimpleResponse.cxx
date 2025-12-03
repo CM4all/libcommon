eacdda54f1f1fdfb9bde1e9a3a29f3d89759afc7
Max Kellermann <max.kellermann@ionos.com>
2025-12-03 15:09:48 +0100

    was/async/SimpleResponse: add method {Move,Copy}TextPlain()

--- src/was/async/SimpleResponse.cxx
+++ src/was/async/SimpleResponse.cxx
@@ -4,6 +4,7 @@
 
 #include "SimpleResponse.hxx"
 #include "SpanOutputProducer.hxx"
+#include "StringOutputProducer.hxx"
 #include "util/SpanCast.hxx"
 
 using std::string_view_literals::operator""sv;
@@ -15,6 +16,19 @@ SimpleResponse::SetTextPlain(std::string_view _body) noexcept
 {
 	body = std::make_unique<SpanOutputProducer>(AsBytes(_body));
 	headers.emplace("content-type"sv, "text/plain"sv);
+}
+
+void
+SimpleResponse::MoveTextPlain(std::string &&_body) noexcept
+{
+	body = std::make_unique<StringOutputProducer>(std::move(_body));
+	headers.emplace("content-type"sv, "text/plain"sv);
+}
+
+void
+SimpleResponse::CopyTextPlain(std::string_view _body) noexcept
+{
+	MoveTextPlain(std::string{_body});
 }
 
 } // namespace Was
--- src/was/async/SimpleResponse.hxx
+++ src/was/async/SimpleResponse.hxx
@@ -22,7 +22,22 @@ struct SimpleResponse {
 	std::multimap<std::string, std::string, std::less<>> headers;
 	std::unique_ptr<OutputProducer> body;
 
+	/**
+	 * Set a "text/plain" body referring to the specified string.
+	 * It must remain valid until the response has been written to
+	 * the pipe.
+	 */
 	void SetTextPlain(std::string_view _body) noexcept;
+
+	/**
+	 * Like SetTextPlain(), but move from a std::string.
+	 */
+	void MoveTextPlain(std::string &&_body) noexcept;
+
+	/**
+	 * Like SetTextPlain(), but copy the specified string.
+	 */
+	void CopyTextPlain(std::string_view _body) noexcept;
 
 	static SimpleResponse MethodNotAllowed(std::string allow) noexcept {
 		return {
