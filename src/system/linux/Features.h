264edd796a7a6ec15f48b8de21978985a4a098c8
Max Kellermann <max.kellermann@gmail.com>
2026-01-26 14:18:53 +0100

    system/linux: support glibc 2.43, fix openat2() conflict
    The Linux man-pages notes about openat2(): "glibc provides no wrapper
    for openat2(), necessitating the use of syscall(2)"
    
    .. but glibc 2.43 finally added a wrapper which caused a conflict with
    our existing definition.

--- src/system/linux/Features.h
+++ src/system/linux/Features.h
@@ -1,1 +1,9 @@
+#include <features.h> // for __GLIBC__ and __GLIBC_MINOR__
+
 #define HAVE_OPENAT2
+
+#ifdef __GLIBC__
+#if __GLIBC__ > 2 || (__GLIBC__ == 2 && __GLIBC_MINOR__ >= 43)
+#define HAVE_NATIVE_OPENAT2
+#endif
+#endif
