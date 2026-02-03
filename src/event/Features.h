074b56832c9d1d14ef866ff199afd1cb8ad59360
Max Kellermann <max.kellermann@ionos.com>
2026-02-03 11:52:59 +0100

    event: merge Features.h into config.h

--- src/event/Features.h
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: BSD-2-Clause
-// Copyright CM4all GmbH
-// author: Max Kellermann <max.kellermann@ionos.com>
-
-#pragma once
-
-#define USE_EVENTFD
-#define USE_SIGNALFD
-#define USE_EPOLL
