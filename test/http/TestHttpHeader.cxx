4fb583d9187643eea97f17cbfeebdb95d6f642d6
Max Kellermann <max.kellermann@ionos.com>
2026-04-09 11:56:42 +0200

    test/http/TestHttpHeader: new unit test

---
+++ test/http/TestHttpHeader.cxx
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// Copyright CM4all GmbH
+// author: Max Kellermann <max.kellermann@ionos.com>
+
+#include "http/HeaderName.hxx"
+
+#include <gtest/gtest.h>
+
+using std::string_view_literals::operator""sv;
+
+static constexpr const char *valid_http_headers[] = {
+	"0",
+	"x",
+	"x-",
+	"foo",
+	"foo-bar",
+	"!#$%&'*+-.^_`|~",
+};
+
+static constexpr const char *invalid_http_headers[] = {
+	"",
+	"foo bar",
+	"foo\n-bar",
+	"foo\r-bar",
+	"foo\t-bar",
+	"foo\x7f-bar",
+	"foo\x80-bar",
+	"foo\xff-bar",
+};
+
+static constexpr std::string_view invalid_http_headers2[] = {
+	"foo\0bar"sv,
+};
+
+TEST(HttpHeader, NameValid)
+{
+	for (const char *i : valid_http_headers)
+		EXPECT_TRUE(http_header_name_valid(i));
+
+	for (const char *i : invalid_http_headers)
+		EXPECT_FALSE(http_header_name_valid(i));
+
+	for (std::string_view i : valid_http_headers)
+		EXPECT_TRUE(http_header_name_valid(i));
+
+	for (std::string_view i : invalid_http_headers)
+		EXPECT_FALSE(http_header_name_valid(i));
+
+	for (std::string_view i : invalid_http_headers2)
+		EXPECT_FALSE(http_header_name_valid(i));
+}
